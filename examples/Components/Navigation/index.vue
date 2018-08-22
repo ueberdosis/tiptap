bc80b9209e4b7ed9b4741cbcc2a4e989a3fea61d
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-08-22 15:42:03 +0200

    improve navigation

--- examples/Components/Navigation/index.vue
+++ examples/Components/Navigation/index.vue
@@ -1,15 +1,38 @@
 <template>
 	<div class="navigation">
-		<router-link class="navigation__link" to="/">
-			Default
-		</router-link>
-		<router-link class="navigation__link" to="/bubble-menu">
-			Bubble Menu
-		</router-link>
-		<router-link class="navigation__link" to="/links">
-			Links
-		</router-link>
+
+		<div class="navigation__header">
+			<h1 class="navigation__logo">
+				tiptap
+			</h1>
+			<a href="https://github.com/heyscrumpy/tiptap">
+				<icon class="navigation__icon" name="github" />
+			</a>
+		</div>
+
+		<div class="navigation__links">
+			<router-link class="navigation__link" to="/">
+				Default
+			</router-link>
+			<router-link class="navigation__link" to="/bubble-menu">
+				Bubble Menu
+			</router-link>
+			<router-link class="navigation__link" to="/links">
+				Links
+			</router-link>
+		</div>
+		
 	</div>
 </template>
 
-<style lang="scss" src="./style.scss"></style>
+<script>
+import Icon from 'Components/Icon'
+
+export default {
+	components: {
+		Icon,
+	},
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
