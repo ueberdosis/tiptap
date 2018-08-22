3025642dfb0eebb75bc7bcc917d866ef4790b0b0
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-08-22 13:30:53 +0200

    add vue router

--- examples/main.js
+++ examples/main.js
@@ -1,5 +1,6 @@
 import '@babel/polyfill'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import svgSpriteLoader from 'helpers/svg-sprite-loader'
 import App from './App.vue'
 
@@ -8,6 +9,20 @@ svgSpriteLoader(__svg__.filename)
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('Components/Routes/Default'),
+  },
+]
+
+const router = new VueRouter({
+  routes,
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
