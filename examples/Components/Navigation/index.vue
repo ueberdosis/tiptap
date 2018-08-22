c6fb4ce8a3e131943f4cb4e0fb990b3d127ca406
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-08-22 19:22:57 +0200

    add contribute link

--- examples/Components/Navigation/index.vue
+++ examples/Components/Navigation/index.vue
@@ -5,9 +5,14 @@
 			<h1 class="navigation__logo">
 				tiptap <span class="navigation__beta">beta</span>
 			</h1>
-			<a href="https://github.com/heyscrumpy/tiptap" target="_blank">
-				<icon class="navigation__icon" name="github" />
-			</a>
+			<div>
+				<a class="navigation__link" href="https://github.com/heyscrumpy/tiptap/blob/master/CONTRIBUTING.md" target="_blank">
+					Contribute
+				</a>
+				<a class="navigation__github-link" href="https://github.com/heyscrumpy/tiptap" target="_blank">
+					<icon class="navigation__icon" name="github" />
+				</a>
+			</div>
 		</div>
 
 		<div class="navigation__links">
