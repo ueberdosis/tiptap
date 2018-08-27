1de45acef5ba41833c90fbb924ff2603f855ccb1
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-08-27 21:53:12 +0200

    fix em input rule

--- packages/tiptap-commands/src/commands/markInputRule.js
+++ packages/tiptap-commands/src/commands/markInputRule.js
@@ -6,11 +6,16 @@ export default function (regexp, markType, getAttrs) {
 		let tr = state.tr
 
 		if (match[1]) {
+			const startSpaces = match[0].search(/\S/)
 			let textStart = start + match[0].indexOf(match[1])
 			let textEnd = textStart + match[1].length
-			if (textEnd < end) tr.delete(textEnd, end)
-			if (textStart > start) tr.delete(start, textStart)
-			end = start + match[1].length
+			if (textEnd < end) {
+				tr.delete(textEnd, end)
+			}
+			if (textStart > start) {
+				tr.delete(start + startSpaces, textStart)
+			}
+			end = start + startSpaces + match[1].length
 		}
 
 		tr.addMark(start, end, markType.create(attrs))
