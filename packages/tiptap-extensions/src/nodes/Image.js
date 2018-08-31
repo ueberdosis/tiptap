674d1b03e058cd5426bf047499be02f672e85dce
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-08-31 21:47:11 +0200

    add basic drop support for images

--- packages/tiptap-extensions/src/nodes/Image.js
+++ packages/tiptap-extensions/src/nodes/Image.js
@@ -1,4 +1,4 @@
-import { Node } from 'tiptap'
+import { Node, Plugin } from 'tiptap'
 
 export default class ImageNode extends Node {
 
@@ -32,6 +32,52 @@ export default class ImageNode extends Node {
 			],
 			toDOM: node => ['img', node.attrs],
 		}
+	}
+
+	get plugins() {
+		return [
+			new Plugin({
+				props: {
+					handleDOMEvents: {
+						drop(view, event) {
+							event.preventDefault()
+
+							const hasFiles = event.dataTransfer
+								&& event.dataTransfer.files
+								&& event.dataTransfer.files.length
+
+							if (!hasFiles) {
+								return
+							}
+
+							const images = [...event.dataTransfer.files]
+								.filter(file => (/image/i).test(file.type))
+
+							if (images.length === 0) {
+								return
+							}
+
+							const { schema } = view.state
+							const coordinates = view.posAtCoords({ left: event.clientX, top: event.clientY })
+
+							images.forEach(image => {
+								const reader = new FileReader()
+
+								reader.onload = readerEvent => {
+									const node = schema.nodes.image.create({
+										src: readerEvent.target.result,
+									})
+									const transaction = view.state.tr.insert(coordinates.pos, node)
+									view.dispatch(transaction)
+								}
+								reader.readAsDataURL(image)
+							})
+
+						},
+					},
+				},
+			}),
+		]
 	}
 
 }
---
+++ packages/tiptap/src/utils/plugin.js
@@ -0,0 +1,3 @@
+import { Plugin } from 'prosemirror-state'
+
+export default Plugin
