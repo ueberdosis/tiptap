06bb2ab802a5ae961e9584326baaef46a3a9d200
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-10-30 22:49:50 +0100

    fix linting error

--- packages/tiptap/src/Components/FloatingMenu.js
+++ packages/tiptap/src/Components/FloatingMenu.js
@@ -20,14 +20,16 @@ export default {
 		},
 	},
 	render(createElement) {
-		if (this.editor) {
-			return createElement('div', this.$scopedSlots.default({
-				focused: this.editor.view.focused,
-				focus: this.editor.focus,
-				commands: this.editor.commands,
-				isActive: this.editor.isActive.bind(this.editor),
-				markAttrs: this.editor.markAttrs.bind(this.editor),
-			}))
+		if (!this.editor) {
+			return null
 		}
+
+		return createElement('div', this.$scopedSlots.default({
+			focused: this.editor.view.focused,
+			focus: this.editor.focus,
+			commands: this.editor.commands,
+			isActive: this.editor.isActive.bind(this.editor),
+			markAttrs: this.editor.markAttrs.bind(this.editor),
+		}))
 	},
 }
