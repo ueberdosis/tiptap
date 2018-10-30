35cc0a285f6330563f4c4c0a6efc1566e3e44727
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-10-30 22:43:17 +0100

    remove unused file

--- packages/tiptap/src/utils/index.js
+++ packages/tiptap/src/utils/index.js
@@ -1,4 +1,3 @@
-export { default as buildMenuActions } from './buildMenuActions'
 export { default as builtInKeymap } from './builtInKeymap'
 export { default as ComponentView } from './ComponentView'
 export { default as initNodeViews } from './initNodeViews'
