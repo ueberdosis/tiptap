807c2a3d23ffd9d16a54449339eeafb1b4d78bd2
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-11-08 21:43:44 +0100

    remove legacy code

--- packages/_legacy-tiptap/src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Editor } from './components/editor'
-export { default as Extension } from './utils/extension'
-export { default as Node } from './utils/node'
-export { default as Mark } from './utils/mark'
-export { default as Plugin } from './utils/plugin'
--- packages/_legacy-tiptap/src/nodes/index.js
+++
@@ -1,9 +0,0 @@
-import Doc from './Doc'
-import Paragraph from './Paragraph'
-import Text from './Text'
-
-export default [
-	new Doc(),
-	new Text(),
-	new Paragraph(),
-]
