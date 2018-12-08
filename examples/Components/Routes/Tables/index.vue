5acbe8e69deba57ac829a768231003141de6941b
Philipp Kühn <kontakt@philipp-kuehn.com>
2018-12-08 19:32:50 +0100

    fix whitespace

--- examples/Components/Routes/Tables/index.vue
+++ examples/Components/Routes/Tables/index.vue
@@ -244,25 +244,25 @@
 						new TableRow(),
 					],
 					content: `
-          <h2>
-            Tables
-          </h2>
-          <table>
-            <tr>
-              <th colspan="3" data-colwidth="100,0,0">Wide header</th>
-            </tr>
-            <tr>
-              <td>One</td>
-              <td>Two</td>
-              <td>Three</td>
-            </tr>
-            <tr>
-              <td>Four</td>
-              <td>Five</td>
-              <td>Six</td>
-            </tr>
-          </table>
-        `,
+            <h2>
+              Tables
+            </h2>
+            <table>
+              <tr>
+                <th colspan="3" data-colwidth="100,0,0">Wide header</th>
+              </tr>
+              <tr>
+                <td>One</td>
+                <td>Two</td>
+                <td>Three</td>
+              </tr>
+              <tr>
+                <td>Four</td>
+                <td>Five</td>
+                <td>Six</td>
+              </tr>
+            </table>
+          `,
 				}),
 			}
 		},
