0cf905abb9f9546fb379b7f539428d9ff655fa02
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-01-19 09:29:15 +0100

    fix a bug for getting mark range, fix #156

--- packages/tiptap-utils/src/utils/getMarkRange.js
+++ packages/tiptap-utils/src/utils/getMarkRange.js
@@ -22,12 +22,14 @@ export default function ($pos = null, type = null) {
     startPos -= $pos.parent.child(startIndex).nodeSize
   }
 
-  let endIndex = $pos.indexAfter()
-  let endPos = startPos + start.node.nodeSize
-  while (endIndex < $pos.parent.childCount && link.isInSet($pos.parent.child(endIndex).marks)) {
-    endPos += $pos.parent.child(endIndex).nodeSize
-    endIndex += 1
-  }
+  const endIndex = $pos.indexAfter()
+  const endPos = startPos + start.node.nodeSize
+
+  // disable for now. see #156
+  // while (endIndex < $pos.parent.childCount && link.isInSet($pos.parent.child(endIndex).marks)) {
+  //   endPos += $pos.parent.child(endIndex).nodeSize
+  //   endIndex += 1
+  // }
 
   return { from: startPos, to: endPos }
 
