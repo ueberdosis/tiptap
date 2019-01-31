fce6d9dac90323ac8787f096713c71fcfd3ab9b1
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-01-31 11:46:32 +0100

    improve performance for codeblocks with highlight.js

--- packages/tiptap-extensions/src/nodes/CodeBlockHighlight.js
+++ packages/tiptap-extensions/src/nodes/CodeBlockHighlight.js
@@ -122,13 +122,16 @@ export default class CodeBlockHighlight extends Node {
           init(_, { doc }) {
             return getDecorations(doc)
           },
-          apply(tr, set) {
+          apply(transaction, decorationSet) {
             // TODO: find way to cache decorations
             // see: https://discuss.prosemirror.net/t/how-to-update-multiple-inline-decorations-on-node-change/1493
-            if (tr.docChanged) {
-              return getDecorations(tr.doc)
+
+            const currentNodeName = transaction.curSelection.$head.parent.type.name
+            if (transaction.docChanged && currentNodeName === 'code_block') {
+              return getDecorations(transaction.doc)
             }
-            return set.map(tr.mapping, tr.doc)
+
+            return decorationSet.map(transaction.mapping, transaction.doc)
           },
         },
         props: {
