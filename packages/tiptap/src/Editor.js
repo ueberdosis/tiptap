e9cd6b6f6c8e8873d30a472782d2cbcdc13e6dae
Alecyrus <heyuangunia@gmail.com>
2019-05-07 08:50:21 +0800

    fix #289
    A workaround before the better solution comes out.

--- packages/tiptap/src/Editor.js
+++ packages/tiptap/src/Editor.js
@@ -433,6 +433,7 @@ export default class Editor extends Emitter {
       plugins: this.state.plugins.concat([plugin]),
     })
     this.view.updateState(newState)
+    this.view.updatePluginViews()
   }
 
   destroy() {
