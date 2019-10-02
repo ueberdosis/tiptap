b0ed628d2f8f7323c33c8d7df770f53750fb5e1d
Chrissi2812 <cf@pixel-kraft.de>
2019-10-02 18:22:04 +0200

    change updateMark to update empty selection

--- packages/tiptap-commands/src/commands/updateMark.js
+++ packages/tiptap-commands/src/commands/updateMark.js
@@ -1,6 +1,17 @@
 export default function (type, attrs) {
   return (state, dispatch) => {
-    const { from, to } = state.selection
+    const { empty, $cursor } = state.selection
+    let { from, to } = state.selection
+
+    if (empty && $cursor) {
+      const { parent, pos, textOffset } = $cursor
+      const parentOffset = pos - textOffset
+      const { node: { nodeSize } } = parent.childAfter(parentOffset)
+
+      from = parentOffset
+      to = from + nodeSize
+    }
+
     return dispatch(state.tr.addMark(from, to, type.create(attrs)))
   }
 }
