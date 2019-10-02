faa5e83f0e0fc34ff7950db975a2739b388011f1
Chrissi2812 <cf@pixel-kraft.de>
2019-10-02 18:10:06 +0200

    change removeMark to remove empty selection

--- packages/tiptap-commands/src/commands/removeMark.js
+++ packages/tiptap-commands/src/commands/removeMark.js
@@ -1,6 +1,17 @@
 export default function (type) {
   return (state, dispatch) => {
-    const { from, to } = state.selection
+    const { empty, $cursor } = state.selection
+    let { from, to } = state.selection
+
+    if (empty && $cursor) {
+      const { parent, pos, textOffset } = $cursor
+      const parentOffset = pos - textOffset
+      const { node: { nodeSize } } = parent.childAfter(parentOffset)
+
+      from = parentOffset
+      to = from + nodeSize
+    }
+
     return dispatch(state.tr.removeMark(from, to, type))
   }
 }
