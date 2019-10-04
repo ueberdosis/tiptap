08aa7e88bc546eb9789d9d8df321f990fd04a576
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-10-04 14:16:38 +0200

    use markRange for empty selections in removeMark and updateMark, fix #449

--- packages/tiptap-commands/src/commands/updateMark.js
+++ packages/tiptap-commands/src/commands/updateMark.js
@@ -1,6 +1,17 @@
+import { getMarkRange } from 'tiptap-utils'
+
 export default function (type, attrs) {
   return (state, dispatch) => {
-    const { from, to } = state.selection
+    let { from, to } = state.selection
+    const { $from, empty } = state.selection
+
+    if (empty) {
+      const range = getMarkRange($from, type)
+
+      from = range.from
+      to = range.to
+    }
+
     return dispatch(state.tr.addMark(from, to, type.create(attrs)))
   }
 }
