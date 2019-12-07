d8fe4ee3f8ab69ca33ef7c09da7c2f1f074808be
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-12-07 22:23:49 +0100

    add dynamic alias

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,27 +1,16 @@
-// Server API makes it possible to hook into various parts of Gridsome
-// on server-side and add custom data to the GraphQL data layer.
-// Learn more: https://gridsome.org/docs/server-api/
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 const path = require('path')
+const globby = require('globby')
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
-
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
-
   api.chainWebpack(config => {
     config.resolve.extensions
       .add('.ts')
-
-    config.resolve.alias
-      .set('@tiptap/core', path.resolve('./packages/tiptap-core'))
+    
+    globby.sync('./packages/*', { onlyDirectories: true })
+      .map(name => name.replace('./packages/tiptap-', ''))
+      .forEach(name => {
+        config.resolve.alias
+          .set(`@tiptap/${name}`, path.resolve(`./packages/tiptap-${name}`))
+      })
   })
-  
 }
