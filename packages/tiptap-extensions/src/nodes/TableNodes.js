b849e4d9dbe758c98d70809d69123ff340289c09
Tymotheos Szulc <tim.e.szulc@gmail.com>
2019-12-11 11:03:46 -0500

    feat: add alignment to table cells

--- packages/tiptap-extensions/src/nodes/TableNodes.js
+++ packages/tiptap-extensions/src/nodes/TableNodes.js
@@ -1,9 +1,29 @@
 import { tableNodes } from 'prosemirror-tables'
+import { ALIGN_PATTERN } from 'tiptap/Utils'
 
 export default tableNodes({
   tableGroup: 'block',
   cellContent: 'block+',
   cellAttributes: {
+    align: {
+      default: null,
+      getFromDOM(dom) {
+        const { textAlign = null } = dom.style
+        const align = dom.getAttribute('align') || textAlign || ''
+
+        return align && ALIGN_PATTERN.test(align)
+          ? align
+          : null
+      },
+      setDOMAttr(value, attrs) {
+        if (!value) {
+          return
+        }
+
+        const style = { style: `${(attrs.style || '')}text-align: ${value};` }
+        Object.assign(attrs, style)
+      },
+    },
     background: {
       default: null,
       getFromDOM(dom) {
