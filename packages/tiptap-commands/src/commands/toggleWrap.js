8478747d35208f3c7bef19236a67c69b3b15eabc
Tymotheos Szulc <tim.e.szulc@gmail.com>
2019-12-13 11:42:05 -0500

    fix: use getNodeAttrs when wrapping

--- packages/tiptap-commands/src/commands/toggleWrap.js
+++ packages/tiptap-commands/src/commands/toggleWrap.js
@@ -1,5 +1,5 @@
 import { wrapIn, lift } from 'prosemirror-commands'
-import { nodeIsActive } from 'tiptap-utils'
+import { getNodeAttrs, nodeIsActive } from 'tiptap-utils'
 
 export default function (type) {
   return (state, dispatch, view) => {
@@ -9,6 +9,8 @@ export default function (type) {
       return lift(state, dispatch)
     }
 
-    return wrapIn(type)(state, dispatch, view)
+    const attrs = getNodeAttrs(state, type)
+
+    return wrapIn(type, attrs)(state, dispatch, view)
   }
 }
