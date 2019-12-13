f4ddc738b3f7f868b96807ae7226ce224bce66a1
Tymotheos Szulc <tim.e.szulc@gmail.com>
2019-12-13 11:24:28 -0500

    feat: maintain alignment when toggling block type

--- packages/tiptap-commands/src/commands/toggleBlockType.js
+++ packages/tiptap-commands/src/commands/toggleBlockType.js
@@ -1,14 +1,47 @@
 import { setBlockType } from 'prosemirror-commands'
-import { nodeIsActive } from 'tiptap-utils'
+import { nodeIsActive, nodeEqualsType } from 'tiptap-utils'
 
-export default function (type, toggletype, attrs = {}) {
+export default function toggleBlockType(type, toggletype, attrs = {}) {
   return (state, dispatch, view) => {
     const isActive = nodeIsActive(state, type, attrs)
+    let attributes = !isActive
+      ? { ...attrs }
+      : null
 
-    if (isActive) {
-      return setBlockType(toggletype)(state, dispatch, view)
+    if ('align' in type.attrs) {
+      const {
+        schema: {
+          nodes: { paragraph, heading },
+        },
+        selection: { from, to },
+      } = state
+      let align = null
+
+      state.doc.nodesBetween(from, to, node => {
+        if (align) {
+          return false
+        }
+
+        if (!nodeEqualsType({ node, types: [paragraph, heading] })) {
+          return true
+        }
+
+        align = node.attrs.align || null
+
+        return false
+      })
+
+      if (align) {
+        attributes = !isActive
+          ? { ...attrs, align }
+          : { align }
+      }
     }
 
-    return setBlockType(type, attrs)(state, dispatch, view)
+    if (isActive) {
+      return setBlockType(toggletype, attributes)(state, dispatch, view)
+    }
+
+    return setBlockType(type, attributes)(state, dispatch, view)
   }
 }
