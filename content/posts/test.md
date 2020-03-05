aff52325a2c9bf3a23c5e0d84c5a241c453f6b54
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-03-05 09:24:30 +0100

    add more demos

--- content/posts/test.md
+++ content/posts/test.md
@@ -3,4 +3,16 @@ title: Test
 slug: test
 ---
 
-<demo name="Time" />
+# Super epic editor demo
+
+- magic
+- epic
+
+<!-- <demo name="Basic" key="basic" /> -->
+
+# Super epic time demo
+
+- nice numbers
+- nice time
+
+<demo name="Time" key="time" />
---
+++ src/demos/Basic/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    <div ref="editor"></div>
+  </Layout>
+</template>
+
+<script>
+import Editor from '@tiptap/core'
+
+export default {
+  mounted() {
+    new Editor({
+      element: this.$refs.editor,
+      content: '<p>foo</p>',
+    })
+  }
+}
+</script>
