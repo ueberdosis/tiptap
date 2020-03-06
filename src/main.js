3f442a65923a8e9b722bc79df8ee709e67bbe3d7
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-03-06 12:06:55 +0100

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import Prism from 'prismjs'
 import 'prismjs/themes/prism-okaidia.css'
-
 import DefaultLayout from '~/layouts/Default.vue'
 import Demo from '~/components/Demo.vue'
 
