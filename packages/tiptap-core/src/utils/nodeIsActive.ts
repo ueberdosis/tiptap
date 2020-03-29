adfa7f0243419c1c914aae035e2fccacd1440808
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-03-30 00:15:20 +0200

    add nodeIsActive

---
+++ packages/tiptap-core/src/utils/nodeIsActive.ts
@@ -0,0 +1,15 @@
+import { findParentNode, findSelectedNodeOfType } from 'prosemirror-utils'
+import { EditorState } from 'prosemirror-state'
+import { Node, NodeType } from 'prosemirror-model'
+
+export default function nodeIsActive(state: EditorState, type: NodeType, attrs = {}) {
+  const predicate = (node: Node) => node.type === type
+  const node = findSelectedNodeOfType(type)(state.selection)
+    || findParentNode(predicate)(state.selection)
+
+  if (!Object.keys(attrs).length || !node) {
+    return !!node
+  }
+
+  return node.node.hasMarkup(type, attrs)
+}
