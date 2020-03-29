18c5164af97897d60e94f384f0db1ee10f8cfc85
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-03-30 00:25:14 +0200

    add getMarkAttrs

---
+++ packages/tiptap-core/src/utils/getMarkAttrs.ts
@@ -0,0 +1,19 @@
+import { EditorState } from 'prosemirror-state'
+import { Mark, MarkType } from 'prosemirror-model'
+
+export default function getMarkAttrs(state: EditorState, type: MarkType) {
+  const { from, to } = state.selection
+  let marks: Mark[] = []
+
+  state.doc.nodesBetween(from, to, node => {
+    marks = [...marks, ...node.marks]
+  })
+
+  const mark = marks.find(markItem => markItem.type.name === type.name)
+
+  if (mark) {
+    return mark.attrs
+  }
+
+  return {}
+}
