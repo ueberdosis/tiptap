3c0727fd59097c4edb9b03d872998f73fbe7270d
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-04-17 12:51:49 +0200

    move docs to own folder

--- docs/src/main.js
+++ docs/src/main.js
@@ -4,13 +4,13 @@ import 'prismjs/components/prism-jsx.js'
 import DefaultLayout from '~/layouts/Default.vue'
 import Demo from '~/components/Demo'
 import Tab from '~/components/Tab'
-import ReactWrapper from '~/components/ReactWrapper'
+import ReactRenderer from '~/components/ReactRenderer'
 
 export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout)
   Vue.component('Demo', Demo)
   Vue.component('Tab', Tab)
-  Vue.component('ReactWrapper', ReactWrapper)
+  Vue.component('ReactRenderer', ReactRenderer)
   Vue.filter('highlight', (code, lang = 'javascript') => {
     return Prism.highlight(code, Prism.languages[lang], lang)
   })
