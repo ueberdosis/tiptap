3deae61a15bf05f1b36f1575f5521c78c4646736
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-04-21 22:48:27 +0200

    add heading input rule

--- packages/extension-heading/index.ts
+++ packages/extension-heading/index.ts
@@ -1,5 +1,7 @@
 import { Node, CommandSpec } from '@tiptap/core'
 import { NodeSpec } from 'prosemirror-model'
+import VerEx from 'verbal-expressions'
+import { textblockTypeInputRule } from 'prosemirror-inputrules'
 
 type Level = 1 | 2 | 3 | 4 | 5 | 6
 
@@ -50,6 +52,19 @@ export default class Heading extends Node {
         next()
       },
     }
+  }
+
+  inputRules() {
+    return this.options.levels.map((level: Level) => {
+      const regex = VerEx()
+        .startOfLine()
+        .find('#')
+        .repeatPrevious(level)
+        .whitespace()
+        .endOfLine()
+
+      return textblockTypeInputRule(regex, this.type, { level })
+    })
   }
 
 }
