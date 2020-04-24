3cca79b9609456b03d0612d532657d0cdfff4875
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-04-24 12:23:29 +0200

    refactoring

--- packages/core/src/ComponentView.ts
+++ packages/core/src/ComponentView.ts
@@ -3,26 +3,29 @@ import Vue from 'vue'
 export default class ComponentView {
   // @ts-ignore
   constructor(component, options) {
-    // @ts-ignore
-    this.component = component
-    // @ts-ignore
-    this.dom = this.createDOM()
-    // @ts-ignore
-    this.contentDOM = this.vm.$refs.content
+    this.mount(component)
   }
 
-  createDOM() {
+  // @ts-ignore
+  mount(component) {
     // @ts-ignore
-    const Component = Vue.extend(this.component)
+    const Component = Vue.extend(component)
 
     // @ts-ignore
     this.vm = new Component({
       // parent: this.parent,
       // propsData: props,
     }).$mount()
+  }
 
+  get dom() {
     // @ts-ignore
     return this.vm.$el
+  }
+
+  get contentDOM() {
+    // @ts-ignore
+    return this.vm.$refs.content
   }
 
 }
