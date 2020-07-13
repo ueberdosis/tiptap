2f1b6989772ad46df12566921481768960c71217
Hans Pagel <hans.pagel@ueber.io>
2020-07-13 14:45:19 +0200

    remove ad

--- examples/Components/Ad/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <a class="ad" href="https://scrumpy.io/" target="_blank">
-    <img class="ad__image" src="https://drop.philipp-kuehn.com/7RIu3ptVUQ.png" alt="Scrumpy. Agile, Made Simple. Get Started." />
-  </a>
-</template>
-
-<style lang="scss" src="./style.scss" scoped></style>
