e5a6039706fa4cdb7263afbb105446779c0714e8
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-07-15 11:36:33 +0200

    fix xss issue

--- packages/core/src/utils/elementFromString.ts
+++ packages/core/src/utils/elementFromString.ts
@@ -1,6 +1,7 @@
-export default function elementFromString(value: string): HTMLDivElement {
-  const element = document.createElement('div')
-  element.innerHTML = value.trim()
+export default function elementFromString(value: string): HTMLElement {
+  const htmlString = `<div>${value}</div>`
+  const parser = new window.DOMParser
+  const element = parser.parseFromString(htmlString, 'text/html').body
 
   return element
 }
