1873cd89fcf05897b51cd7830f34dcce849646bf
Hans Pagel <hans.pagel@ueber.io>
2020-08-19 18:17:44 +0200

    naming

--- docs/src/demos/Focus/index.spec.js
+++ docs/src/demos/Focus/index.spec.js
@@ -3,7 +3,7 @@ context('focus', () => {
     cy.visit('/examples/focus')
   })
 
-  describe('editable', () => {
+  describe('focus class', () => {
     it('should have class', () => {
       cy.get('.ProseMirror').window().then(window => {
         cy.get('.ProseMirror p:first').should('have.class', 'has-focus')
