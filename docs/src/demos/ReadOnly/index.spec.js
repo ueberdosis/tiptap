399be73e7b20a18df1919af706b53c10c61dd65a
Hans Pagel <hans.pagel@ueber.io>
2020-08-19 17:55:08 +0200

    add read-only test

--- docs/src/demos/ReadOnly/index.spec.js
+++ docs/src/demos/ReadOnly/index.spec.js
@@ -2,4 +2,34 @@ context('read-only', () => {
   beforeEach(() => {
     cy.visit('/examples/read-only')
   })
+
+
+  describe('editable', () => {
+    it('should be editable', () => {
+      cy.get('.ProseMirror').window().then(window => {
+        const { editor } = window
+
+        cy.get('#editable').check()
+
+        editor.insertText('Edited: ')
+
+        cy.get('.ProseMirror h2:first')
+          .should('contain', 'Edited: ')
+      })
+    })
+
+    it('should be read-only', () => {
+      cy.get('.ProseMirror').window().then(window => {
+        const { editor } = window
+
+        cy.get('#editable').uncheck()
+
+        editor.insertText('Edited: ')
+
+        cy.get('.ProseMirror h2:first')
+          .should('not.contain', 'Edited: ')
+      })
+    })
+
+  })
 })
