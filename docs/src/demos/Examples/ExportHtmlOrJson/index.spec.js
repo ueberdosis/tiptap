c6dcacdc19988d0e4b93c7c276756776eec5c449
Hans Pagel <hans.pagel@ueber.io>
2020-08-20 18:00:35 +0200

    add tests to export html and json

--- docs/src/demos/Examples/ExportHtmlOrJson/index.spec.js
+++ docs/src/demos/Examples/ExportHtmlOrJson/index.spec.js
@@ -2,4 +2,63 @@ context('export-html-or-json', () => {
   beforeEach(() => {
     cy.visit('/examples/export-html-or-json')
   })
+
+  describe('export', () => {
+    it('should return json', () => {
+      cy.get('.ProseMirror').window().then(window => {
+        const { editor } = window
+        const json = editor.json()
+
+        expect(json).to.deep.equal({
+          'type': 'document',
+          'content': [
+            {
+              'type': 'paragraph',
+              'content': [
+                {
+                  'type': 'text',
+                  'text': 'You are able to export your data as '
+                },
+                {
+                  'type': 'text',
+                  'marks': [
+                    {
+                      'type': 'code'
+                    }
+                  ],
+                  'text': 'HTML'
+                },
+                {
+                  'type': 'text',
+                  'text': ' or '
+                },
+                {
+                  'type': 'text',
+                  'marks': [
+                    {
+                      'type': 'code'
+                    }
+                  ],
+                  'text': 'JSON'
+                },
+                {
+                  'type': 'text',
+                  'text': '.'
+                }
+              ]
+            }
+          ]
+        })
+      })
+    })
+
+    it('should return html', () => {
+      cy.get('.ProseMirror').window().then(window => {
+        const { editor } = window
+        const html = editor.html()
+
+        expect(html).to.equal('<p>You are able to export your data as <code>HTML</code> or <code>JSON</code>.</p>')
+      })
+    })
+  })
 })
