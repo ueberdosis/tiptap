69a56e5473213e375a79dc92d56ab7ff4baea6cf
Hans Pagel <hans.pagel@ueber.io>
2020-08-31 14:47:53 +0200

    add minimalist example

---
+++ docs/src/demos/Examples/Simple/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-starter-kit'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null,
+    }
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: '<p>This is a radically reduced version of tiptap for minimalisits. It has only support for a document, paragraphs and text, that’s it.</p>',
+      extensions: [
+        new Document(),
+        new Paragraph(),
+        new Text(),
+      ],
+    })
+
+    window.editor = this.editor
+  },
+
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>
---
+++ docs/src/docPages/examples/simple.md
@@ -0,0 +1,3 @@
+# Simple
+
+<demo name="Examples/Simple" highlight="7-9,27-29" />
