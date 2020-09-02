b394f8619d48820a498b2494fb26cbddaf2be9e0
Hans Pagel <hans.pagel@ueber.io>
2020-09-02 15:20:23 +0200

    add tests to the bold extension demo

---
+++ docs/src/demos/Extensions/Bold/index.spec.js
@@ -0,0 +1,26 @@
+context('/api/extensions/bold', () => {
+  beforeEach(() => {
+    cy.visit('/api/extensions/bold')
+
+    cy.get('.ProseMirror').window().then(window => {
+      const { editor } = window
+      editor.setContent('<p>Example Text</p>')
+      editor.focus().selectAll()
+    })
+  })
+
+  describe('bold', () => {
+    it('should make the selected text bold', () => {
+      cy.get('.demo__preview button:first').click({ force: true })
+      cy.get('.ProseMirror').contains('strong', 'Example Text')
+
+      cy.get('.demo__preview button:first').click({ force: true })
+      cy.get('.ProseMirror strong').should('not.exist')
+    })
+
+    it('should toggle the selected text bold', () => {
+      cy.get('.demo__preview button:first').dblclick({ force: true })
+      cy.get('.ProseMirror strong').should('not.exist')
+    })
+  })
+})
