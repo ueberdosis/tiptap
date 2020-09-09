fab2fce67167012fc64478caf6bd946f0666bb4d
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-09-09 18:47:37 +0200

    remove test file

--- docs/src/main.js
+++ docs/src/main.js
@@ -6,8 +6,6 @@ import Demo from '~/components/Demo'
 import Tab from '~/components/Tab'
 import ReactRenderer from '~/components/ReactRenderer'
 
-import '@tiptap/core/../src/test.ts'
-
 export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', App)
   Vue.component('Demo', Demo)
