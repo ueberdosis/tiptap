0efab2133828f70562a1cba286c6c6323710f5c8
Hans Pagel <hans.pagel@ueber.io>
2020-09-09 16:12:39 +0200

    write about the paragraph extension

--- docs/src/demos/Extensions/Document/index.spec.js
+++ docs/src/demos/Extensions/Document/index.spec.js
@@ -1,37 +1,5 @@
-context('/api/extensions/bold', () => {
+context('/api/extensions/document', () => {
   beforeEach(() => {
-    cy.visit('/api/extensions/bold')
-
-    cy.get('.ProseMirror').window().then(window => {
-      const { editor } = window
-      editor.setContent('<p>Example Text</p>')
-      editor.focus().selectAll()
-    })
-  })
-
-  describe('bold', () => {
-    it('the button should make the selected text bold', () => {
-      cy.get('.demo__preview button:first').click({ force: true })
-      cy.get('.ProseMirror').contains('strong', 'Example Text')
-    })
-
-    it('the button should toggle the selected text bold', () => {
-      cy.get('.demo__preview button:first').dblclick({ force: true })
-      cy.get('.ProseMirror strong').should('not.exist')
-    })
-
-    it('the keyboard shortcut should make the selected text bold', () => {
-      const shortcut = Cypress.platform === 'darwin' ? '{meta}b' : '{ctrl}b'
-
-      cy.get('.ProseMirror').type(shortcut, {force: true})
-      cy.get('.ProseMirror').contains('strong', 'Example Text')
-    })
-
-    it('the keyboard shortcut should toggle the selected text bold', () => {
-      const shortcut = Cypress.platform === 'darwin' ? '{meta}b' : '{ctrl}b'
-
-      cy.get('.ProseMirror').type(shortcut, {force: true}).type(shortcut, {force: true})
-      cy.get('.ProseMirror strong').should('not.exist')
-    })
+    cy.visit('/api/extensions/document')
   })
 })
--- docs/src/docPages/api/extensions/paragraph.md
+++ docs/src/docPages/api/extensions/paragraph.md
@@ -1,2 +1,17 @@
 # Paragraph
 Enables you to use paragraphs in the editor.
+
+## Options
+*None*
+
+## Commands
+*None*
+
+## Keybindings
+*None*
+
+## Source Code
+[packages/extension-paragraph/](https://github.com/ueberdosis/tiptap-next/blob/main/packages/extension-paragraph/)
+
+## Usage
+<demo name="Extensions/Paragraph" highlight="11,29" />
