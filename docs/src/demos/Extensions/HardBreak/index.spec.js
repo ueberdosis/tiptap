f5dca8defba17476987a2b4f2f3bddda9e4ed9fe
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-09-11 22:26:53 +0200

    refactoring

--- docs/src/demos/Extensions/HardBreak/index.spec.js
+++ docs/src/demos/Extensions/HardBreak/index.spec.js
@@ -18,7 +18,7 @@ context('/api/extensions/hard-break', () => {
 
   it('the default keyboard shortcut should add a line break', () => {
     cy.get('.ProseMirror br').should('not.exist')
-    cy.get('.ProseMirror').type('{shift}{enter}', { force: true })
+    cy.get('.ProseMirror').trigger('keydown', { metaKey: true, shiftKey: true, key: 'Enter' })
     cy.get('.ProseMirror br').should('exist')
   })
 
