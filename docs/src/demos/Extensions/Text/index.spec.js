297981c5d84c60e40fb71f3d47f423c419aef203
Hans Pagel <hans.pagel@ueber.io>
2020-09-15 17:31:43 +0200

    add a test to the text extension page

--- docs/src/demos/Extensions/Text/index.spec.js
+++ docs/src/demos/Extensions/Text/index.spec.js
@@ -1,5 +1,18 @@
-context('/api/extensions/document', () => {
+context('/api/extensions/text', () => {
   before(() => {
-    cy.visit('/api/extensions/document')
+    cy.visit('/api/extensions/text')
+  })
+
+  beforeEach(() => {
+    cy.get('.ProseMirror').then(([{ editor }]) => {
+      editor.clearContent()
+    })
+  })
+
+  it('text should be wrapped in a paragraph by default', () => {
+    cy.get('.ProseMirror')
+      .type('Example Text')
+      .find('p')
+      .should('contain', 'Example Text')
   })
 })
