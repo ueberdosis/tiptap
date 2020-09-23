59adb2c616f7436b8cc7194278916cc4555e2db7
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-09-23 23:31:06 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  plugins: ['html', 'cypress'],
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+
+  env: {
+    es6: true,
+    node: true,
+    'cypress/globals': true,
+  },
+
+  globals: {
+    document: false,
+    window: false,
+  },
+
+  extends: [
+    'plugin:vue/strongly-recommended',
+    'airbnb-base',
+  ],
+
+  rules: {
+    semi: ['error', 'never'],
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-dynamic-require': 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    'padded-blocks': 'off',
+    'class-methods-use-this': 'off',
+    'global-require': 'off',
+    'func-names': ['error', 'never'],
+    'vue/this-in-template': ['error', 'never'],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
+    }],
+  },
+}
