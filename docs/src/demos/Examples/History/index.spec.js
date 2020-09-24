e283ec4399e4b4b0868e4c5abbe656f8f7802b5c
Hans Pagel <hans.pagel@ueber.io>
2020-09-24 18:29:34 +0200

    fix tests

--- docs/src/demos/Examples/History/index.spec.js
+++ docs/src/demos/Examples/History/index.spec.js
@@ -1,25 +1,31 @@
 context('/examples/history', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('/examples/history')
+  })
+
+  beforeEach(() => {
+    cy.get('.ProseMirror').then(([{ editor }]) => {
+      editor.clearContent()
+    })
   })
 
   it('should not have a mistake', () => {
     cy.get('.ProseMirror').then(() => {
-      cy.get('.ProseMirror h2:first').should('not.contain', 'Mistake')
+      cy.get('.ProseMirror').should('not.contain', 'Mistake')
     })
   })
 
   it('should have a mistake', () => {
     cy.get('.ProseMirror').then(([{ editor }]) => {
       editor.insertText('Mistake')
-      cy.get('.ProseMirror h2:first').should('contain', 'Mistake')
+      cy.get('.ProseMirror').should('contain', 'Mistake')
     })
   })
 
   it('the mistake should be removed again', () => {
     cy.get('.ProseMirror').then(([{ editor }]) => {
       editor.undo()
-      cy.get('.ProseMirror h2:first').should('not.contain', 'Mistake')
+      cy.get('.ProseMirror').should('not.contain', 'Mistake')
     })
   })
 })
