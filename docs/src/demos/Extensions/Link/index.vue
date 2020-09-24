7c2b39353da64fd756fee277e7bc9632b67bbd2e
Hans Pagel <hans.pagel@ueber.io>
2020-09-24 22:26:23 +0200

    add link extension

--- docs/src/demos/Extensions/Link/index.vue
+++ docs/src/demos/Extensions/Link/index.vue
@@ -10,7 +10,7 @@ import { EditorContent } from '@tiptap/vue'
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
-// import Link from '@tiptap/extension-link'
+import Link from '@tiptap/extension-link'
 
 export default {
   components: {
@@ -29,7 +29,7 @@ export default {
         Document(),
         Paragraph(),
         Text(),
-        // Link(),
+        Link(),
       ],
       content: `
         <p>
