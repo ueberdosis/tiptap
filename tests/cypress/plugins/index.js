de05deb16973f167293aa863b57ba6527039098e
Hans Pagel <hans.pagel@ueber.io>
2020-09-30 17:35:41 +0200

    whatever, doesn’t work

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -11,8 +11,6 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/// <reference types="cypress" />
-
 // eslint-disable-next-line
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
