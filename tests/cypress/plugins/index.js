70fc578cfdc204a5eb1d5430bdff1fea874e46e1
Hans Pagel <hans.pagel@ueber.io>
2020-09-30 17:34:05 +0200

    fix cypress types for VS code

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -11,6 +11,8 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+/// <reference types="cypress" />
+
 // eslint-disable-next-line
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
