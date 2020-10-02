b6a06c071fb2057c9767f25574961501c56b0673
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-10-02 21:57:46 +0200

    refactoring

--- packages/core/src/Editor.ts
+++ packages/core/src/Editor.ts
@@ -122,14 +122,7 @@ export class Editor extends EventEmitter {
     this.registerCommands(coreCommands)
     this.injectCSS()
 
-    // TODO: The autoFocus option has no impact
-    // Doesn’t work
-    this.proxy.focus(this.options.autoFocus)
-    // Does work
-    // window.setTimeout(() => {
-    //   console.log(this.options.autoFocus)
-    //   this.proxy.focus(this.options.autoFocus)
-    // }, 100)
+    window.setTimeout(() => this.proxy.focus(this.options.autoFocus), 0)
   }
 
   /**
