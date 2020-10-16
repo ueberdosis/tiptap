e549f3954fa668536c5cedd878297550002027d3
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-10-16 09:28:14 +0200

    improve scrolling again

--- docs/src/main.js
+++ docs/src/main.js
@@ -5,30 +5,9 @@ import 'prismjs/components/prism-scss.js'
 import PortalVue from 'portal-vue'
 import App from '~/layouts/App'
 
-export default function (Vue, { router }) {
+export default function (Vue) {
   Vue.use(PortalVue)
   Vue.component('Layout', App)
   Vue.component('Demo', () => import(/* webpackChunkName: "demo" */ '~/components/Demo'))
   Vue.component('LiveDemo', () => import(/* webpackChunkName: "live-demo" */ '~/components/LiveDemo'))
-
-  router.options.scrollBehavior = async (to, from, savedPosition) => {
-    if (to.hash) {
-      const elem = document.querySelector(to.hash)
-
-      if (elem) {
-        const offset = parseFloat(getComputedStyle(elem).scrollMarginTop)
-        return {
-          selector: to.hash,
-          offset: { y: offset },
-        }
-      }
-    }
-
-    if (savedPosition) {
-      return savedPosition
-    }
-
-    return { x: 0, y: 0 }
-  }
-
 }
