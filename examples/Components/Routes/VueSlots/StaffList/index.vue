f298d82372b36aeb50b8f0b05db029b78b21be0c
Nathan <nathan@precedence.com.au>
2020-10-22 21:07:40 +0000

    Add basic stafflist pending all other nodes

---
+++ examples/Components/Routes/VueSlots/StaffList/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="stafflist">
+    <div class="stafflist__content">
+      <slot>
+        <h3>Our Team</h3>
+        <p>Meet them all.</p>
+      </slot>
+    </div>
+    <div class="stafflist__itemsregion">
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Node from '../../../../../packages/tiptap/src/Utils/Node'
+import toggleWrap from '../../../../../packages/tiptap-commands/src/commands/toggleWrap'
+
+const component = {}
+
+const Extension = class extends Node {
+  get name() {
+    return 'stafflist'
+  }
+
+  get schema() {
+    return {
+      content: 'defaultblockslot staffmemberslot',
+      group: 'block',
+      defining: true,
+      selectable: true,
+      draggable: true,
+      parseDOM: [{
+        tag: 'staff-list',
+      }],
+      toDOM: () => ['staff-list', 0],
+    }
+  }
+
+  get view() {
+    return component
+  }
+
+  commands({ type }) {
+    return () => toggleWrap(type)
+  }
+}
+
+component.Extension = Extension
+
+export default component
+
+</script>
--- examples/Components/Routes/VueSlots/index.vue
+++ examples/Components/Routes/VueSlots/index.vue
@@ -5,129 +5,19 @@
 
         <button
           class="menubar__button"
-          :class="{ 'is-active': isActive.bold() }"
-          @click="commands.bold"
+          :class="{ 'is-active': isActive.stafflist() }"
+          @click="commands.stafflist"
         >
           <icon name="bold" />
         </button>
 
         <button
           class="menubar__button"
-          :class="{ 'is-active': isActive.italic() }"
-          @click="commands.italic"
+          :class="{ 'is-active': isActive.staffmember() }"
+          @click="commands.staffmember"
         >
           <icon name="italic" />
         </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.strike() }"
-          @click="commands.strike"
-        >
-          <icon name="strike" />
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.underline() }"
-          @click="commands.underline"
-        >
-          <icon name="underline" />
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.code() }"
-          @click="commands.code"
-        >
-          <icon name="code" />
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.paragraph() }"
-          @click="commands.paragraph"
-        >
-          <icon name="paragraph" />
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.heading({ level: 1 }) }"
-          @click="commands.heading({ level: 1 })"
-        >
-          H1
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-          @click="commands.heading({ level: 2 })"
-        >
-          H2
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-          @click="commands.heading({ level: 3 })"
-        >
-          H3
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.bullet_list() }"
-          @click="commands.bullet_list"
-        >
-          <icon name="ul" />
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.ordered_list() }"
-          @click="commands.ordered_list"
-        >
-          <icon name="ol" />
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.blockquote() }"
-          @click="commands.blockquote"
-        >
-          <icon name="quote" />
-        </button>
-
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.code_block() }"
-          @click="commands.code_block"
-        >
-          <icon name="code" />
-        </button>
-
-        <button
-          class="menubar__button"
-          @click="commands.horizontal_rule"
-        >
-          <icon name="hr" />
-        </button>
-
-        <button
-          class="menubar__button"
-          @click="commands.undo"
-        >
-          <icon name="undo" />
-        </button>
-
-        <button
-          class="menubar__button"
-          @click="commands.redo"
-        >
-          <icon name="redo" />
-        </button>
-
       </div>
     </editor-menu-bar>
 
@@ -138,25 +28,7 @@
 <script>
 import Icon from 'Components/Icon'
 import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
-import {
-  Blockquote,
-  CodeBlock,
-  HardBreak,
-  Heading,
-  HorizontalRule,
-  OrderedList,
-  BulletList,
-  ListItem,
-  TodoItem,
-  TodoList,
-  Bold,
-  Code,
-  Italic,
-  Link,
-  Strike,
-  Underline,
-  History,
-} from 'tiptap-extensions'
+import StaffList from './StaffList'
 
 export default {
   components: {
@@ -168,45 +40,10 @@ export default {
     return {
       editor: new Editor({
         extensions: [
-          new Blockquote(),
-          new BulletList(),
-          new CodeBlock(),
-          new HardBreak(),
-          new Heading({ levels: [1, 2, 3] }),
-          new HorizontalRule(),
-          new ListItem(),
-          new OrderedList(),
-          new TodoItem(),
-          new TodoList(),
-          new Link(),
-          new Bold(),
-          new Code(),
-          new Italic(),
-          new Strike(),
-          new Underline(),
-          new History(),
+          new StaffList.Extension(),
         ],
         content: `
-          <h2>
-            Hi there,
-          </h2>
-          <p>
-            this is a very <em>basic</em> example of tiptap.
-          </p>
-          <pre><code>body { display: none; }</code></pre>
-          <ul>
-            <li>
-              A regular list
-            </li>
-            <li>
-              With regular items
-            </li>
-          </ul>
-          <blockquote>
-            It's amazing 👏
-            <br />
-            – mom
-          </blockquote>
+          <p>Insert a staff list</p>
         `,
       }),
     }
