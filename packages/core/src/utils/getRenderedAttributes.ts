c85844ae11940a041d9c3cb1a91ccfcba43163b8
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-10-24 13:48:41 +0200

    add fallback renderer for attributes

--- packages/core/src/utils/getRenderedAttributes.ts
+++ packages/core/src/utils/getRenderedAttributes.ts
@@ -1,13 +1,21 @@
 import { Node, Mark } from 'prosemirror-model'
 import { ExtensionAttribute } from '../types'
 import mergeAttributes from './mergeAttributes'
+import isEmptyObject from './isEmptyObject'
 
 export default function getRenderedAttributes(nodeOrMark: Node | Mark, extensionAttributes: ExtensionAttribute[]): { [key: string]: any } {
   return extensionAttributes
     .filter(item => item.attribute.rendered)
     .map(item => {
-      // TODO: fallback if renderHTML doesn’t exist
-      return item.attribute.renderHTML(nodeOrMark.attrs)
+      const renderedAttributes = item.attribute.renderHTML(nodeOrMark.attrs)
+
+      if (isEmptyObject(renderedAttributes)) {
+        return {
+          [`data-${item.name}`]: nodeOrMark.attrs[item.name],
+        }
+      }
+
+      return renderedAttributes
     })
     .reduce((attributes, attribute) => {
       return mergeAttributes(attributes, attribute)
