bfb52ebfa637071e385491745e0156648839847b
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-10-27 22:41:19 +0100

    update image content

--- docs/src/demos/Extensions/Image/index.vue
+++ docs/src/demos/Extensions/Image/index.vue
@@ -43,8 +43,9 @@ export default {
         Image(),
       ],
       content: `
-        <p>This is basic example of implementing images. Try to drop new images here. Reordering also works.</p>
+        <p>This is a basic example of implementing images. Drag to re-order.</p>
         <img src="https://source.unsplash.com/8xznAGy4HcY/800x600" />
+        <img src="https://source.unsplash.com/K9QHL52rE2k/800x600" />
       `,
     })
   },
