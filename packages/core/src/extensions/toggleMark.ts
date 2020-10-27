59dbe41620bb649acfa6fc85343356010e75af76
Hans Pagel <hans.pagel@ueber.io>
2020-10-27 20:36:29 +0100

    add todo

--- packages/core/src/extensions/toggleMark.ts
+++ packages/core/src/extensions/toggleMark.ts
@@ -3,12 +3,24 @@ import { MarkType } from 'prosemirror-model'
 import { Command } from '../Editor'
 import { createExtension } from '../Extension'
 import getMarkType from '../utils/getMarkType'
+import markIsActive from '../utils/markIsActive'
 
 export const ToggleMark = createExtension({
   addCommands() {
     return {
       toggleMark: (typeOrName: string | MarkType): Command => ({ state, dispatch }) => {
         const type = getMarkType(typeOrName, state.schema)
+
+        /* TODO:
+        const hasMarkWithDifferentAttributes = attrs
+          && markIsActive(state, type)
+          && !markIsActive(state, type, attrs)
+
+        if (hasMarkWithDifferentAttributes) {
+          // @ts-ignore
+          return commands.updateMark(type, attrs)
+        }
+        */
 
         return originalToggleMark(type)(state, dispatch)
       },
