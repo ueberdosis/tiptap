723997439b0d7ee440932c8fbc48fd7978e3d9ab
Joost-dm <58904826+Joost-dm@users.noreply.github.com>
2020-11-02 19:13:55 +0300

    Adding a supporting of the "type" attribute to OrderedList
    Adding a supporting of the "type" attribute to OrderedList

--- packages/tiptap-extensions/src/nodes/OrderedList.js
+++ packages/tiptap-extensions/src/nodes/OrderedList.js
@@ -7,26 +7,37 @@ export default class OrderedList extends Node {
     return 'ordered_list'
   }
 
-  get schema() {
-    return {
-      attrs: {
-        order: {
-          default: 1,
-        },
-      },
-      content: 'list_item+',
-      group: 'block',
-      parseDOM: [
-        {
-          tag: 'ol',
-          getAttrs: dom => ({
-            order: dom.hasAttribute('start') ? +dom.getAttribute('start') : 1,
-          }),
-        },
-      ],
-      toDOM: node => (node.attrs.order === 1 ? ['ol', 0] : ['ol', { start: node.attrs.order }, 0]),
+ get schema() {
+        return {
+            attrs: {
+                order: {
+                    default: 1
+                },
+                type: {
+                    default: 1
+                }
+            },
+            content: 'list_item+',
+            group: 'block',
+            parseDOM: [{
+                tag: 'ol',
+                getAttrs: dom => ({
+                    order: dom.hasAttribute('start') ? +dom.getAttribute('start') : 1,
+                    type: dom.getAttribute('type')
+                })
+            }],
+            toDOM: (node) => {
+                let attrs = {}
+                if (node.attrs.order !== 1) {
+                    attrs.start = node.attrs.order
+                }
+                if (node.attrs.type !== 1) {
+                    attrs.type = node.attrs.type
+                }
+                return ['ol', attrs, 0]
+            }
+        }
     }
-  }
 
   commands({ type, schema }) {
     return () => toggleList(type, schema.nodes.list_item)
