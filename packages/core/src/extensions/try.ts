1d3de73f82e236909b5eb7154c200a7713c10fe8
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-11-04 15:40:32 +0100

    refactoring

--- packages/core/src/extensions/try.ts
+++ packages/core/src/extensions/try.ts
@@ -4,11 +4,13 @@ import { createExtension } from '../Extension'
 export const Try = createExtension({
   addCommands() {
     return {
-      try: (fn: (props: Parameters<Command>[0]) => Command[]): Command => props => {
-        const commands = fn(props)
+      try: (commands: Command[] | ((props: Parameters<Command>[0]) => Command[])): Command => props => {
+        const items = typeof commands === 'function'
+          ? commands(props)
+          : commands
 
-        for (let i = 0; i < commands.length; i += 1) {
-          if (commands[i](props)) {
+        for (let i = 0; i < items.length; i += 1) {
+          if (items[i](props)) {
             return true
           }
         }
