0ba95a7ae3e12c079aef22c23842a19fdade72b9
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-11-04 12:40:13 +0100

    use splitBlockKeepMarks by default, disable weird selectAll

--- packages/core/src/extensions/baseKeymap.ts
+++ packages/core/src/extensions/baseKeymap.ts
@@ -1,9 +1,51 @@
-import { baseKeymap } from 'prosemirror-commands'
+import {
+  chainCommands,
+  newlineInCode,
+  createParagraphNear,
+  liftEmptyBlock,
+  splitBlockKeepMarks,
+  exitCode,
+  deleteSelection,
+  joinForward,
+  joinBackward,
+  selectNodeForward,
+  selectNodeBackward,
+} from 'prosemirror-commands'
+import { undoInputRule } from 'prosemirror-inputrules'
 import { createExtension } from '../Extension'
 
 export const BaseKeymap = createExtension({
   addKeyboardShortcuts() {
-    return baseKeymap
+    const enter = chainCommands(
+      newlineInCode,
+      createParagraphNear,
+      liftEmptyBlock,
+      splitBlockKeepMarks,
+    )
+
+    const backspace = chainCommands(
+      undoInputRule,
+      deleteSelection,
+      joinBackward,
+      selectNodeBackward,
+    )
+
+    const del = chainCommands(
+      deleteSelection,
+      joinForward,
+      selectNodeForward,
+    )
+
+    return {
+      Enter: enter,
+      'Mod-Enter': exitCode,
+      Backspace: backspace,
+      'Mod-Backspace': backspace,
+      Delete: del,
+      'Mod-Delete': del,
+      // we don’t need a custom `selectAll` for now
+      // 'Mod-a': () => this.editor.selectAll(),
+    }
   },
 })
 
