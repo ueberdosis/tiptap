da1aaec025ec2fc45121f07a819590eb95833c17
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-11-06 14:03:51 +0100

    improve getMarkAttrs when selection is empty

--- packages/core/src/utils/getMarkAttrs.ts
+++ packages/core/src/utils/getMarkAttrs.ts
@@ -5,10 +5,13 @@ export default function getMarkAttrs(state: EditorState, type: MarkType) {
   const { from, to, empty } = state.selection
   let marks: Mark[] = []
 
-  // TODO: -1 only for inclusive marks?
-  state.doc.nodesBetween(empty ? from - 1 : from, to, node => {
-    marks = [...marks, ...node.marks]
-  })
+  if (empty) {
+    marks = state.selection.$head.marks()
+  } else {
+    state.doc.nodesBetween(from, to, node => {
+      marks = [...marks, ...node.marks]
+    })
+  }
 
   const mark = marks.find(markItem => markItem.type.name === type.name)
 
