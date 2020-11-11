dbefbb98fa1b9b4473d1eded8d062b5d98cbc1bd
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-11-12 00:01:18 +0100

    add FullEditor component

---
+++ docs/src/demos/Examples/FullEditor/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <full-editor v-model="content" />
+    <div>
+      {{ content }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { FullEditor } from '@tiptap/vue'
+
+export default {
+  components: {
+    FullEditor,
+  },
+
+  data() {
+    return {
+      content: '<p>full editor</p>',
+    }
+  },
+}
+</script>
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,3 +1,4 @@
 export * from '@tiptap/core'
 export { default as VueRenderer } from './VueRenderer'
 export { default as EditorContent } from './components/EditorContent'
+export { default as FullEditor } from './components/FullEditor.vue'
