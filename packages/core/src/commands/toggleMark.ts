6f8632f64369c3634540b19f3d781a3df91b81b1
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-11-30 21:31:57 +0100

    fix toggleMark

--- packages/core/src/commands/toggleMark.ts
+++ packages/core/src/commands/toggleMark.ts
@@ -1,4 +1,3 @@
-import { toggleMark as originalToggleMark } from 'prosemirror-commands'
 import { MarkType } from 'prosemirror-model'
 import { Command } from '../types'
 import getMarkType from '../helpers/getMarkType'
@@ -7,16 +6,13 @@ import markIsActive from '../helpers/markIsActive'
 /**
  * Toggle a mark on and off.
  */
-export const toggleMark = (typeOrName: string | MarkType, attributes?: {}): Command => ({ state, dispatch, commands }) => {
+export const toggleMark = (typeOrName: string | MarkType, attributes?: {}): Command => ({ state, commands }) => {
   const type = getMarkType(typeOrName, state.schema)
+  const isActive = markIsActive(state, type, attributes)
 
-  const hasMarkWithDifferentAttributes = attributes
-    && markIsActive(state, type)
-    && !markIsActive(state, type, attributes)
-
-  if (attributes && hasMarkWithDifferentAttributes) {
-    return commands.setMark(type, attributes)
+  if (isActive) {
+    return commands.unsetMark(type)
   }
 
-  return originalToggleMark(type, attributes)(state, dispatch)
+  return commands.setMark(type, attributes)
 }
