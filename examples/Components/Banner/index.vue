44c016e4e32b9e8e34c642716052bf64d6b44d9e
Hans Pagel <hans.pagel@ueber.io>
2020-12-10 23:27:18 +0100

    update wording

--- examples/Components/Banner/index.vue
+++ examples/Components/Banner/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="banner">
     <span class="banner__message">
-      We’re about to release <a href="https://blog.ueber.io/post/our-plan-for-tiptap-2/">tiptap 2</a>. Become a sponsor to get early access!
+      We’re working on <a href="https://blog.ueber.io/post/our-plan-for-tiptap-2/">tiptap 2</a>. Become a sponsor to get early access!
     </span>
     <a href="https://github.com/sponsors/ueberdosis" class="banner__action">
       Sponsor 💖
