dd0c1044d0c928f9360b9084485bf1c65eac69e7
Hans Pagel <hans.pagel@ueber.io>
2020-12-10 16:15:01 +0100

    improve responsive view

--- examples/Components/Banner/index.vue
+++ examples/Components/Banner/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="banner">
-    We’re about to release <a href="https://blog.ueber.io/post/our-plan-for-tiptap-2/">tiptap 2</a>. Become a sponsor to get early access!
+    <span class="banner__message">
+      We’re about to release <a href="https://blog.ueber.io/post/our-plan-for-tiptap-2/">tiptap 2</a>. Become a sponsor to get early access!
+    </span>
     <a href="https://github.com/sponsors/ueberdosis" class="banner__action">
       Sponsor 💖
     </a>
