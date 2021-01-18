7adc8b24a00fd9f8ba91c82ee5ba085dcf372275
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-01-18 16:45:49 +0100

    add basic keyboard navigation

--- docs/src/demos/Nodes/Mention/MentionList.vue
+++ docs/src/demos/Nodes/Mention/MentionList.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <div v-for="(item, index) in items" :key="index">
+  <div class="items">
+    <div
+      class="item"
+      :class="{ 'is-selected': index === selectedIndex }"
+      v-for="(item, index) in items"
+      :key="index"
+    >
       {{ item }}
     </div>
   </div>
@@ -14,9 +19,69 @@ export default {
       default: () => [],
     },
   },
+
+  data() {
+    return {
+      selectedIndex: 0,
+    }
+  },
+
+  watch: {
+    items() {
+      this.selectedIndex = 0
+    },
+  },
+
+  methods: {
+    onKeyDown({ event }) {
+      if (event.key === 'ArrowUp') {
+        this.upHandler()
+        return true
+      }
+
+      if (event.key === 'ArrowDown') {
+        this.downHandler()
+        return true
+      }
+
+      if (event.key === 'Enter') {
+        this.enterHandler()
+        return true
+      }
+
+      return false
+    },
+
+    upHandler() {
+      this.selectedIndex = ((this.selectedIndex + this.items.length) - 1) % this.items.length
+    },
+
+    downHandler() {
+      this.selectedIndex = (this.selectedIndex + 1) % this.items.length
+    },
+
+    enterHandler() {
+      const item = this.items[this.selectedIndex]
+
+      if (item) {
+        console.log('select', item)
+      }
+    },
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.items {
+  position: relative;
+}
+.item {
+  background: white;
+  color: black;
+  padding: 0.5rem;
 
+  &.is-selected {
+    background: #ccc;
+  }
+}
 </style>
