1880b43e37e57414fa8a4de3a922df6ba79df5e2
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-01-20 20:17:17 +0100

    add backspace handler to mentions

--- packages/extension-mention/src/mention.ts
+++ packages/extension-mention/src/mention.ts
@@ -72,6 +72,31 @@ export const Mention = Node.create({
     return `@${node.attrs.id}`
   },
 
+  addKeyboardShortcuts() {
+    return {
+      Backspace: () => this.editor.commands.command(({ tr, state }) => {
+        let isMention = false
+        const { selection } = state
+        const { empty, anchor } = selection
+
+        if (!empty) {
+          return false
+        }
+
+        state.doc.nodesBetween(anchor - 1, anchor, (node, pos) => {
+          if (node.type.name === 'mention') {
+            isMention = true
+            tr.insertText(this.options.suggestion.char || '', pos, pos + node.nodeSize)
+
+            return false
+          }
+        })
+
+        return isMention
+      }),
+    }
+  },
+
   addProseMirrorPlugins() {
     return [
       Suggestion({
