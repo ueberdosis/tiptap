dba0c1ac15e9beda3ebd027c67c969a4fe7ae7c7
Hans Pagel <hans.pagel@ueber.io>
2021-01-20 15:10:30 +0100

    fix: don’t merge classes

--- packages/core/src/utilities/isObject.ts
+++ packages/core/src/utilities/isObject.ts
@@ -1,3 +1,19 @@
 export default function isObject(item: any): boolean {
-  return (item && typeof item === 'object' && !Array.isArray(item))
+  if (!item) {
+    return false
+  }
+
+  if (typeof item !== 'object') {
+    return false
+  }
+
+  if (Array.isArray(item)) {
+    return false
+  }
+
+  if (item.constructor?.toString().substring(0, 5) === 'class') {
+    return false
+  }
+
+  return true
 }
