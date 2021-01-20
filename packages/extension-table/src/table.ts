4fc66747108dcae932fe5514e0b2da827dc951e0
Hans Pagel <hans.pagel@ueber.io>
2021-01-20 23:53:53 +0100

    add schema

--- packages/extension-table/src/table.ts
+++ packages/extension-table/src/table.ts
@@ -1,7 +1,33 @@
-import { Node } from '@tiptap/core'
+import { Node, mergeAttributes } from '@tiptap/core'
+
+export interface TableOptions {
+  HTMLAttributes: {
+    [key: string]: any
+  },
+}
 
 export const Table = Node.create({
   name: 'table',
+
+  defaultOptions: <TableOptions>{
+    HTMLAttributes: {},
+  },
+
+  content: 'tableRow+',
+
+  // tableRole: 'table',
+
+  isolating: true,
+
+  group: 'block',
+
+  parseHTML() {
+    return [{ tag: 'table' }]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['table', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), ['tbody', 0]]
+  },
 })
 
 declare module '@tiptap/core' {
