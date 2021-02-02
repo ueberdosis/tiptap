14bcf806594617dcf75bbe0e6437de7a848f85f4
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-02-02 23:28:17 +0100

    fix react renderer

--- docs/src/templates/DemoPage/index.vue
+++ docs/src/templates/DemoPage/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="demo-page">
-    <demo-content :name="$context.name" />
+    <demo-content :name="$context.name" v-bind="props" />
   </div>
 </template>
 
@@ -28,6 +28,40 @@ export default {
     return {
       resizeObserver: null,
     }
+  },
+
+  methods: {
+    fromString(value) {
+      if (typeof value !== 'string') {
+        return value
+      }
+
+      if (value.match(/^\d*(\.\d+)?$/)) {
+        return Number(value)
+      }
+
+      if (value === 'true') {
+        return true
+      }
+
+      if (value === 'false') {
+        return false
+      }
+
+      if (value === 'null') {
+        return null
+      }
+
+      return value
+    },
+  },
+
+  computed: {
+    props() {
+      return Object.fromEntries(Object
+        .entries(this.$route.query)
+        .map(([key, value]) => [key, this.fromString(value)]))
+    },
   },
 
   mounted() {
