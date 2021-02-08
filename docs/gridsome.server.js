93a5f8325405b419325e312b4705fcae7928c21a
Hans Pagel <hans.pagel@ueber.io>
2021-02-08 16:09:49 +0100

    docs: refactor the open graph image generation

--- docs/gridsome.server.js
+++ docs/gridsome.server.js
@@ -1,44 +1,8 @@
-const fs = require('fs')
-const { createCanvas, registerFont } = require('canvas')
 const path = require('path')
 const globby = require('globby')
+const { createDefaultOpenGraphImage, createSpecificOpenGraphImage } = require('./utilities/opengraph-images')
 
-registerFont('fonts/Inter-Regular.otf', { family: 'InterRegular' })
-registerFont('fonts/Inter-Medium.otf', { family: 'InterMedium' })
-
-const wrapText = function (context, text, x, y, maxWidth, lineHeight) {
-  const words = text.split(' ')
-  let line = ''
-
-  for (let n = 0; n < words.length; n += 1) {
-    const testLine = `${line + words[n]} `
-    const metrics = context.measureText(testLine)
-    const testWidth = metrics.width
-    if (testWidth > maxWidth && n > 0) {
-      context.fillText(line, x, y)
-      line = `${words[n]} `
-      y += lineHeight
-    } else {
-      line = testLine
-    }
-  }
-  context.fillText(line, x, y)
-}
-
-const calculateReadingTime = function (text) {
-  const wordsPerMinute = 200
-  const textLength = text.split(' ').length
-
-  if (textLength > 0) {
-    const value = Math.ceil(textLength / wordsPerMinute)
-
-    if (value === 1) {
-      return `${value} minute`
-    }
-
-    return `${value} minutes`
-  }
-}
+createDefaultOpenGraphImage('The headless editor framework for web artisans.', 'static/images/og-image.png')
 
 module.exports = function (api) {
 
@@ -111,64 +75,9 @@ module.exports = function (api) {
       })
   })
 
-  // Generate OpenGraph images for all pages
   api.onCreateNode(options => {
-    if (process.env.NODE_ENV !== 'production') {
-      return
+    if (/* process.env.NODE_ENV === 'production' && */options.internal.typeName === 'DocPage') {
+      createSpecificOpenGraphImage(options.title, options.content, `static/images${options.path}og-image.png`)
     }
-
-    if (options.internal.typeName !== 'DocPage') {
-      return
-    }
-
-    const imagePath = `static/images${options.path}`
-    const imageFile = `static/images${options.path}og-image.png`
-
-    // console.log(`Found Post “${options.title}” in ${options.internal.origin} …`)
-
-    const width = 1200
-    const height = 630
-    const border = 40
-    const canvas = createCanvas(width, height)
-    const context = canvas.getContext('2d')
-
-    // background
-    context.fillStyle = '#000000'
-    context.fillRect(0, 0, width, height)
-
-    // project
-    const project = 'tiptap documentation'
-    context.textBaseline = 'top'
-    context.fillStyle = '#666666'
-    context.font = '32pt InterRegular'
-    context.fillText(project, border, border)
-
-    // title
-    const { title } = options
-    const lineHeight = 96
-    context.textBaseline = 'top'
-    context.fillStyle = '#ffffff'
-    context.font = '58pt InterMedium'
-    wrapText(context, title, border, border + 60 + border, width - border - border, lineHeight)
-
-    // reading time
-    const readingTime = calculateReadingTime(options.content)
-    context.textBaseline = 'bottom'
-    context.fillStyle = '#666666'
-    context.font = '32pt InterRegular'
-    context.fillText(readingTime, border, height - border)
-
-    // store
-    const buffer = canvas.toBuffer('image/png')
-
-    fs.mkdir(imagePath, { recursive: true }, error => {
-      if (error) {
-        throw error
-      }
-
-      fs.writeFileSync(imageFile, buffer)
-
-      // console.log(`OpenGraph image generated (${imageFile}).`)
-    })
   })
 }
