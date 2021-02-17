501d540c2dcdda0ec48c9bfd112c806006efbb49
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-02-17 23:30:40 +0100

    move selectionToInsertionEnd

--- packages/core/src/commands/insertHTML.ts
+++ packages/core/src/commands/insertHTML.ts
@@ -1,21 +1,7 @@
 import { DOMParser } from 'prosemirror-model'
-import { Selection, Transaction } from 'prosemirror-state'
-import { ReplaceStep, ReplaceAroundStep } from 'prosemirror-transform'
 import elementFromString from '../utilities/elementFromString'
+import selectionToInsertionEnd from '../helpers/selectionToInsertionEnd'
 import { Command, RawCommands } from '../types'
-
-// TODO: move to utils
-// https://github.com/ProseMirror/prosemirror-state/blob/master/src/selection.js#L466
-function selectionToInsertionEnd(tr: Transaction, startLen: number, bias: number) {
-  const last = tr.steps.length - 1
-  if (last < startLen) return
-  const step = tr.steps[last]
-  if (!(step instanceof ReplaceStep || step instanceof ReplaceAroundStep)) return
-  const map = tr.mapping.maps[last]
-  let end = 0
-  map.forEach((_from, _to, _newFrom, newTo) => { if (end === 0) end = newTo })
-  tr.setSelection(Selection.near(tr.doc.resolve(end as unknown as number), bias))
-}
 
 declare module '@tiptap/core' {
   interface Commands {
