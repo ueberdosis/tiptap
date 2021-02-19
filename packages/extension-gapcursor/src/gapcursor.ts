46501ee2266f0cdc170fedfd44ae55e7216c0d21
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-02-19 11:17:27 +0100

    add allowGapCursor option

--- packages/extension-gapcursor/src/gapcursor.ts
+++ packages/extension-gapcursor/src/gapcursor.ts
@@ -1,5 +1,14 @@
-import { Extension } from '@tiptap/core'
+import { Extension, callOrReturn } from '@tiptap/core'
 import { gapCursor } from 'prosemirror-gapcursor'
+
+declare module '@tiptap/core' {
+  interface NodeConfig<Options> {
+    /**
+     * Allow gap cursor
+     */
+    allowGapCursor?: boolean | ((this: { options: Options }) => boolean),
+  }
+}
 
 export const Gapcursor = Extension.create({
   name: 'gapCursor',
@@ -8,5 +17,13 @@ export const Gapcursor = Extension.create({
     return [
       gapCursor(),
     ]
+  },
+
+  extendNodeSchema(extension) {
+    const context = { options: extension.options }
+
+    return {
+      allowGapCursor: callOrReturn(extension.config.allowGapCursor, context) ?? null,
+    }
   },
 })
