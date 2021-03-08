789a1d55517b9c25893d90f5d05a9b48a4996ac2
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-03-08 23:33:19 +0100

    add type

--- packages/react/src/Editor.ts
+++ packages/react/src/Editor.ts
@@ -1,5 +1,6 @@
+import React from 'react'
 import { Editor as CoreEditor } from '@tiptap/core'
 
 export class Editor extends CoreEditor {
-  public contentComponent: any | null = null
+  public contentComponent: React.Component | null = null
 }
