ead5f77d0855e9462043ed7320cd5592f9610eac
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-03-15 09:05:13 +0100

    create ReactPlayground example

--- docs/src/demos/Examples/Default/React/index.jsx
+++ docs/src/demos/Examples/Default/React/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import tippy from 'tippy.js'
-import { useEditor, EditorContent, ReactRenderer, ReactNodeViewRenderer, NodeViewWrapper, NodeViewContent } from '@tiptap/react'
+import { useEditor, EditorContent } from '@tiptap/react'
 import { defaultExtensions } from '@tiptap/starter-kit'
-import Heading from '@tiptap/extension-heading'
-import Paragraph from '@tiptap/extension-paragraph'
-import Mention from '@tiptap/extension-mention'
 import './styles.scss'
-import { render } from 'react-dom'
 
 const MenuBar = ({ editor }) => {
   if (!editor) {
@@ -127,162 +122,45 @@ const MenuBar = ({ editor }) => {
   )
 }
 
-const MentionList = (props) => {
-  console.log({props})
-
-  return (
-    <div>
-      mentions
-      {props.items.map((item) => (
-        <div>
-          {item}
-        </div>
-      ))}
-    </div>
-  )
-}
-
-class MentionList2 extends React.Component {
-
-  onKeyDown(props) {
-    console.log('onKeyDown', props)
-  }
-
-  render() {
-    return (
-      <div>
-        mentions
-        {this.props.items.map((item, index) => (
-          <div key={index}>
-            {item}
-          </div>
-        ))}
-      </div>
-    )
-  }
-}
-
 export default () => {
   const editor = useEditor({
-    // onTransaction({ editor }) {
-    //   console.log('anchor', editor.state.selection.anchor)
-    // },
     extensions: [
-      ...defaultExtensions().filter(item => item.config.name !== 'heading'),
-      Heading.extend({
-        draggable: true,
-        addNodeView() {
-          return ReactNodeViewRenderer((props) => {
-            return (
-              <NodeViewWrapper>
-                <div className="heading">
-                  <span
-                    data-drag-handle
-                    contentEditable={false}
-                    draggable={true}
-                    suppressContentEditableWarning={true}
-                  >⠿</span>
-                  level: {props.node.attrs.level}
-                  <button onClick={() => props.updateAttributes({ level: 1 })}>
-                    set level 1
-                  </button>
-                  <NodeViewContent />
-                </div>
-              </NodeViewWrapper>
-            )
-          })
-        }
-      }),
-      Mention.configure({
-        suggestion: {
-          items: query => {
-            return [
-              'Lea Thompson', 'Cyndi Lauper', 'Tom Cruise', 'Madonna', 'Jerry Hall', 'Joan Collins', 'Winona Ryder', 'Christina Applegate', 'Alyssa Milano', 'Molly Ringwald', 'Ally Sheedy', 'Debbie Harry', 'Olivia Newton-John', 'Elton John', 'Michael J. Fox', 'Axl Rose', 'Emilio Estevez', 'Ralph Macchio', 'Rob Lowe', 'Jennifer Grey', 'Mickey Rourke', 'John Cusack', 'Matthew Broderick', 'Justine Bateman', 'Lisa Bonet',
-            ].filter(item => item.toLowerCase().startsWith(query.toLowerCase())).slice(0, 10)
-          },
-          render: () => {
-            let reactRenderer
-            let popup
-
-            return {
-              onStart: props => {
-                reactRenderer = new ReactRenderer(MentionList2, {
-                  props,
-                  editor: props.editor,
-                })
-
-                popup = tippy('body', {
-                  getReferenceClientRect: props.clientRect,
-                  appendTo: () => document.body,
-                  content: reactRenderer.element,
-                  showOnCreate: true,
-                  interactive: true,
-                  trigger: 'manual',
-                  placement: 'bottom-start',
-                })
-              },
-              onUpdate(props) {
-                reactRenderer.updateProps(props)
-
-                popup[0].setProps({
-                  getReferenceClientRect: props.clientRect,
-                })
-              },
-              onKeyDown(props) {
-                return reactRenderer.ref.onKeyDown(props)
-              },
-              onExit() {
-                popup[0].destroy()
-                reactRenderer.destroy()
-              },
-            }
-          }
-        },
-      })
+      ...defaultExtensions(),
     ],
     content: `
-      <h1>heading</h1>
-      <h2>heading</h2>
-      <p>paragraph</p>
+      <h2>
+        Hi there,
+      </h2>
+      <p>
+        this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+      </p>
+      <ul>
+        <li>
+          That’s a bullet list with one …
+        </li>
+        <li>
+          … or two list items.
+        </li>
+      </ul>
+      <p>
+        Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+      </p>
+      <pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+      <p>
+        I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+      </p>
+      <blockquote>
+        Wow, that’s amazing. Good work, boy! 👏
+        <br />
+        — Mom
+      </blockquote>
     `,
-//     content: `
-//       <h2>
-//         Hi there,
-//       </h2>
-//       <p>
-//         this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-//       </p>
-//       <ul>
-//         <li>
-//           That’s a bullet list with one …
-//         </li>
-//         <li>
-//           … or two list items.
-//         </li>
-//       </ul>
-//       <p>
-//         Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-//       </p>
-//       <pre><code class="language-css">body {
-//   display: none;
-// }</code></pre>
-//       <p>
-//         I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-//       </p>
-//       <blockquote>
-//         Wow, that’s amazing. Good work, boy! 👏
-//         <br />
-//         — Mom
-//       </blockquote>
-//     `,
   })
 
   return (
     <div>
-      <div>
-        <button onClick={() => editor.setEditable(true)}>editable</button>
-        <button onClick={() => editor.setEditable(false)}>readonly</button>
-      </div>
       <MenuBar editor={editor} />
       <EditorContent editor={editor} />
     </div>
