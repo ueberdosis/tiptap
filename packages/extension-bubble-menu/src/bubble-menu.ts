56a037b8e8fdc09d253deefc1863052de3d459f2
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-03-31 16:51:48 +0200

    dont use document in BubbleMenu extension

--- packages/extension-bubble-menu/src/bubble-menu.ts
+++ packages/extension-bubble-menu/src/bubble-menu.ts
@@ -1,17 +1,23 @@
 import { Extension } from '@tiptap/core'
 import { BubbleMenuPlugin, BubbleMenuPluginProps } from './bubble-menu-plugin'
 
-export type BubbleMenuOptions = Omit<BubbleMenuPluginProps, 'editor'>
+export type BubbleMenuOptions = Omit<BubbleMenuPluginProps, 'editor' | 'element'> & {
+  element: HTMLElement | null,
+}
 
 export const BubbleMenu = Extension.create<BubbleMenuOptions>({
   name: 'bubbleMenu',
 
   defaultOptions: {
-    element: document.createElement('div'),
+    element: null,
     keepInBounds: true,
   },
 
   addProseMirrorPlugins() {
+    if (!this.options.element) {
+      return []
+    }
+
     return [
       BubbleMenuPlugin({
         editor: this.editor,
