b1aa7863e88d4650a933062fdd66c4fe5c90d0c5
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-04-17 21:25:04 +0200

    test: add more extension option tests

--- tests/cypress/integration/core/extensionOptions.spec.ts
+++ tests/cypress/integration/core/extensionOptions.spec.ts
@@ -3,13 +3,30 @@
 import { Extension } from '@tiptap/core/src/Extension'
 
 describe('extension options', () => {
-  it('should pass through', () => {
+  it('should set options', () => {
     const extension = Extension.create({
       defaultOptions: {
         foo: 1,
         bar: 1,
       },
     })
+
+    expect(extension.options).to.deep.eq({
+      foo: 1,
+      bar: 1,
+    })
+  })
+
+  it('should pass through', () => {
+    const extension = Extension
+      .create({
+        defaultOptions: {
+          foo: 1,
+          bar: 1,
+        },
+      })
+      .extend()
+      .configure()
 
     expect(extension.options).to.deep.eq({
       foo: 1,
@@ -35,24 +52,44 @@ describe('extension options', () => {
     })
   })
 
-  // TODO: this fails. not sure about it
+  it('should be extendable', () => {
+    const extension = Extension.create({
+      defaultOptions: {
+        foo: 1,
+        bar: 1,
+      },
+    })
 
-  // it('should be extendable', () => {
-  //   const extension = Extension
-  //     .create({
-  //       defaultOptions: {
-  //         foo: 1,
-  //         bar: 1,
-  //       },
-  //     })
-  //     .extend({
-  //       defaultOptions: {
-  //         baz: 1,
-  //       },
-  //     })
+    const newExtension = extension.extend({
+      defaultOptions: {
+        ...extension.options,
+        baz: 1,
+      },
+    })
 
-  //   expect(extension.options).to.deep.eq({
-  //     baz: 1,
-  //   })
-  // })
+    expect(newExtension.options).to.deep.eq({
+      foo: 1,
+      bar: 1,
+      baz: 1,
+    })
+  })
+
+  it('should be overwritable', () => {
+    const extension = Extension
+      .create({
+        defaultOptions: {
+          foo: 1,
+          bar: 1,
+        },
+      })
+      .extend({
+        defaultOptions: {
+          baz: 1,
+        },
+      })
+
+    expect(extension.options).to.deep.eq({
+      baz: 1,
+    })
+  })
 })
