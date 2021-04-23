cfd29fac86e03d72a0c05ec8d26aac905d19c5a2
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-04-23 08:58:03 +0200

    fix: better merge mark attributes for existing marks, fix #1039

--- packages/core/src/commands/updateAttributes.ts
+++ packages/core/src/commands/updateAttributes.ts
@@ -40,7 +40,10 @@ export const updateAttributes: RawCommands['updateAttributes'] = (typeOrName, at
 
   if (dispatch) {
     tr.selection.ranges.forEach(range => {
-      state.doc.nodesBetween(range.$from.pos, range.$to.pos, (node, pos) => {
+      const from = range.$from.pos
+      const to = range.$to.pos
+
+      state.doc.nodesBetween(from, to, (node, pos) => {
         if (nodeType && nodeType === node.type) {
           tr.setNodeMarkup(pos, undefined, {
             ...node.attrs,
@@ -51,7 +54,10 @@ export const updateAttributes: RawCommands['updateAttributes'] = (typeOrName, at
         if (markType && node.marks.length) {
           node.marks.forEach(mark => {
             if (markType === mark.type) {
-              tr.addMark(pos, pos + node.nodeSize, markType.create({
+              const trimmedFrom = Math.max(pos, from)
+              const trimmedTo = Math.min(pos + node.nodeSize, to)
+
+              tr.addMark(trimmedFrom, trimmedTo, markType.create({
                 ...mark.attrs,
                 ...attributes,
               }))
