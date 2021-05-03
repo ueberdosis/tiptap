80cf8bb71b61daffcb5c93070194544a513729c8
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-05-03 09:50:57 +0200

    throw error for missing mark/node type

--- packages/core/src/helpers/getNodeType.ts
+++ packages/core/src/helpers/getNodeType.ts
@@ -2,6 +2,10 @@ import { NodeType, Schema } from 'prosemirror-model'
 
 export default function getNodeType(nameOrType: string | NodeType, schema: Schema): NodeType {
   if (typeof nameOrType === 'string') {
+    if (!schema.nodes[nameOrType]) {
+      throw Error(`There is no node type named '${nameOrType}'. Maybe you forgot to add the extension?`)
+    }
+
     return schema.nodes[nameOrType]
   }
 
