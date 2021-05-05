3b9578b6ba746b162cc6c06c1162c55fe6f99139
Hans Pagel <hans.pagel@ueber.io>
2021-05-05 23:14:57 +0200

    add a test for the core utility

--- tests/cypress/integration/core/generateHTML.spec.ts
+++ tests/cypress/integration/core/generateHTML.spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import { generateHTML } from 'packages/html/src/index'
+import { generateHTML } from '@tiptap/core'
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
