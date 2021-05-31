52dd6e0e64d82530b24a06b367af0066fd937d24
Markus Machatschek <mmachatschek@yahoo.com>
2021-05-31 19:05:46 +0200

    Prevent tiptap from creating duplicate style tags when injecting css

--- packages/core/src/utilities/createStyleTag.ts
+++ packages/core/src/utilities/createStyleTag.ts
@@ -1,6 +1,13 @@
 export default function createStyleTag(style: string): HTMLStyleElement {
+  const tipTapStyleTag = (<HTMLStyleElement>document.querySelector('style[data-tiptap-style]'))
+
+  if (tipTapStyleTag !== null) {
+    return tipTapStyleTag
+  }
+
   const styleNode = document.createElement('style')
 
+  styleNode.setAttribute('data-tiptap-style', '');
   styleNode.innerHTML = style
   document.getElementsByTagName('head')[0].appendChild(styleNode)
 
