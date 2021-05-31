f54b9c9b424ba1e81627fb2d18d316bc442e3a8c
philippkuehn <philippkuehn@users.noreply.github.com>
2021-05-31 17:20:25 +0000

    ci: fix code style linting errors

--- packages/core/src/utilities/createStyleTag.ts
+++ packages/core/src/utilities/createStyleTag.ts
@@ -7,7 +7,7 @@ export default function createStyleTag(style: string): HTMLStyleElement {
 
   const styleNode = document.createElement('style')
 
-  styleNode.setAttribute('data-tiptap-style', '');
+  styleNode.setAttribute('data-tiptap-style', '')
   styleNode.innerHTML = style
   document.getElementsByTagName('head')[0].appendChild(styleNode)
 
