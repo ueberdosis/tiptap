f5febda470c1ed96b96ece67dc4720cc9a81b109
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-06-01 17:48:46 +0200

    fix figure drag

--- docs/src/demos/Experiments/Figure/figure.ts
+++ docs/src/demos/Experiments/Figure/figure.ts
@@ -2,6 +2,7 @@ import {
   Command,
   Node,
   nodeInputRule,
+  mergeAttributes,
 } from '@tiptap/core'
 
 export interface FigureOptions {
@@ -76,7 +77,7 @@ export const Figure = Node.create<FigureOptions>({
   renderHTML({ HTMLAttributes }) {
     return [
       'figure', this.options.HTMLAttributes,
-      ['img', HTMLAttributes],
+      ['img', mergeAttributes(HTMLAttributes, { draggable: false })],
       ['figcaption', 0],
     ]
   },
