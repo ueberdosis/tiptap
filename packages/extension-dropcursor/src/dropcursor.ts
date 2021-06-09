4fdeb944a2e15364606ca48b77d96f797b74f581
shadow-light <42055707+shadow-light@users.noreply.github.com>
2021-06-10 09:56:06 +1000

    Make drop cursor default to regular caret color
    Currently drop cursor defaults to black rather than the text color, which is what normal carets default to. Using `currentcolor` should provide a better default value.

--- packages/extension-dropcursor/src/dropcursor.ts
+++ packages/extension-dropcursor/src/dropcursor.ts
@@ -11,7 +11,7 @@ export const Dropcursor = Extension.create<DropcursorOptions>({
   name: 'dropCursor',
 
   defaultOptions: {
-    color: 'black',
+    color: 'currentcolor',
     width: 1,
     class: null,
   },
