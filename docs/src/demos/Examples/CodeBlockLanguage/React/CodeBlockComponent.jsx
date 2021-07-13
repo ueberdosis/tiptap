056ed53bc168ba72db1ebcdf9a5ed997bd00ddcd
Sven Adlung <info@svenadlung.de>
2021-07-13 22:33:12 +0200

    Docs: Clean up syntax highlighting demo, make use of functional component

--- docs/src/demos/Examples/CodeBlockLanguage/React/CodeBlockComponent.jsx
+++ docs/src/demos/Examples/CodeBlockLanguage/React/CodeBlockComponent.jsx
@@ -2,36 +2,23 @@ import React from 'react'
 import { NodeViewWrapper, NodeViewContent } from '@tiptap/react'
 import './CodeBlockComponent.scss'
 
-export default class CodeBlockComponent extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      selectedLanguage: props.node.attrs.language,
-    }
-  }
-
-  render() {
-    const { selectedLanguage } = this.state
-    const { updateAttributes, extension } = this.props
-
-    return (
-      <NodeViewWrapper className="code-block">
-        <select contentEditable={false} defaultValue={selectedLanguage} onChange={event => updateAttributes({ language: event.target.value })}>
-          <option value="null">
-            auto
-          </option>
-          <option disabled>
-            —
-          </option>
-          {extension.options.lowlight.listLanguages().map((language, index) => (
-            <option value={language} key={index}>
-              {language}
-            </option>
-          ))}
-        </select>
-        <pre><NodeViewContent as="code" /></pre>
-      </NodeViewWrapper>
-    )
-  }
-}
+export default ({ node: { attrs: { language: defaultLanguage } }, updateAttributes, extension }) => (
+  <NodeViewWrapper className="code-block">
+    <select contentEditable={false} defaultValue={defaultLanguage} onChange={event => updateAttributes({ language: event.target.value })}>
+      <option value="null">
+        auto
+      </option>
+      <option disabled>
+        —
+      </option>
+      {extension.options.lowlight.listLanguages().map((lang, index) => (
+        <option key={index} value={lang}>
+          {lang}
+        </option>
+      ))}
+    </select>
+    <pre>
+      <NodeViewContent as="code" />
+    </pre>
+  </NodeViewWrapper>
+)
