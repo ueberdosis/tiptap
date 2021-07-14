97dd95199fa8aa825f08ade98b43c78ec2541528
Hans Pagel <hans.pagel@ueber.io>
2021-07-14 23:51:53 +0200

    history: code style

--- packages/extension-history/src/history.ts
+++ packages/extension-history/src/history.ts
@@ -49,9 +49,11 @@ export const History = Extension.create<HistoryOptions>({
   addKeyboardShortcuts() {
     return {
       'Mod-z': () => this.editor.commands.undo(),
-      'Mod-я': () => this.editor.commands.undo(),
       'Mod-y': () => this.editor.commands.redo(),
       'Shift-Mod-z': () => this.editor.commands.redo(),
+
+      // Russian keyboard layouts
+      'Mod-я': () => this.editor.commands.undo(),
       'Shift-Mod-я': () => this.editor.commands.redo(),
     }
   },
