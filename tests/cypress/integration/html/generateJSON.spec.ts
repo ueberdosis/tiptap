98c7535ac4efdc3d134e113ab5ed0b5d58c5b452
sibiraj-s <dev.sibiraj@outlook.com>
2021-07-20 16:23:19 +0530

    Update hostic-dom to fix style attributes

--- tests/cypress/integration/html/generateJSON.spec.ts
+++ tests/cypress/integration/html/generateJSON.spec.ts
@@ -4,6 +4,7 @@ import { generateJSON } from '@tiptap/html'
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
+import TextAlign from '@tiptap/extension-text-align'
 
 describe('generateJSON', () => {
   it('generate JSON from HTML without an editor instance', () => {
@@ -19,6 +20,32 @@ describe('generateJSON', () => {
       type: 'doc',
       content: [{
         type: 'paragraph',
+        content: [{
+          type: 'text',
+          text: 'Example Text',
+        }],
+      }],
+    }))
+  })
+
+  // issue: https://github.com/ueberdosis/tiptap/issues/1601
+  it('generate JSON with style attributes', () => {
+    const html = '<p style="text-align: center;">Example Text</p>'
+
+    const json = generateJSON(html, [
+      Document,
+      Paragraph,
+      Text,
+      TextAlign.configure({ types: ['paragraph'] })
+    ])
+
+    expect(JSON.stringify(json)).to.eq(JSON.stringify({
+      type: 'doc',
+      content: [{
+        type: 'paragraph',
+        attrs: {
+          textAlign: 'center'
+        },
         content: [{
           type: 'text',
           text: 'Example Text',
