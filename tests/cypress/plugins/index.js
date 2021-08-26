1322f12028aa2c23f0e17be8a6cce1e1506bdf70
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-08-26 16:49:08 +0200

    add @cypress/vite-dev-server

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -11,8 +11,16 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-// eslint-disable-next-line
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+const path = require('path')
+const { startDevServer } = require('@cypress/vite-dev-server')
+
+module.exports = on => {
+  on('dev-server:start', options => {
+    return startDevServer({
+      options,
+      viteConfig: {
+        configFile: path.resolve(__dirname, '../../demos/vite.config.js'),
+      },
+    })
+  })
 }
