14e458ea7d7475a51ac00d6f67509dc769919d0b
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-09-08 09:32:22 +0200

    feat: add extendEmptyMarkRange option to mark commands (#1859)

--- packages/core/src/commands/toggleMark.ts
+++ packages/core/src/commands/toggleMark.ts
@@ -9,17 +9,27 @@ declare module '@tiptap/core' {
       /**
        * Toggle a mark on and off.
        */
-      toggleMark: (typeOrName: string | MarkType, attributes?: Record<string, any>) => ReturnType,
+      toggleMark: (
+        typeOrName: string | MarkType,
+        attributes?: Record<string, any>,
+        options?: {
+          /**
+           * Removes the mark even across the current selection. Defaults to `false`.
+           */
+          extendEmptyMarkRange?: boolean,
+        },
+      ) => ReturnType,
     }
   }
 }
 
-export const toggleMark: RawCommands['toggleMark'] = (typeOrName, attributes = {}) => ({ state, commands }) => {
+export const toggleMark: RawCommands['toggleMark'] = (typeOrName, attributes = {}, options = {}) => ({ state, commands }) => {
+  const { extendEmptyMarkRange = false } = options
   const type = getMarkType(typeOrName, state.schema)
   const isActive = isMarkActive(state, type, attributes)
 
   if (isActive) {
-    return commands.unsetMark(type)
+    return commands.unsetMark(type, { extendEmptyMarkRange })
   }
 
   return commands.setMark(type, attributes)
