2f15a11572e24b699599d0d335e6f1c5a62666ee
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-09-16 14:41:25 +0200

    remove gridsome

--- docs/src/components/AppSection/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section class="app-section">
-    <div class="app-section__inner">
-      <slot />
-    </div>
-  </section>
-</template>
-
-<style lang="scss" src="./style.scss"></style>
