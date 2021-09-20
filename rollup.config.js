a0e5eac9cc6136df8474de0a552f5617afa3131a
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-09-20 09:44:38 +0200

    fix typo

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ async function getSortedPackages(scope, ignore) {
   const filtered = filterPackages(packages, scope, ignore, false)
 
   return batchPackages(filtered)
-    .filter(item => item.name !== '@tiptap/docs')
+    .filter(item => item.name !== '@tiptap/demos')
     .reduce((arr, batch) => arr.concat(batch), [])
 }
 
