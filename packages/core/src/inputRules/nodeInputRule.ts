d9ba907d131648e08a03f378c1c65db092169ca5
Nokola <nikolami@outlook.com>
2021-10-11 10:29:01 -0700

    Undo group node support in nodeInputRule()
    Fixes #2016

--- packages/core/src/inputRules/nodeInputRule.ts
+++ packages/core/src/inputRules/nodeInputRule.ts
@@ -25,23 +25,7 @@ export default function nodeInputRule(config: {
       const start = range.from
       let end = range.to
 
-      if (match[1]) {
-        const offset = match[0].lastIndexOf(match[1])
-        let matchStart = start + offset
-
-        if (matchStart > end) {
-          matchStart = end
-        } else {
-          end = matchStart + match[1].length
-        }
-
-        // insert last typed character
-        const lastChar = match[0][match[0].length - 1]
-        tr.insertText(lastChar, start + match[0].length - 1)
-
-        // insert node from input rule
-        tr.replaceWith(matchStart, end, config.type.create(attributes))
-      } else if (match[0]) {
+      if (match[0]) {
         tr.replaceWith(start, end, config.type.create(attributes))
       }
     },
