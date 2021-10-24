da2d3d4165a825e25b93f8110fc6695469f3aa59
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-10-24 23:29:40 +0200

    demos: update collab rooms

--- demos/src/Examples/CollaborativeEditing/React/index.jsx
+++ demos/src/Examples/CollaborativeEditing/React/index.jsx
@@ -16,7 +16,7 @@ import MenuBar from './MenuBar'
 import './styles.scss'
 
 const colors = ['#958DF1', '#F98181', '#FBBC88', '#FAF594', '#70CFF8', '#94FADB', '#B9F18D']
-const rooms = ['rooms.7', 'rooms.8', 'rooms.9']
+const rooms = ['rooms.10', 'rooms.11', 'rooms.12']
 const names = [
   'Lea Thompson',
   'Cyndi Lauper',
