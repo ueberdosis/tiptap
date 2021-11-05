9055026158935b5910c8bc9b23e4fcc757ac592f
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-11-05 13:09:09 +0100

    fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,11 +53,13 @@ module.exports = {
         'max-len': 'off',
         'vue/one-component-per-file': 'off',
         'vue/this-in-template': ['error', 'never'],
+        'vue/multi-word-component-names': 'off',
         'vue/max-attributes-per-line': ['error', {
-          singleline: 3,
+          singleline: {
+            max: 3,
+          },
           multiline: {
             max: 1,
-            allowFirstLine: false,
           },
         }],
         'vue/singleline-html-element-content-newline': 'off',
