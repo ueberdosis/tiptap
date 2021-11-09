538e91d23c10f80fcdea64b5af6c5d51e4ab0f29
Philipp Kühn <kontakt@philipp-kuehn.com>
2021-11-09 14:17:14 +0100

    add zeed-dom

--- packages/html/src/generateJSON.ts
+++ packages/html/src/generateJSON.ts
@@ -1,11 +1,10 @@
 import { DOMParser } from 'prosemirror-model'
 import { getSchema, Extensions } from '@tiptap/core'
-// @ts-ignore
-import { parseHTML } from 'hostic-dom'
+import { parseHTML } from 'zeed-dom'
 
 export default function generateJSON(html: string, extensions: Extensions): Record<string, any> {
   const schema = getSchema(extensions)
-  const dom = parseHTML(html)
+  const dom = parseHTML(html) as unknown as Node
 
   return DOMParser.fromSchema(schema)
     .parse(dom)
