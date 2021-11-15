0333ca39281b401e8323101b473d90309948c1f8
Sven Adlung <svenadlung@MBP-von-Sven.fritz.box>
2021-11-15 17:19:36 +0100

    Add Savvy React example

---
+++ demos/src/Examples/Savvy/React/findColors.ts
@@ -0,0 +1,28 @@
+import { Decoration, DecorationSet } from 'prosemirror-view'
+import { Node } from 'prosemirror-model'
+
+export default function (doc: Node): DecorationSet {
+  const hexColor = /(#[0-9a-f]{3,6})\b/gi
+  const decorations: Decoration[] = []
+
+  doc.descendants((node, position) => {
+    if (!node.text) {
+      return
+    }
+
+    Array.from(node.text.matchAll(hexColor)).forEach(match => {
+      const color = match[0]
+      const index = match.index || 0
+      const from = position + index
+      const to = from + color.length
+      const decoration = Decoration.inline(from, to, {
+        class: 'color',
+        style: `--color: ${color}`,
+      })
+
+      decorations.push(decoration)
+    })
+  })
+
+  return DecorationSet.create(doc, decorations)
+}
