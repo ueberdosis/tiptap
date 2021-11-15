40624e75005ddadf32c1333fa93042c2983fd1b0
Sven Adlung <svenadlung@MBP-von-Sven.fritz.box>
2021-11-15 17:01:03 +0100

    Add CharacterCount demo for React

---
+++ demos/src/Extensions/CharacterCount/React/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useEditor, EditorContent } from '@tiptap/react'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import CharacterCount from '@tiptap/extension-character-count'
+import './styles.scss'
+
+const limit = 280
+
+export default () => {
+  const editor = useEditor({
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      CharacterCount.configure({
+        limit,
+      }),
+    ],
+    content: `
+        <p>
+          Let‘s make sure people can’t write more than 280 characters. I bet you could build one of the biggest social networks on that idea.
+        </p>
+      `,
+  })
+
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <div>
+      <EditorContent editor={editor} />
+
+      <div className="character-count">
+        {editor.getCharacterCount()}/{limit} characters
+      </div>
+    </div>
+  )
+}
