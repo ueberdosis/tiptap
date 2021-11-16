60fa25c1caaa1faa8b7287f60f0ece7ba73910af
Sven Adlung <svenadlung@MBP-von-Sven.fritz.box>
2021-11-16 13:03:26 +0100

    Add TextAlign demo for React

---
+++ demos/src/Extensions/TextAlign/React/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useEditor, EditorContent } from "@tiptap/react";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Heading from "@tiptap/extension-heading";
+import Text from "@tiptap/extension-text";
+import TextAlign from "@tiptap/extension-text-align";
+
+export default () => {
+  const editor = useEditor({
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      Heading,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+    content: `
+        <h2>Heading</h2>
+        <p style="text-align: center">first paragraph</p>
+        <p style="text-align: right">second paragraph</p>
+      `,
+  });
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("left").run()}
+        className={editor.isActive({ textAlign: "left" }) ? "is-active" : ""}
+      >
+        left
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("center").run()}
+        className={editor.isActive({ textAlign: "center" }) ? "is-active" : ""}
+      >
+        center
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("right").run()}
+        className={editor.isActive({ textAlign: "right" }) ? "is-active" : ""}
+      >
+        right
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign("justify").run()}
+        className={editor.isActive({ textAlign: "justify" }) ? "is-active" : ""}
+      >
+        justify
+      </button>
+      <button onClick={() => editor.chain().focus().unsetTextAlign().run()}>unsetTextAlign</button>
+      <EditorContent editor={editor} />
+    </div>
+  );
+};
