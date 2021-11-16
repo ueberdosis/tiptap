9956c9c51bbc4c12408725fcc868b05ae68a1533
svenadlung <info@svenadlung.de>
2021-11-16 18:26:28 +0100

    Add Mention demo for React

---
+++ demos/src/Nodes/Mention/React/MentionList.jsx
@@ -0,0 +1,65 @@
+import React, {
+  useState, useEffect, forwardRef, useImperativeHandle,
+} from 'react'
+import './MentionList.scss'
+
+export default forwardRef((props, ref) => {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  const selectItem = index => {
+    const item = props.items[index]
+
+    if (item) {
+      props.command({ id: item })
+    }
+  }
+
+  const upHandler = () => {
+    setSelectedIndex((selectedIndex + props.items.length - 1) % props.items.length)
+  }
+
+  const downHandler = () => {
+    setSelectedIndex((selectedIndex + 1) % props.items.length)
+  }
+
+  const enterHandler = () => {
+    selectItem(selectedIndex)
+  }
+
+  useEffect(() => setSelectedIndex(0), [props.items])
+
+  useImperativeHandle(ref, () => ({
+    onKeyDown: ({ event }) => {
+      if (event.key === 'ArrowUp') {
+        upHandler()
+        return true
+      }
+
+      if (event.key === 'ArrowDown') {
+        downHandler()
+        return true
+      }
+
+      if (event.key === 'Enter') {
+        enterHandler()
+        return true
+      }
+
+      return false
+    },
+  }))
+
+  return (
+    <div className="items">
+      {props.items.map((item, index) => (
+        <button
+          className={`item ${index === selectedIndex ? 'is-selected' : ''}`}
+          key={index}
+          onClick={() => selectItem(index)}
+        >
+          {item}
+        </button>
+      ))}
+    </div>
+  )
+})
---
+++ demos/src/Nodes/Mention/React/index.spec.js
@@ -0,0 +1,7 @@
+context('/src/Nodes/Mention/React/', () => {
+  before(() => {
+    cy.visit('/src/Nodes/Mention/React/')
+  })
+
+  // TODO: Write tests
+})
