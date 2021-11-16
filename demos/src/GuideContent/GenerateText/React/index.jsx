d6286e5d5c73938f12ca1048cbb3b6c7a0a7dfd8
svenadlung <info@svenadlung.de>
2021-11-16 20:28:31 +0100

    Add GenerateText demo for React

---
+++ demos/src/GuideContent/GenerateText/React/index.jsx
@@ -0,0 +1,62 @@
+import React, { useMemo } from 'react'
+import { generateText } from '@tiptap/core'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import HardBreak from '@tiptap/extension-hard-break'
+
+const json = {
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'This is a paragraph.',
+        },
+      ],
+    },
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'Here is another paragraph …',
+        },
+        {
+          type: 'hardBreak',
+        },
+        {
+          type: 'text',
+          text: '… with an hard break.',
+        },
+      ],
+    },
+  ],
+}
+
+export default () => {
+  const output = useMemo(() => {
+    return generateText(
+      json,
+      [
+        Document,
+        Paragraph,
+        Text,
+        HardBreak,
+        // other extensions …
+      ],
+      {
+        // define a custom block separator if you want to
+        blockSeparator: '\n\n',
+      },
+    )
+  }, [json])
+
+  return (
+    <pre>
+      <code>{output}</code>
+    </pre>
+  )
+}
