3541f537c21f0cfb46d391714e50b119810793f7
Jan-Niklas Voß <github@jnvoss.de>
2021-11-22 18:13:58 +0100

    fix: replace spread operator with `Array.from`

--- packages/core/src/utilities/findDuplicates.ts
+++ packages/core/src/utilities/findDuplicates.ts
@@ -1,5 +1,5 @@
 export default function findDuplicates(items: any[]): any[] {
   const filtered = items.filter((el, index) => items.indexOf(el) !== index)
 
-  return [...new Set(filtered)]
+  return Array.from(new Set(filtered))
 }
