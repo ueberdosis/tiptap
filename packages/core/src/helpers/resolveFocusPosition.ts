a96a57dd81800d1d03fd76d353f66755893184f6
Philipp Kühn <philippkuehn@MacBook-Pro-von-Philipp.local>
2021-12-03 10:40:56 +0100

    code style

--- packages/core/src/helpers/resolveFocusPosition.ts
+++ packages/core/src/helpers/resolveFocusPosition.ts
@@ -8,10 +8,21 @@ export default function resolveFocusPosition(
   position: FocusPosition = null,
 ): Selection | null {
 
-  if (!position) { return null }
-  if (position === 'start' || position === true) { return Selection.atStart(doc) }
-  if (position === 'end') { return Selection.atEnd(doc) }
-  if (position === 'all') { return TextSelection.create(doc, 0, doc.content.size) }
+  if (!position) {
+    return null
+  }
+
+  if (position === 'start' || position === true) {
+    return Selection.atStart(doc)
+  }
+
+  if (position === 'end') {
+    return Selection.atEnd(doc)
+  }
+
+  if (position === 'all') {
+    return TextSelection.create(doc, 0, doc.content.size)
+  }
 
   // Check if `position` is in bounds of the doc if `position` is a number.
   const minPos = Selection.atStart(doc).from
