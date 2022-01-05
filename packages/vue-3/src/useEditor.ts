3ba3afbea38b9b46b5d164e0d6a374ecd0ef63c6
Philipp Kühn <philippkuehn@MacBook-Pro-von-Philipp.local>
2022-01-05 09:21:54 +0100

    fix: revert type changes for useEditor in vue-3 package

--- packages/vue-3/src/useEditor.ts
+++ packages/vue-3/src/useEditor.ts
@@ -1,15 +1,9 @@
-import {
-  onMounted,
-  onBeforeUnmount,
-  shallowRef,
-  Ref,
-} from 'vue'
+import { onMounted, onBeforeUnmount, shallowRef } from 'vue'
 import { EditorOptions } from '@tiptap/core'
 import { Editor } from './Editor'
 
-// We set a custom return type. Otherwise TypeScript will throw TS4023. Not sure why.
-export const useEditor = (options: Partial<EditorOptions> = {}): Ref<Editor> => {
-  const editor = shallowRef<Editor>() as Ref<Editor>
+export const useEditor = (options: Partial<EditorOptions> = {}) => {
+  const editor = shallowRef<Editor>()
 
   onMounted(() => {
     editor.value = new Editor(options)
