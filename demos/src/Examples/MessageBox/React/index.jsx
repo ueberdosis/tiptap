98f05066645f8a23ce1e3720a97816c99322b9d3
Markus Krause <markus.krause@ueber.io>
2022-01-24 15:04:08 +0100

    Add MessageBox (Community example copy)

---
+++ demos/src/Examples/MessageBox/React/index.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { useEditor, EditorContent } from '@tiptap/react'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import CharacterCount from '@tiptap/extension-character-count'
+import Mention from '@tiptap/extension-mention'
+import suggestion from './suggestion'
+import './styles.scss'
+
+export default () => {
+  const limit = 280
+
+  const editor = useEditor({
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      CharacterCount.configure({
+        limit,
+      }),
+      Mention.configure({
+        HTMLAttributes: {
+          class: 'mention',
+        },
+        suggestion,
+      }),
+    ],
+    content: `
+      <p>
+        What do you all think about the new <span data-type="mention" data-id="Winona Ryder"></span> movie?
+      </p>
+    `,
+  })
+
+  const percentage = editor
+    ? Math.round((100 / limit) * editor.storage.characterCount.characters())
+    : 0
+
+  return (
+    <div className='message-box'>
+      <EditorContent editor={editor} />
+      {editor
+        && <div className={`character-count ${editor.storage.characterCount.characters() === limit ? 'character-count--warning' : ''}`}>
+          <svg
+            height="20"
+            width="20"
+            viewBox="0 0 20 20"
+            className="character-count__graph"
+          >
+            <circle
+              r="10"
+              cx="10"
+              cy="10"
+              fill="#e9ecef"
+            />
+            <circle
+              r="5"
+              cx="10"
+              cy="10"
+              fill="transparent"
+              stroke="currentColor"
+              strokeWidth="10"
+              strokeDasharray={`calc(${percentage} * 31.4 / 100) 31.4`}
+              transform="rotate(-90) translate(-20)"
+            />
+            <circle
+              r="6"
+              cx="10"
+              cy="10"
+              fill="white"
+            />
+          </svg>
+
+          <div className="character-count__text">
+            {editor.storage.characterCount.characters()}/{limit} characters
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
