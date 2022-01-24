9693e6037c44b1c5fecede6d350564ef38267ea2
Markus Krause <markus.krause@ueber.io>
2022-01-24 17:29:57 +0100

    Add first text formatting buttons and placeholder for content

--- demos/src/Examples/MessageBox/React/index.jsx
+++ demos/src/Examples/MessageBox/React/index.jsx
@@ -4,10 +4,42 @@ import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import CharacterCount from '@tiptap/extension-character-count'
-import Mention from '@tiptap/extension-mention'
-import suggestion from './suggestion'
+import Bold from '@tiptap/extension-bold'
+import Italic from '@tiptap/extension-italic'
+import Strike from '@tiptap/extension-strike'
+import Placeholder from '@tiptap/extension-placeholder'
 import { content } from '../content'
 import './styles.scss'
+
+const TextFormattingMenu = ({ editor }) => {
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <nav>
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        className={editor.isActive('bold') ? 'is-active' : ''}
+      >
+        bold
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        className={editor.isActive('italic') ? 'is-active' : ''}
+      >
+        italic
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        className={editor.isActive('strike') ? 'is-active' : ''}
+      >
+        strike
+      </button>
+      <div className="divider"></div>
+    </nav>
+  )
+}
 
 export default () => {
   const limit = 280
@@ -17,63 +49,26 @@ export default () => {
       Document,
       Paragraph,
       Text,
+      Bold,
+      Italic,
+      Strike,
       CharacterCount.configure({
         limit,
       }),
-      Mention.configure({
-        HTMLAttributes: {
-          class: 'mention',
-        },
-        suggestion,
-      }),
+      Placeholder,
     ],
     content,
   })
 
-  const percentage = editor
-    ? Math.round((100 / limit) * editor.storage.characterCount.characters())
-    : 0
-
   return (
     <div className='message-box'>
-      <EditorContent editor={editor} />
-      {editor
-        && <div className={`character-count ${editor.storage.characterCount.characters() === limit ? 'character-count--warning' : ''}`}>
-          <svg
-            height="20"
-            width="20"
-            viewBox="0 0 20 20"
-            className="character-count__graph"
-          >
-            <circle
-              r="10"
-              cx="10"
-              cy="10"
-              fill="#e9ecef"
-            />
-            <circle
-              r="5"
-              cx="10"
-              cy="10"
-              fill="transparent"
-              stroke="currentColor"
-              strokeWidth="10"
-              strokeDasharray={`calc(${percentage} * 31.4 / 100) 31.4`}
-              transform="rotate(-90) translate(-20)"
-            />
-            <circle
-              r="6"
-              cx="10"
-              cy="10"
-              fill="white"
-            />
-          </svg>
-
-          <div className="character-count__text">
-            {editor.storage.characterCount.characters()}/{limit} characters
-          </div>
-        </div>
-      }
+      <header>
+        <TextFormattingMenu editor={editor} />
+      </header>
+      <main>
+        <EditorContent editor={editor} />
+      </main>
+      <footer></footer>
     </div>
   )
 }
--- demos/src/Examples/MessageBox/content.js
+++ demos/src/Examples/MessageBox/content.js
@@ -1,5 +1,5 @@
 export const content = `
   <p>
-  What do you all think about the new <span data-type="mention" data-id="Winona Ryder"></span> movie?
+
   </p>
 `
