4be16fcd2e6c107e59679eebf1ea1d6112a88afb
Philipp Kühn <philippkuehn@MacBook-Pro-von-Philipp.local>
2022-01-27 09:44:53 +0100

    fix: set max textselection correctly, fix #2401

--- packages/core/src/commands/setTextSelection.ts
+++ packages/core/src/commands/setTextSelection.ts
@@ -19,8 +19,8 @@ export const setTextSelection: RawCommands['setTextSelection'] = position => ({ 
     const { from, to } = typeof position === 'number'
       ? { from: position, to: position }
       : position
-    const minPos = Selection.atStart(doc).from
-    const maxPos = Selection.atEnd(doc).to
+    const minPos = 0
+    const maxPos = tr.doc.content.size
     const resolvedFrom = minMax(from, minPos, maxPos)
     const resolvedEnd = minMax(to, minPos, maxPos)
     const selection = TextSelection.create(doc, resolvedFrom, resolvedEnd)
