81f5c521e1772d6db75802286a472e3bca0df21e
svenadlung <info@svenadlung.de>
2022-04-26 21:51:04 +0200

    Demos: Update collab rooms

--- demos/src/Examples/CollaborativeEditing/React/index.jsx
+++ demos/src/Examples/CollaborativeEditing/React/index.jsx
@@ -15,7 +15,7 @@ import MenuBar from './MenuBar'
 import './styles.scss'
 
 const colors = ['#958DF1', '#F98181', '#FBBC88', '#FAF594', '#70CFF8', '#94FADB', '#B9F18D']
-const rooms = ['rooms.10', 'rooms.11', 'rooms.12']
+const rooms = ['rooms.13', 'rooms.14', 'rooms.15']
 const names = [
   'Lea Thompson',
   'Cyndi Lauper',
