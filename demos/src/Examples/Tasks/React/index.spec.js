f9ab39e53a67cf9ac177a751156966a84fcd3be2
Dominik Biedebach <dominik.biedebach@ueber.io>
2022-05-12 11:30:57 +0200

    refactor(tests): added todo note for tasks test

--- demos/src/Examples/Tasks/React/index.spec.js
+++ demos/src/Examples/Tasks/React/index.spec.js
@@ -18,6 +18,8 @@ context('/src/Examples/Tasks/React/', () => {
     cy.get('.ProseMirror input[type="checkbox"]').should('have.length', 3)
   })
 
+  // TODO: Make this test work properly - right now it's not doing what it
+  // should do.
   it('should check and uncheck tasks on click', () => {
     cy.get('.ProseMirror').type('Cook food{enter}Eat food{enter}Clean dishes')
     cy.get('.ProseMirror').find('input[type="checkbox"]').then(console.log)
