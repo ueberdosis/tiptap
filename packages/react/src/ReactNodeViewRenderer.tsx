2ecedb6955537ae30e1501f5e840e7e19ae27908
Dominik Biedebach <dominik@bdbch.com>
2022-06-07 02:37:13 +0200

    style: fix linting issues

--- packages/react/src/ReactNodeViewRenderer.tsx
+++ packages/react/src/ReactNodeViewRenderer.tsx
@@ -57,10 +57,10 @@ class ReactNodeView extends NodeView<
       this.component.displayName = capitalizeFirstChar(this.extension.name)
     }
 
-    const ReactNodeViewProvider: React.FunctionComponent = (componentProps) => {
+    const ReactNodeViewProvider: React.FunctionComponent = componentProps => {
       const Component = this.component
       const onDragStart = this.onDragStart.bind(this)
-      const nodeViewContentRef: ReactNodeViewContextProps['nodeViewContentRef'] = (element) => {
+      const nodeViewContentRef: ReactNodeViewContextProps['nodeViewContentRef'] = element => {
         if (element && this.contentDOMElement && element.firstChild !== this.contentDOMElement) {
           element.appendChild(this.contentDOMElement)
         }
@@ -104,8 +104,8 @@ class ReactNodeView extends NodeView<
 
   get dom() {
     if (
-      this.renderer.element.firstElementChild &&
-      !this.renderer.element.firstElementChild?.hasAttribute('data-node-view-wrapper')
+      this.renderer.element.firstElementChild
+      && !this.renderer.element.firstElementChild?.hasAttribute('data-node-view-wrapper')
     ) {
       throw Error('Please use the NodeViewWrapper component for your node view.')
     }
@@ -178,7 +178,7 @@ class ReactNodeView extends NodeView<
 
 export function ReactNodeViewRenderer(
   component: any,
-  options?: Partial<ReactNodeViewRendererOptions>
+  options?: Partial<ReactNodeViewRendererOptions>,
 ): NodeViewRenderer {
   return (props: NodeViewRendererProps) => {
     // try to get the parent component
