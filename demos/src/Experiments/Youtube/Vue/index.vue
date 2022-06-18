b711affa0ed335241247a9cc9eba38e6d47345f8
Dominik Biedebach <dominik@bdbch.com>
2022-06-18 14:40:04 +0200

    fix: fix tests for youtube

--- demos/src/Experiments/Youtube/Vue/index.vue
+++ demos/src/Experiments/Youtube/Vue/index.vue
@@ -43,7 +43,9 @@ export default {
     this.editor = new Editor({
       extensions: [
         StarterKit,
-        Youtube,
+        Youtube.configure({
+          controls: true,
+        }),
       ],
       content: `
         <p>Tiptap now supports youtube embeds! Awesome!</p>
