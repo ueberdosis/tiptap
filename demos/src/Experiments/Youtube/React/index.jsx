63146e4a69123fc009ee1369fe64f1a62e91ba94
Dominik Biedebach <dominik@bdbch.com>
2022-06-18 13:01:02 +0200

    fix(extension-test): fix broken build because of wrong output file names

--- demos/src/Experiments/Youtube/React/index.jsx
+++ demos/src/Experiments/Youtube/React/index.jsx
@@ -43,7 +43,9 @@ export default () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
-      Youtube,
+      Youtube.configure({
+        controls: false,
+      }),
     ],
     content: `
       <p>Tiptap now supports youtube embeds! Awesome!</p>
