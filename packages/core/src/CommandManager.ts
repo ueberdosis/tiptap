fea510376a2467d1154d59a28641a82397a2120b
Asherah Connor <asherah@hrzn.ee>
2022-07-26 15:22:19 +1000

    fix(core): createCan command props shouldn't try dispatch (#3025)

--- packages/core/src/CommandManager.ts
+++ packages/core/src/CommandManager.ts
@@ -107,13 +107,13 @@ export class CommandManager {
 
   public createCan(startTr?: Transaction): CanCommands {
     const { rawCommands, state } = this
-    const dispatch = undefined
+    const dispatch = false
     const tr = startTr || state.tr
     const props = this.buildProps(tr, dispatch)
     const formattedCommands = Object.fromEntries(Object
       .entries(rawCommands)
       .map(([name, command]) => {
-        return [name, (...args: never[]) => command(...args)({ ...props, dispatch })]
+        return [name, (...args: never[]) => command(...args)({ ...props, dispatch: undefined })]
       })) as unknown as SingleCommands
 
     return {
