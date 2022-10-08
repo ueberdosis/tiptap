fb442f4999d0b9adf08aee6298202667c3d0ae6a
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2022-10-09 01:40:22 +0330

    fix(hashtag-extension): demo content

--- demos/src/Marks/Hashtag/Vue/index.vue
+++ demos/src/Marks/Hashtag/Vue/index.vue
@@ -34,11 +34,11 @@ export default {
         }),
       ],
       content: `
-        <p>Hi everyone! Don’t forget the daily stand up at 8 AM.</p>
-        <p><span data-type="mention" data-id="Jennifer Grey"></span> Would you mind to share what you’ve been working on lately? We fear not much happened since Dirty Dancing.
+        <p>Hi everyone! Don’t forget the <span data-type="hashtag">#daily_standup</span> up at 8 AM.</p>
+        <p>Would you mind to share what you’ve been working on lately? We fear not much happened since <span class="hashtag" data-type="hashtag">#dirty_dancing</span>.
         <p><span data-type="mention" data-id="Winona Ryder"></span> <span data-type="mention" data-id="Axl Rose"></span> Let’s go through your most important points quickly.</p>
         <p>I have a meeting with <span data-type="mention" data-id="Christina Applegate"></span> and don’t want to come late.</p>
-        <p>– Thanks, your big boss</p>
+        <p>– Thanks, your <span class="hashtag" data-type="hashtag">#big_boss</span></p>
       `,
     })
   },
