0b89f251703cc2f9e38678d9493237e71179103e
Nikita Konev <nikit.cpp@yandex.ru>
2022-12-01 04:07:45 +0400

    Computable HTMLAttributes, file upload function for dragNdrop and paste

--- packages/extension-image/src/image.ts
+++ packages/extension-image/src/image.ts
@@ -1,13 +1,22 @@
 import {
+  isFunction,
   mergeAttributes,
   Node,
   nodeInputRule,
 } from '@tiptap/core'
+import {
+  Node as ProseMirrorNode,
+} from 'prosemirror-model'
+import { Plugin, PluginKey } from 'prosemirror-state'
 
 export interface ImageOptions {
   inline: boolean,
   allowBase64: boolean,
-  HTMLAttributes: Record<string, any>,
+  HTMLAttributes: Record<string, any> | ((input: ProseMirrorNode) => Record<string, any>),
+  allowPasteFromClipboard: boolean,
+  allowDragNDrop: boolean,
+  // accept File and returns src attribute
+  imageUpload?: ((image: any) => Promise<string>) | null
 }
 
 declare module '@tiptap/core' {
@@ -31,6 +40,9 @@ export const Image = Node.create<ImageOptions>({
       inline: false,
       allowBase64: false,
       HTMLAttributes: {},
+      allowPasteFromClipboard: false,
+      allowDragNDrop: false,
+      imageUpload: null,
     }
   },
 
@@ -68,8 +80,12 @@ export const Image = Node.create<ImageOptions>({
     ]
   },
 
-  renderHTML({ HTMLAttributes }) {
-    return ['img', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)]
+  renderHTML({ node, HTMLAttributes }) {
+    const attrs = isFunction(this.options.HTMLAttributes)
+      ? this.options.HTMLAttributes(node)
+      : this.options.HTMLAttributes
+
+    return ['img', mergeAttributes(attrs, HTMLAttributes)]
   },
 
   addCommands() {
@@ -92,6 +108,84 @@ export const Image = Node.create<ImageOptions>({
           const [,, alt, src, title] = match
 
           return { src, alt, title }
+        },
+      }),
+    ]
+  },
+
+  addProseMirrorPlugins() {
+    return [
+      new Plugin({
+        key: new PluginKey('imageHandler'),
+        props: {
+          handlePaste: (view, event) => {
+            if (!this.options.allowPasteFromClipboard) {
+              return false
+            }
+            const items = Array.from(event.clipboardData?.items || [])
+
+            event.preventDefault()
+
+            items.forEach(item => {
+              if (item.type.indexOf('image') === 0) {
+                const { schema } = view.state
+
+                const image = item.getAsFile()
+
+                this.options.imageUpload?.(image).then(src => {
+                  const node = schema.nodes.image.create({
+                    src,
+                  })
+                  const transaction = view.state.tr.replaceSelectionWith(node)
+
+                  view.dispatch(transaction)
+                })
+              }
+            })
+          },
+          handleDOMEvents: {
+            drop: (view, event) => {
+              if (!this.options.allowDragNDrop) {
+                return false
+              }
+
+              const hasFiles = event.dataTransfer && event.dataTransfer.files && event.dataTransfer.files.length
+
+              if (!hasFiles) {
+                return false
+              }
+
+              const images = Array.from(
+                event.dataTransfer?.files ?? [],
+              ).filter(file => /image/i.test(file.type))
+
+              if (images.length === 0) {
+                return false
+              }
+
+              event.preventDefault()
+
+              const { schema } = view.state
+              const coordinates = view.posAtCoords({
+                left: event.clientX,
+                top: event.clientY,
+              })
+
+              if (!coordinates) { return false }
+
+              images.forEach(async image => {
+
+                const node = schema.nodes.image.create({
+                  src: await this.options.imageUpload?.(image),
+                })
+                const transaction = view.state.tr.insert(coordinates.pos, node)
+
+                view.dispatch(transaction)
+              })
+
+              return true
+            },
+          },
         },
       }),
     ]
