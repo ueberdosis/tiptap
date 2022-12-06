e5cc771345cc870533da273d53a6a36ed8465a58
svenadlung <info@svenadlung.de>
2022-12-06 13:50:42 +0100

    refactor(extension-youtube): rename utility function name

--- packages/extension-youtube/src/utils.ts
+++ packages/extension-youtube/src/utils.ts
@@ -30,7 +30,7 @@ export const getYoutubeEmbedUrl = (nocookie?: boolean) => {
   return nocookie ? 'https://www.youtube-nocookie.com/embed/' : 'https://www.youtube.com/embed/'
 }
 
-export const getEmbedURLFromYoutubeURL = (options: GetEmbedUrlOptions) => {
+export const getEmbedUrlFromYoutubeUrl = (options: GetEmbedUrlOptions) => {
   const {
     url,
     allowFullscreen,
