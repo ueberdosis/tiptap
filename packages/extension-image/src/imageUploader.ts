5f1f9301e7cbe2e24b0d2e298662ef10173cc034
四条眉毛 <fourfishxiake@gmail.com>
2022-12-15 19:06:36 +0800

    fix: keep the original pic

--- packages/extension-image/src/imageUploader.ts
+++ packages/extension-image/src/imageUploader.ts
@@ -27,8 +27,6 @@ Transaction.prototype.replaceAttrs = function replaceAttrs(
   return this.step(new ReplaceAttrsStep(pos, attrs))
 }
 
-const uploadFailedSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3.515 2.1l19.092 19.092-1.415 1.415-2.014-2.015A5.985 5.985 0 0 1 17 21H7A6 6 0 0 1 5.008 9.339a6.992 6.992 0 0 1 .353-2.563L2.1 3.514 3.515 2.1zM7 9c0 .081.002.163.006.243l.07 1.488-1.404.494A4.002 4.002 0 0 0 7 19h10c.186 0 .369-.013.548-.037L7.03 8.445C7.01 8.627 7 8.812 7 9zm5-7a7 7 0 0 1 6.992 7.339 6.003 6.003 0 0 1 3.212 8.65l-1.493-1.493a3.999 3.999 0 0 0-5.207-5.206L14.01 9.795C14.891 9.29 15.911 9 17 9a5 5 0 0 0-7.876-4.09l-1.43-1.43A6.97 6.97 0 0 1 12 2z"/></svg>'
-
 export class ImageUploaderPlugin {
   public view!: EditorView
 
@@ -128,7 +126,7 @@ export class ImageUploaderPlugin {
     const imageNodes = getImagePositions()
 
     let imageInfo: ImageInfo = {
-      src: `data:image/svg+xml,${uploadFailedSvg}`,
+      src: typeof fileOrUrl === 'string' ? fileOrUrl : URL.createObjectURL(fileOrUrl),
     }
 
     if (imageNodes.length && this.config.upload) {
