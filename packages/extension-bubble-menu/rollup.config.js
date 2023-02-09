516f28f788651b88a03136220334b6dd0f8b235f
Dominik Biedebach <dominik.biedebach@ueber.io>
2023-02-09 09:11:04 +0100

    fix(bubble-menu): exclude lodash/debounce from externals

--- packages/extension-bubble-menu/rollup.config.js
+++ packages/extension-bubble-menu/rollup.config.js
@@ -9,7 +9,7 @@ import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 
 export default {
-  external: [/@tiptap\/pm\/.*/, 'lodash/debounce'],
+  external: [/@tiptap\/pm\/.*/],
   input: 'src/index.ts',
   output: [
     {
