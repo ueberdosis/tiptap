f8695073968c5c6865ad8faf05351020abb2a3cc
Dominik <6538827+bdbch@users.noreply.github.com>
2023-02-27 21:23:30 +0100

    fix: update typings for node view decorations (#3783)
    * fix: update typings for node view decorations
    
    * fix(core): update types for NodeView
    
    * fix(core): declare props.decorations as decorationWithType

--- packages/core/src/NodeView.ts
+++ packages/core/src/NodeView.ts
@@ -1,10 +1,10 @@
 import { Node as ProseMirrorNode } from '@tiptap/pm/model'
 import { NodeSelection } from '@tiptap/pm/state'
-import { Decoration, NodeView as ProseMirrorNodeView } from '@tiptap/pm/view'
+import { NodeView as ProseMirrorNodeView } from '@tiptap/pm/view'
 
 import { Editor as CoreEditor } from './Editor'
 import { Node } from './Node'
-import { NodeViewRendererOptions, NodeViewRendererProps } from './types'
+import { DecorationWithType, NodeViewRendererOptions, NodeViewRendererProps } from './types'
 import { isiOS } from './utilities/isiOS'
 
 export class NodeView<
@@ -22,7 +22,7 @@ export class NodeView<
 
   node: ProseMirrorNode
 
-  decorations: Decoration[]
+  decorations: DecorationWithType[]
 
   getPos: any
 
@@ -38,7 +38,7 @@ export class NodeView<
     } as Options
     this.extension = props.extension
     this.node = props.node
-    this.decorations = props.decorations
+    this.decorations = props.decorations as DecorationWithType[]
     this.getPos = props.getPos
     this.mount()
   }
