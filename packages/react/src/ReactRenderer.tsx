23788d14a5cae07e92156ba7cd9304b6ec9b4fc1
Dominik Biedebach <dominik.biedebach@ueber.io>
2023-03-03 00:28:38 +0100

    feat(react): allow react renderer to assign attributes to react renderer element

--- packages/react/src/ReactRenderer.tsx
+++ packages/react/src/ReactRenderer.tsx
@@ -23,6 +23,7 @@ export interface ReactRendererOptions {
   props?: Record<string, any>,
   as?: string,
   className?: string,
+  attrs?: Record<string, string>,
 }
 
 type ComponentType<R, P> =
@@ -50,6 +51,7 @@ export class ReactRenderer<R = unknown, P = unknown> {
     props = {},
     as = 'div',
     className = '',
+    attrs,
   }: ReactRendererOptions) {
     this.id = Math.floor(Math.random() * 0xFFFFFFFF).toString()
     this.component = component
@@ -60,6 +62,12 @@ export class ReactRenderer<R = unknown, P = unknown> {
 
     if (className) {
       this.element.classList.add(...className.split(' '))
+    }
+
+    if (attrs) {
+      Object.keys(attrs).forEach(key => {
+        this.element.setAttribute(key, attrs[key])
+      })
     }
 
     this.render()
