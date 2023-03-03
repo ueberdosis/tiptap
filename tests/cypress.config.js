668c2a6592b4df10c4ef3023f2033c0b7c1d4938
Dominik Biedebach <dominik.biedebach@ueber.io>
2023-03-03 13:00:14 +0100

    tests: increase timeout

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 12000,
+  defaultCommandTimeout: 30000,
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
