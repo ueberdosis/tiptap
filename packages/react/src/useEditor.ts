6283cee5c7e3ca1efb4cfaf87aca077a7852e22e
Dominik <6538827+bdbch@users.noreply.github.com>
2023-03-03 11:35:53 +0100

    fix(react): allow updating event handlers on editor (#3811)

--- packages/react/src/useEditor.ts
+++ packages/react/src/useEditor.ts
@@ -11,7 +11,67 @@ function useForceUpdate() {
 
 export const useEditor = (options: Partial<EditorOptions> = {}, deps: DependencyList = []) => {
   const [editor, setEditor] = useState<Editor | null>(null)
+
   const forceUpdate = useForceUpdate()
+
+  const {
+    onBeforeCreate,
+    onBlur,
+    onCreate,
+    onDestroy,
+    onFocus,
+    onSelectionUpdate,
+    onTransaction,
+    onUpdate,
+  } = options
+
+  // This effect will handle updating the editor instance
+  // when the event handlers change.
+  useEffect(() => {
+    if (!editor) {
+      return
+    }
+
+    if (onBeforeCreate) {
+      editor.off('beforeCreate')
+      editor.on('beforeCreate', onBeforeCreate)
+    }
+
+    if (onBlur) {
+      editor.off('blur')
+      editor.on('blur', onBlur)
+    }
+
+    if (onCreate) {
+      editor.off('create')
+      editor.on('create', onCreate)
+    }
+
+    if (onDestroy) {
+      editor.off('destroy')
+      editor.on('destroy', onDestroy)
+    }
+
+    if (onFocus) {
+      editor.off('focus')
+      editor.on('focus', onFocus)
+    }
+
+    if (onSelectionUpdate) {
+      editor.off('selectionUpdate')
+      editor.on('selectionUpdate', onSelectionUpdate)
+    }
+
+    if (onTransaction) {
+      editor.off('transaction')
+      editor.on('transaction', onTransaction)
+    }
+
+    if (onUpdate) {
+      editor.off('update')
+      editor.on('update', onUpdate)
+    }
+  }, [onBeforeCreate, onBlur, onCreate, onDestroy, onFocus, onSelectionUpdate, onTransaction, onUpdate])
 
   useEffect(() => {
     let isMounted = true
