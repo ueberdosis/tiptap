b0fc47211738412ee3eab9b0454db0c98a2cdcb9
HariHaran Subramanian <hharan618@gmail.com>
2023-03-09 10:25:23 +0530

    fix: default attribute + predicate

--- packages/extension-ordered-list/src/ordered-list.ts
+++ packages/extension-ordered-list/src/ordered-list.ts
@@ -91,6 +91,8 @@ export const OrderedList = Node.create<OrderedListOptions>({
     let inputRule = wrappingInputRule({
       find: inputRegex,
       type: this.type,
+      getAttributes: match => ({ start: +match[1] }),
+      joinPredicate: (match, node) => node.childCount + node.attrs.start === +match[1],
     })
 
     if (this.options.keepMarks || this.options.keepAttributes) {
