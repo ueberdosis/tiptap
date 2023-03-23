e22b873e55908604cb8ad04ba115ea2485c32e1a
Sven Adlung <sven.adlung@ueber.io>
2023-03-23 09:54:58 +0100

    refactor(extension-youtube): command types (#3842)

--- packages/extension-youtube/src/youtube.ts
+++ packages/extension-youtube/src/youtube.ts
@@ -26,13 +26,15 @@ export interface YoutubeOptions {
   width: number;
 }
 
+type SetYoutubeVideoOptions = { src: string, width?: number, height?: number, start?: number }
+
 declare module '@tiptap/core' {
   interface Commands<ReturnType> {
     youtube: {
       /**
        * Insert a youtube video
        */
-      setYoutubeVideo: (options: { src: string, width?: number, height?: number, start?: number }) => ReturnType,
+      setYoutubeVideo: (options: SetYoutubeVideoOptions) => ReturnType,
     }
   }
 }
@@ -103,7 +105,7 @@ export const Youtube = Node.create<YoutubeOptions>({
 
   addCommands() {
     return {
-      setYoutubeVideo: options => ({ commands }) => {
+      setYoutubeVideo: (options: SetYoutubeVideoOptions) => ({ commands }) => {
         if (!isValidYoutubeUrl(options.src)) {
           return false
         }
