1e175bcc6c8b15a696b7d066a4cf6aa286c1ef4f
Dominik Biedebach <dominik.biedebach@ueber.io>
2023-04-03 16:31:37 +0200

    fix(react): move ref assignment inside if

--- packages/react/src/useEditor.ts
+++ packages/react/src/useEditor.ts
@@ -49,50 +49,50 @@ export const useEditor = (options: Partial<EditorOptions> = {}, deps: Dependency
     if (onBeforeCreate) {
       editor.off('beforeCreate', onBeforeCreateRef.current)
       editor.on('beforeCreate', onBeforeCreate)
+      onBeforeCreateRef.current = onBeforeCreate
     }
-    onBeforeCreateRef.current = onBeforeCreate
 
     if (onBlur) {
       editor.off('blur', onBlurRef.current)
       editor.on('blur', onBlur)
+      onBlurRef.current = onBlur
     }
-    onBlurRef.current = onBlur
 
     if (onCreate) {
       editor.off('create', onCreateRef.current)
       editor.on('create', onCreate)
+      onCreateRef.current = onCreate
     }
-    onCreateRef.current = onCreate
 
     if (onDestroy) {
       editor.off('destroy', onDestroyRef.current)
       editor.on('destroy', onDestroy)
+      onDestroyRef.current = onDestroy
     }
-    onDestroyRef.current = onDestroy
 
     if (onFocus) {
       editor.off('focus', onFocusRef.current)
       editor.on('focus', onFocus)
+      onFocusRef.current = onFocus
     }
-    onFocusRef.current = onFocus
 
     if (onSelectionUpdate) {
       editor.off('selectionUpdate', onSelectionUpdateRef.current)
       editor.on('selectionUpdate', onSelectionUpdate)
+      onSelectionUpdateRef.current = onSelectionUpdate
     }
-    onSelectionUpdateRef.current = onSelectionUpdate
 
     if (onTransaction) {
       editor.off('transaction', onTransactionRef.current)
       editor.on('transaction', onTransaction)
+      onTransactionRef.current = onTransaction
     }
-    onTransactionRef.current = onTransaction
 
     if (onUpdate) {
       editor.off('update', onUpdateRef.current)
       editor.on('update', onUpdate)
+      onUpdateRef.current = onUpdate
     }
-    onUpdateRef.current = onUpdate
   }, [onBeforeCreate, onBlur, onCreate, onDestroy, onFocus, onSelectionUpdate, onTransaction, onUpdate, editor])
 
   useEffect(() => {
