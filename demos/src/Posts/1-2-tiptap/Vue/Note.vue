63a56639fc3c736ff7e60d9ba4e6bfe3b4c0954c
Jan Thurau <jan@janthurau.de>
2023-05-07 21:11:23 +0200

    TiptapCollab blog post series: first WIP

---
+++ demos/src/Posts/1-2-tiptap/Vue/Note.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import StarterKit from '@tiptap/starter-kit'
+import { EditorContent, useEditor } from '@tiptap/vue-3'
+import { ref, watch } from 'vue'
+
+import type { TNote } from './types'
+
+const props = defineProps<{note: TNote}>()
+
+const modelValueProxy = ref('')
+
+watch(props, () => modelValueProxy.value = props.note.content)
+
+const editor = useEditor({
+  content: props.note.content,
+  extensions: [
+    StarterKit,
+  ],
+})
+
+</script>
+
+<template>
+  <editor-content :editor="editor"></editor-content>
+</template>
