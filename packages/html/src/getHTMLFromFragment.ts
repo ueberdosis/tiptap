7881dea293391296c31705e65c5cbabdd53d225e
Sam Sudar <sudar.sam@gmail.com>
2023-07-08 16:42:17 -0700

    fix up lint errors

--- packages/html/src/getHTMLFromFragment.ts
+++ packages/html/src/getHTMLFromFragment.ts
@@ -5,16 +5,16 @@ export function getHTMLFromFragment(doc: Node, schema: Schema, options?: { docum
   if (options?.document) {
     // The caller is relying on their own document implementation. Use this
     // instead of the default zeed-dom.
-    const contentNode = Node.fromJSON(schema, doc);
-    const wrap = document.createElement('div');
-    const serializedDocument = DOMSerializer.fromSchema(schema).serializeFragment(doc.content, { document: options.document }, wrap);
-    return wrap.innerHTML;
+    const wrap = options.document.createElement('div')
+
+    DOMSerializer.fromSchema(schema).serializeFragment(doc.content, { document: options.document }, wrap)
+    return wrap.innerHTML
   }
 
   // Use zeed-dom for serialization.
-  const document = DOMSerializer.fromSchema(schema).serializeFragment(doc.content, {
+  const zeedDocument = DOMSerializer.fromSchema(schema).serializeFragment(doc.content, {
     document: createHTMLDocument() as unknown as Document,
   }) as unknown as VHTMLDocument
 
-  return document.render()
+  return zeedDocument.render()
 }
