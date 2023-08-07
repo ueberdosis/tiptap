ee06cb01489518d59d2a1f46014b43ea71a0e7ee
Timo Santi <timo.santi@santicon.fi>
2023-08-07 10:35:49 +0300

    Fixed typo and added some documentation

--- packages/extension-table/src/utilities/createColGroup.ts
+++ packages/extension-table/src/utilities/createColGroup.ts
@@ -1,6 +1,15 @@
 import { Node as ProseMirrorNode } from '@tiptap/pm/model'
 import { DOMOutputSpec } from 'prosemirror-model'
 
+/**
+ * Creates a colgroup element for a table node in ProseMirror.
+ *
+ * @param node - The ProseMirror node representing the table.
+ * @param cellMinWidth - The minimum width of a cell in the table.
+ * @param overrideCol - (Optional) The index of the column to override the width of.
+ * @param overrideValue - (Optional) The width value to use for the overridden column.
+ * @returns An object containing the colgroup element, the total width of the table, and the minimum width of the table.
+ */
 export function createColGroup(
   node: ProseMirrorNode,
   cellMinWidth: number,
@@ -9,7 +18,7 @@ export function createColGroup(
 ) {
   let totalWidth = 0
   let fixedWidth = true
-  const colls: DOMOutputSpec[] = []
+  const cols: DOMOutputSpec[] = []
   const row = node.firstChild
 
   if (!row) {
@@ -29,14 +38,14 @@ export function createColGroup(
         fixedWidth = false
       }
 
-      colls.push(['col', cssWidth ? { style: `width: ${cssWidth}` } : {}])
+      cols.push(['col', cssWidth ? { style: `width: ${cssWidth}` } : {}])
     }
   }
 
   const tableWidth = fixedWidth ? `${totalWidth}px` : ''
   const tableMinWidth = fixedWidth ? '' : `${totalWidth}px`
 
-  const colgroup: DOMOutputSpec = ['colgroup', {}, ...colls]
+  const colgroup: DOMOutputSpec = ['colgroup', {}, ...cols]
 
   return { colgroup, tableWidth, tableMinWidth }
 }
