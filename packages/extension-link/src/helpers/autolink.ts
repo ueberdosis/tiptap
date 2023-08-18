8f5a73d6d6e4b3d9431f16a9ef4e5494ddfd43da
Steven Qi <steven-qi@users.noreply.github.com>
2023-08-18 11:04:13 +0800

    fix: fix autolink when code is not enabled for editor

--- packages/extension-link/src/helpers/autolink.ts
+++ packages/extension-link/src/helpers/autolink.ts
@@ -87,6 +87,7 @@ export function autolink(options: AutolinkOptions): Plugin {
             }))
             // ignore link inside code mark
             .filter(link => {
+              if (!newState.schema.marks.code) return true; // maybe code is not enabled for this editor
               return !newState.doc.rangeHasMark(
                 link.from,
                 link.to,
