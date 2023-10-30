accbd429d0a58c7cd8c7a9d2f0981f958ea095ee
Cameron Hessler <cameronhessler123@gmail.com>
2023-10-30 01:48:01 -0500

    Youtube base URL no longer matches youtube node
    Fixes #4560

--- demos/src/Nodes/Youtube/Vue/index.spec.js
+++ demos/src/Nodes/Youtube/Vue/index.spec.js
@@ -17,6 +17,16 @@ context('/src/Nodes/Youtube/Vue/', () => {
     })
   })
 
+  it('adds a video via pasting', () => {
+    cy.get('.tiptap').paste({
+      pastePayload: 'https://music.youtube.com/watch?v=hBp4dgE7Bho&feature=share',
+      pasteType: 'text/plain',
+    })
+    cy.get('.tiptap div[data-youtube-video] iframe')
+      .should('have.length', 1)
+      .should('have.attr', 'src', 'https://www.youtube.com/embed/hBp4dgE7Bho?controls=0')
+  })
+
   it('adds a video with 320 width and 240 height', () => {
     cy.window().then(win => {
       cy.stub(win, 'prompt', () => 'https://music.youtube.com/watch?v=hBp4dgE7Bho&feature=share')
@@ -55,6 +65,30 @@ context('/src/Nodes/Youtube/Vue/', () => {
       cy.get('.tiptap div[data-youtube-video] iframe')
         .should('have.length', 1)
         .should('have.attr', 'src', 'https://www.youtube.com/embed/wRakoMYVHm8?controls=0')
+    })
+  })
+
+  const invalidLinks = [
+    'https://www.youtube.com/',
+    'https://www.youtube.com',
+    'http://notavalidlinkyoutube.com/watch?v=hBp4dgE7Bho&feature=share',
+  ]
+
+  invalidLinks.forEach(url => {
+    it(`does not add a video with invalid url ${url}`, () => {
+      cy.window().then(win => {
+        cy.stub(win, 'prompt', () => url)
+        cy.get('#add').eq(0).click()
+        cy.get('.tiptap div[data-youtube-video] iframe').should('have.length', 0)
+      })
+    })
+
+    it(`does not add a video with invalid via pasting url ${url}`, () => {
+      cy.get('.tiptap').paste({
+        pastePayload: url,
+        pasteType: 'text/plain',
+      })
+      cy.get('.tiptap div[data-youtube-video] iframe').should('have.length', 0)
     })
   })
 })
