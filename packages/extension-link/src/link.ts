7debf2baf0999e09e9775577bb44619a5b7e3786
Cameron Hessler <cameronhessler123@gmail.com>
2023-11-06 01:16:30 -0600

    fix(extension-youtube) Add fix for link too

--- packages/extension-link/src/link.ts
+++ packages/extension-link/src/link.ts
@@ -121,6 +121,12 @@ export const Link = Mark.create<LinkOptions>({
   },
 
   renderHTML({ HTMLAttributes }) {
+    // False positive; we're explicitly checking for javascript: links to ignore them
+    // eslint-disable-next-line no-script-url
+    if (HTMLAttributes.href?.startsWith('javascript:')) {
+      // strip out the href
+      return ['a', mergeAttributes(this.options.HTMLAttributes, { ...HTMLAttributes, href: '' }), 0]
+    }
     return ['a', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
   },
 
