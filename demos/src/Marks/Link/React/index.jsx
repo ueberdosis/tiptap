de8d4bf149e8daff1c97940c5d46925f10bd5007
Jan Thurau <jan@janthurau.de>
2023-11-20 21:11:33 +0100

    fixes typo

--- demos/src/Marks/Link/React/index.jsx
+++ demos/src/Marks/Link/React/index.jsx
@@ -23,7 +23,7 @@ export default () => {
     ],
     content: `
         <p>
-          Wow, this editor has support for links to the whole <a href="https://en.wikipedia.org/wiki/World_Wide_Web"><b>world wide web</b>></a>. We tested a lot of URLs and I think you can add *every URL* you want. Isn’t that cool? Let’s try <a href="https://statamic.com/">another one!</a> Yep, seems to work.
+          Wow, this editor has support for links to the whole <a href="https://en.wikipedia.org/wiki/World_Wide_Web"><b>world wide web</b></a>. We tested a lot of URLs and I think you can add *every URL* you want. Isn’t that cool? Let’s try <a href="https://statamic.com/">another one!</a> Yep, seems to work.
         </p>
         <p>
           By default every link will get a <code>rel="noopener noreferrer nofollow"</code> attribute. It’s configurable though.
