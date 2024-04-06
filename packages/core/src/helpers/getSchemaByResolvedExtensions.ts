9b83bc56802afa3578a11fe45d2bf7c9a4b0ea0b
bdbch <github@bdbch.mozmail.com>
2024-04-06 13:09:42 +0200

    refactor(core): refactor extension manager

--- packages/core/src/helpers/getSchemaByResolvedExtensions.ts
+++ packages/core/src/helpers/getSchemaByResolvedExtensions.ts
@@ -23,6 +23,12 @@ function cleanUpSchemaItem<T>(data: T) {
   ) as T
 }
 
+/**
+ * Get a Prosemirror schema from an array of Tiptap extensions.
+ * @param extensions A list of Tiptap extensions.
+ * @param editor The editor instance.
+ * @returns A Prosemirror schema.
+ */
 export function getSchemaByResolvedExtensions(extensions: Extensions, editor?: Editor): Schema {
   const allAttributes = getAttributesFromExtensions(extensions)
   const { nodeExtensions, markExtensions } = splitExtensions(extensions)
