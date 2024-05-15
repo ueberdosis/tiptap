da9d62f79b82aa01ea17d4a71a38af5822459dec
Aaron HS <asiraky@gmail.com>
2024-05-16 09:45:47 +1000

    Allow caller to supply parser options

--- packages/html/src/generateJSON.ts
+++ packages/html/src/generateJSON.ts
@@ -1,11 +1,12 @@
 import { Extensions, getSchema } from '@tiptap/core'
-import { DOMParser } from '@tiptap/pm/model'
+import { DOMParser, ParseOptions } from '@tiptap/pm/model'
 import { parseHTML } from 'zeed-dom'
 
 /**
  * Generates a JSON object from the given HTML string and converts it into a Prosemirror node with content.
  * @param {string} html - The HTML string to be converted into a Prosemirror node.
  * @param {Extensions} extensions - The extensions to be used for generating the schema.
+ * @param {ParseOptions} options - The options to be supplied to the parser.
  * @returns {Record<string, any>} - The generated JSON object.
  * @example
  * const html = '<p>Hello, world!</p>'
@@ -13,9 +14,9 @@ import { parseHTML } from 'zeed-dom'
  * const json = generateJSON(html, extensions)
  * console.log(json) // { type: 'doc', content: [{ type: 'paragraph', content: [{ type: 'text', text: 'Hello, world!' }] }] }
  */
-export function generateJSON(html: string, extensions: Extensions): Record<string, any> {
+export function generateJSON(html: string, extensions: Extensions, options?: ParseOptions): Record<string, any> {
   const schema = getSchema(extensions)
   const dom = parseHTML(html) as unknown as Node
 
-  return DOMParser.fromSchema(schema).parse(dom).toJSON()
+  return DOMParser.fromSchema(schema).parse(dom, options).toJSON()
 }
