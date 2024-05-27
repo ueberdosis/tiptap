7c8d21745d8fa7585261b82aea15e6e56ee444e2
svenadlung <info@svenadlung.de>
2024-05-27 17:22:55 +0200

    test(images): fix failing tests

--- demos/src/Examples/Images/React/index.spec.js
+++ demos/src/Examples/Images/React/index.spec.js
@@ -16,7 +16,7 @@ context('/src/Examples/Images/React/', () => {
 
   it('allows images to be added via URL', () => {
     cy.window().then(win => {
-      cy.stub(win, 'prompt').returns('https://unsplash.it/250/250')
+      cy.stub(win, 'prompt').returns('https://placehold.co/400x400')
 
       cy.wait(1000)
       cy.get('button').contains('add image from URL').click({ force: false })
