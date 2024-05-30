b8e5bcd8a56cbf847b24fe643b910c80e900bfe8
svenadlung <info@svenadlung.de>
2024-05-30 14:33:45 +0200

    docs: fix tests

--- demos/src/Examples/Community/Vue/index.spec.js
+++ demos/src/Examples/Community/Vue/index.spec.js
@@ -5,15 +5,15 @@ context('/src/Examples/Community/Vue/', () => {
 
   it('should count the characters correctly', () => {
     // check if count text is "44 / 280 characters"
-    cy.get('.character-count').should('contain', ' 44 / 280 characters')
+    cy.get('.character-count').should('contain', '44 / 280 characters')
 
     // type in .tiptap
     cy.get('.tiptap').type(' Hello World')
-    cy.get('.character-count').should('contain', ' 56 / 280 characters')
+    cy.get('.character-count').should('contain', '56 / 280 characters')
 
     // remove content from .tiptap and enter text
     cy.get('.tiptap').type('{selectall}{backspace}Hello World')
-    cy.get('.character-count').should('contain', ' 11 / 280 characters')
+    cy.get('.character-count').should('contain', '11 / 280 characters')
   })
 
   it('should mention a user', () => {
@@ -30,7 +30,7 @@ context('/src/Examples/Community/Vue/', () => {
 
       // check if the user is mentioned
       cy.get('.tiptap').should('have.text', `@${name} `)
-      cy.get('.character-count').should('contain', ' 2 / 280 characters')
+      cy.get('.character-count').should('contain', '2 / 280 characters')
     })
 
   })
