ac022c4037c0e5b1cc9e72f4cade51423c131e5b
svenadlung <info@svenadlung.de>
2024-05-30 14:20:23 +0200

    docs: demo styling

--- demos/src/GuideGettingStarted/VModel/Vue/index.vue
+++ demos/src/GuideGettingStarted/VModel/Vue/index.vue
@@ -2,9 +2,9 @@
   <div>
     <editor v-model="content" />
 
-    <div class="content">
-      <h3>Content</h3>
-      <pre><code>{{ content }}</code></pre>
+    <div class="output-group">
+      <label>Content</label>
+      <code>{{ content }}</code>
     </div>
   </div>
 </template>
@@ -24,39 +24,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss">
-/* Basic editor styles */
-.tiptap {
-  > * + * {
-    margin-top: 0.75em;
-  }
-
-  code {
-    background-color: rgba(#616161, 0.1);
-    color: #616161;
-  }
-}
-
-.content {
-  padding: 1rem 0 0;
-
-  h3 {
-    margin: 1rem 0 0.5rem;
-  }
-
-  pre {
-    border-radius: 5px;
-    color: #333;
-  }
-
-  code {
-    display: block;
-    white-space: pre-wrap;
-    font-size: 0.8rem;
-    padding: 0.75rem 1rem;
-    background-color:#e9ecef;
-    color: #495057;
-  }
-}
-</style>
