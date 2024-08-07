771b25846836bee5e8acf80c94155c747b433f9d
Nick the Sick <nicholas.perez@tiptap.dev>
2024-08-07 09:38:51 +0200

    build: use tsup for compilation

--- packages/core/src/helpers/isList.ts
+++ packages/core/src/helpers/isList.ts
@@ -1,5 +1,5 @@
 import { getExtensionField } from '../helpers/getExtensionField.js'
-import { NodeConfig } from '../index.js'
+import type { NodeConfig } from '../index.js'
 import { Extensions } from '../types.js'
 import { callOrReturn } from '../utilities/callOrReturn.js'
 import { splitExtensions } from './splitExtensions.js'
