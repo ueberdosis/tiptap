e11aa0de22ce117a8ea1d498b54ffd7127b4abfb
Nick the Sick <nicholas.perez@tiptap.dev>
2024-08-12 11:02:32 +0200

    chore: initialize the collaboration cursor after the collaboration plugin

--- packages/extension-collaboration-cursor/src/collaboration-cursor.ts
+++ packages/extension-collaboration-cursor/src/collaboration-cursor.ts
@@ -96,6 +96,8 @@ const defaultOnUpdate = () => null
 export const CollaborationCursor = Extension.create<CollaborationCursorOptions, CollaborationCursorStorage>({
   name: 'collaborationCursor',
 
+  priority: 1001,
+
   addOptions() {
     return {
       provider: null,
