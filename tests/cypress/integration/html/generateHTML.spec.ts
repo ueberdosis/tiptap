b0b2e0b0bb434f6d72e37fdb75e174e603cef6d1
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-17 20:44:44 -0400

    Update generateHTML.spec.ts

--- tests/cypress/integration/html/generateHTML.spec.ts
+++ tests/cypress/integration/html/generateHTML.spec.ts
@@ -3,7 +3,8 @@
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
-import { generateHTML } from '@tiptap/html'
+import Youtube from '@tiptap/extension-youtube'
+import { generateHTML, generateJSON } from '@tiptap/html'
 
 describe('generateHTML', () => {
   it('generate HTML from JSON without an editor instance', () => {
@@ -25,5 +26,16 @@ describe('generateHTML', () => {
     ])
 
     expect(html).to.eq('<p>Example Text</p>')
+  })
+
+  it('can convert from & to html', async () => {
+    const extensions = [Document, Text, Youtube]
+    const html = `<p>Tiptap now supports YouTube embeds! Awesome!</p>
+      <div data-youtube-video>
+        <iframe src="https://www.youtube.com/watch?v=cqHqLQgVCgY"></iframe>
+      </div>`
+    const json = generateJSON(html, extensions)
+
+    expect(generateHTML(json, extensions)).to.equal(html)
   })
 })
