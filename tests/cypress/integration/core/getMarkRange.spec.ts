91ad89fb7e682adff1b0331718dd81e400d6c03e
Dominik Biedebach <dominik.biedebach@tiptap.dev>
2024-10-10 11:02:43 +0200

    added forward and backward boundary checks for tests

--- tests/cypress/integration/core/getMarkRange.spec.ts
+++ tests/cypress/integration/core/getMarkRange.spec.ts
@@ -41,9 +41,6 @@ describe('getMarkRange', () => {
       from: 11,
       to: 17,
     })
-
-    // eslint-disable-next-line no-console
-    console.log(range)
   })
 
   it('gets the correct range for a position at the start of the mark', () => {
@@ -74,5 +71,73 @@ describe('getMarkRange', () => {
     const range = getMarkRange($pos, schema.marks.link)
 
     expect(range).to.eq(undefined)
+  })
+
+  it('doesnt cross node boundaries on backward check', () => {
+    const testDocument = {
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [
+            { type: 'text', text: 'This is a text with a ' },
+            { type: 'text', text: 'link.', marks: [{ type: 'link', attrs: { href: 'https://tiptap.dev' } }] },
+          ],
+        },
+        {
+          type: 'paragraph',
+          content: [
+            { type: 'text', text: 'This is a text without a link.' },
+          ],
+        },
+      ],
+    }
+
+    const doc = Node.fromJSON(schema, testDocument)
+    const $pos = doc.resolve(28)
+    const range = getMarkRange($pos, schema.marks.link)
+
+    expect(range).to.deep.eq({
+      from: 23,
+      to: 28,
+    })
+
+    const nextRange = getMarkRange(doc.resolve(30), schema.marks.link)
+
+    expect(nextRange).to.eq(undefined)
+  })
+
+  it('doesnt cross node boundaries on forward check', () => {
+    const testDocument = {
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [
+            { type: 'text', text: 'This is a text without a link.' },
+          ],
+        },
+        {
+          type: 'paragraph',
+          content: [
+            { type: 'text', text: 'A link', marks: [{ type: 'link', attrs: { href: 'https://tiptap.dev' } }] },
+            { type: 'text', text: ' is at the start of this paragraph.' },
+          ],
+        },
+      ],
+    }
+    const doc = Node.fromJSON(schema, testDocument)
+
+    const range = getMarkRange(doc.resolve(32), schema.marks.link)
+
+    expect(range).to.eq(undefined)
+
+    const $pos = doc.resolve(33)
+    const nextRange = getMarkRange($pos, schema.marks.link)
+
+    expect(nextRange).to.deep.eq({
+      from: 33,
+      to: 39,
+    })
   })
 })
