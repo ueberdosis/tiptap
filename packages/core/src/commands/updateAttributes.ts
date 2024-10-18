84db3d829e13b14412e5ce8bbd5fb9507804ecb1
Julien Cigar <julien@perdition.city>
2024-10-18 12:53:36 +0200

    sync

--- packages/core/src/commands/updateAttributes.ts
+++ packages/core/src/commands/updateAttributes.ts
@@ -1,4 +1,7 @@
-import { MarkType, NodeType } from '@tiptap/pm/model'
+import {
+  Mark, MarkType, Node, NodeType,
+} from '@tiptap/pm/model'
+import { SelectionRange } from '@tiptap/pm/state'
 
 import { getMarkType } from '../helpers/getMarkType.js'
 import { getNodeType } from '../helpers/getNodeType.js'
@@ -50,66 +53,54 @@ export const updateAttributes: RawCommands['updateAttributes'] = (typeOrName, at
     markType = getMarkType(typeOrName as MarkType, state.schema)
   }
 
-  if (dispatch && nodeType !== null) {
-    /*
-     * If the user didn't select anything then start from the
-     * current cursor, grab a NodePos, and find the closest
-     * typeOrName from that NodePos
-     */
-    if (tr.selection.empty) {
-      // get a NodePos
-      const pos = editor.$pos(tr.selection.anchor)
-      const node = pos.closest(nodeType.name)
+  if (dispatch) {
+    tr.selection.ranges.forEach((range: SelectionRange) => {
+      const from = range.$from.pos
+      const to = range.$to.pos
 
-      if (node !== null) {
+      if (tr.selection.empty) {
+        let lastPos: number | undefined
+        let lastNode: Node | undefined
+        let trimmedFrom: number
+        let trimmedTo: number
 
-        if (nodeType === node.node.type) {
-          tr.setNodeMarkup(node.pos - 1, null, {
-            ...node.node.attrs,
-            ...attributes,
-          })
+        state.doc.nodesBetween(from, to, (node: Node, pos: number) => {
+          if (nodeType && nodeType === node.type) {
+            trimmedFrom = Math.max(pos, from)
+            trimmedTo = Math.min(pos + node.nodeSize, to)
+            lastPos = pos
+            lastNode = node
+          }
+        })
+
+        if (lastNode) {
+
+          if (lastPos !== undefined) {
+            tr.setNodeMarkup(lastPos, undefined, {
+              ...lastNode.attrs,
+              ...attributes,
+            })
+          }
+
+          if (markType && lastNode.marks.length) {
+            lastNode.marks.forEach((mark: Mark) => {
+              if (markType === mark.type) {
+                tr.addMark(
+                  trimmedFrom,
+                  trimmedTo,
+                  markType.create({
+                    ...mark.attrs,
+                    ...attributes,
+                  }),
+                )
+              }
+            })
+          }
         }
-
-        if (markType && node.node.marks.length) {
-          node.node.marks.forEach(mark => {
-            if (markType === mark.type) {
-              tr.addMark(
-                pos.pos,
-                node.node.nodeSize,
-                markType.create({
-                  ...mark.attrs,
-                  ...attributes,
-                }),
-              )
-            }
-          })
-        }
-      }
-    } else {
-      tr.selection.ranges.forEach(range => {
-        /* If we have typeOrName == flexItem and something like the following
-         * in the editor:
-         * <someflexItemTag>
-         *   <p>foo</p><p>bar</p>
-         * </someflexItemTag>
-         *
-         * If we (text) select "ooba" from that part in
-         * the editor the selection range would not include
-         * the flexItem, so be sure to start from that node
-         * pos.
-         *
-         * I'm not sure that this is the best way to do it
-         * but.. it works. (I've to check the
-         * prosemirror/tiptap documentation)
-         */
-        const rnode = editor.$pos(range.$from.pos).closest(nodeType.name)
-
-        if (rnode !== null) {
-          const from = rnode.pos - 1
-          const to = range.$to.pos
-
-          state.doc.nodesBetween(from, to, (node, pos) => {
-            if (nodeType && nodeType === node.type && pos >= from && pos <= to) {
+      } else {
+        state.doc.nodesBetween(from, to, (node: Node, pos: number) => {
+          if (pos >= from && pos <= to) {
+            if (nodeType && nodeType === node.type) {
               tr.setNodeMarkup(pos, undefined, {
                 ...node.attrs,
                 ...attributes,
@@ -117,7 +108,7 @@ export const updateAttributes: RawCommands['updateAttributes'] = (typeOrName, at
             }
 
             if (markType && node.marks.length) {
-              node.marks.forEach((mark: any) => {
+              node.marks.forEach((mark: Mark) => {
                 if (markType === mark.type) {
                   const trimmedFrom = Math.max(pos, from)
                   const trimmedTo = Math.min(pos + node.nodeSize, to)
@@ -133,10 +124,11 @@ export const updateAttributes: RawCommands['updateAttributes'] = (typeOrName, at
                 }
               })
             }
-          })
-        }
-      })
-    }
+          }
+        })
+      }
+    })
   }
+
   return true
 }
