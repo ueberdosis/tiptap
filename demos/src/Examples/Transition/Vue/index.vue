50b255b19ad98ee3410de7f8edaf5c3cc8d5abd1
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2024-10-26 13:55:17 +0200

    fix: add nested and direct cases to example

--- demos/src/Examples/Transition/Vue/index.vue
+++ demos/src/Examples/Transition/Vue/index.vue
@@ -4,9 +4,12 @@ import { EditorContent, useEditor } from '@tiptap/vue-3'
 import { ref } from 'vue'
 
 import VueComponent from './Extension.js'
+import ParentComponent from './ParentComponent.vue'
 import type { TNote } from './types.js'
 
-const showEditor = ref(false)
+const showDirectEditor = ref(false)
+
+const showNestedEditor = ref(false)
 
 const note = ref<TNote>({
   id: 'note-1',
@@ -35,16 +38,35 @@ const editor = useEditor({
   <div>
     <button
       type="button"
-      @click="showEditor = !showEditor"
+      @click="showDirectEditor = !showDirectEditor"
       style="margin-bottom: 1rem;"
       id="toggle-editor"
     >
-      {{ showEditor ? 'Hide editor' : 'Show editor' }}
+      {{ showDirectEditor ? 'Hide direct editor' : 'Show direct editor' }}
     </button>
 
     <transition name="fade">
-      <div v-if="showEditor" class="tiptap-wrapper">
+      <div v-if="showDirectEditor" class="tiptap-wrapper">
         <EditorContent :editor="editor" />
+      </div>
+    </transition>
+  </div>
+
+  <hr>
+
+  <div>
+    <button
+      type="button"
+      @click="showNestedEditor = !showNestedEditor"
+      style="margin-bottom: 1rem;"
+      id="toggle-editor"
+    >
+      {{ showNestedEditor ? 'Hide nested editor' : 'Show nested editor' }}
+    </button>
+
+    <transition name="fade">
+      <div v-if="showNestedEditor" class="tiptap-wrapper">
+        <ParentComponent />
       </div>
     </transition>
   </div>
@@ -59,6 +81,11 @@ const editor = useEditor({
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+
+hr {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
 }
 
 .tiptap-wrapper {
