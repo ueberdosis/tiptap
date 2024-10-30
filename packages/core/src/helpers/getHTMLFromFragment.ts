f6a90482c4faf8699d6efcfa493d4ea7bc0a5878
Dominik Biedebach <dominik.biedebach@ueber.io>
2024-10-30 13:50:59 +0100

    remove node type from getHTML functionalities

--- packages/core/src/helpers/getHTMLFromFragment.ts
+++ packages/core/src/helpers/getHTMLFromFragment.ts
@@ -3,6 +3,11 @@ import { DOMSerializer, Fragment, Schema } from '@tiptap/pm/model'
 export function getHTMLFromFragment(fragment: Fragment, schema: Schema): string {
   const documentFragment = DOMSerializer.fromSchema(schema).serializeFragment(fragment)
 
+  // remove the data-node-type attribute
+  documentFragment.querySelectorAll('[data-node-type]').forEach(node => {
+    node.removeAttribute('data-node-type')
+  })
+
   const temporaryDocument = document.implementation.createHTMLDocument()
   const container = temporaryDocument.createElement('div')
 
