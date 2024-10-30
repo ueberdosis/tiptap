a7f66eab0b0763f7753e0489fcbf8302db832213
Dominik Biedebach <dominik.biedebach@ueber.io>
2024-10-30 14:35:43 +0100

    remove data-tiptap stripping

--- packages/core/src/helpers/getHTMLFromFragment.ts
+++ packages/core/src/helpers/getHTMLFromFragment.ts
@@ -4,8 +4,8 @@ export function getHTMLFromFragment(fragment: Fragment, schema: Schema): string 
   const documentFragment = DOMSerializer.fromSchema(schema).serializeFragment(fragment)
 
   // remove the data-node-type attribute
-  documentFragment.querySelectorAll('[data-node-type]').forEach(node => {
-    node.removeAttribute('data-node-type')
+  documentFragment.querySelectorAll('[data-tiptap]').forEach(node => {
+    node.removeAttribute('data-tiptap')
   })
 
   const temporaryDocument = document.implementation.createHTMLDocument()
