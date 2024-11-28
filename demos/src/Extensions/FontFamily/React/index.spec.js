cabd6c05ee534d00d795b8108451a8d0b8c42ba8
Alex Casillas <alexvcasilas@gmail.com>
2024-11-28 10:36:07 +0100

    fix: remove unused spec for font family extension

--- demos/src/Extensions/FontFamily/React/index.spec.js
+++ demos/src/Extensions/FontFamily/React/index.spec.js
@@ -29,15 +29,6 @@ context('/src/Extensions/FontFamily/React/', () => {
     cy.get('.tiptap span').should('not.exist')
   })
 
-  it('should work with font-family that have spaces in them', () => {
-    cy.get('[data-test-id="comic-sans"]')
-      .should('not.have.class', 'is-active')
-      .click()
-      .should('have.class', 'is-active')
-
-    cy.get('.tiptap').find('span').should('have.attr', 'style', 'font-family: Comic Sans MS, Comic Sans')
-  })
-
   it('should allow CSS variables as a font-family', () => {
     cy.get('[data-test-id="css-variable"]')
       .should('not.have.class', 'is-active')
