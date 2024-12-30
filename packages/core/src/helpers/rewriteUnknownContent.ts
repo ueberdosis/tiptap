e9b6c9bd6934d2a72cc47b36415c47c5d18af585
Nick the Sick <nicholas.perez@tiptap.dev>
2024-12-30 13:22:49 +0100

    feat: return the rewritten content

--- packages/core/src/helpers/rewriteUnknownContent.ts
+++ packages/core/src/helpers/rewriteUnknownContent.ts
@@ -10,6 +10,17 @@ type RewriteUnknownContentOptions = {
   fallbackToParagraph?: boolean;
 };
 
+type RewrittenContent = {
+  /**
+   * The original JSON content that was rewritten
+   */
+  original: JSONContent;
+  /**
+   * The name of the node or mark that was unsupported
+   */
+  unsupported: string;
+}[];
+
 /**
  * The actual implementation of the rewriteUnknownContent function
  */
@@ -18,12 +29,23 @@ function rewriteUnknownContentInner({
   validMarks,
   validNodes,
   options,
+  rewrittenContent = [],
 }: {
   json: JSONContent;
   validMarks: Set<string>;
   validNodes: Set<string>;
   options?: RewriteUnknownContentOptions;
-}) {
+  rewrittenContent?: RewrittenContent;
+}): {
+  /**
+   * The cleaned JSON content
+   */
+  json: JSONContent | null;
+  /**
+   * The array of nodes and marks that were rewritten
+   */
+  rewrittenContent: RewrittenContent;
+} {
   if (json.marks && Array.isArray(json.marks)) {
     json.marks = json.marks.filter(mark => {
       const name = typeof mark === 'string' ? mark : mark.type
@@ -31,26 +53,54 @@ function rewriteUnknownContentInner({
       if (validMarks.has(name)) {
         return true
       }
+
+      rewrittenContent.push({
+        original: JSON.parse(JSON.stringify(mark)),
+        unsupported: name,
+      })
       // Just ignore any unknown marks
       return false
     })
   }
 
   if (json.content && Array.isArray(json.content)) {
-    json.content = json.content.map(value => rewriteUnknownContentInner({
-      json: value,
-      validMarks,
-      validNodes,
-      options,
-    }))
+    json.content = json.content
+      .map(
+        value => rewriteUnknownContentInner({
+          json: value,
+          validMarks,
+          validNodes,
+          options,
+          rewrittenContent,
+        }).json,
+      )
+      .filter(a => a !== null && a !== undefined)
   }
 
   if (json.type && !validNodes.has(json.type)) {
-    // Just treat it like a paragraph and hope for the best
-    json.type = 'paragraph'
+    rewrittenContent.push({
+      original: JSON.parse(JSON.stringify(json)),
+      unsupported: json.type,
+    })
+
+    if (json.content && Array.isArray(json.content) && (options?.fallbackToParagraph !== false)) {
+      // Just treat it like a paragraph and hope for the best
+      json.type = 'paragraph'
+
+      return {
+        json,
+        rewrittenContent,
+      }
+    }
+
+    // or just omit it entirely
+    return {
+      json: null,
+      rewrittenContent,
+    }
   }
 
-  return json
+  return { json, rewrittenContent }
 }
 
 /**
@@ -70,7 +120,25 @@ export function rewriteUnknownContent(
    * Options for the cleaning process
    */
   options?: RewriteUnknownContentOptions,
-) {
+): {
+  /**
+   * The cleaned JSON content
+   */
+  json: JSONContent | null;
+  /**
+   * The array of nodes and marks that were rewritten
+   */
+  rewrittenContent: {
+    /**
+     * The original JSON content that was rewritten
+     */
+    original: JSONContent;
+    /**
+     * The name of the node or mark that was unsupported
+     */
+    unsupported: string;
+  }[];
+} {
   return rewriteUnknownContentInner({
     json,
     validNodes: new Set(Object.keys(schema.nodes)),
