8adb851a57f3ba8e389f9505ee4b003a6ad796ad
Nick the Sick <nicholas.perez@tiptap.dev>
2025-01-03 17:19:38 +0100

    feat(html): switch from `zeed-dom` to `happy-dom-without-node`

--- tests/cypress/integration/html/generateHTML.spec.ts
+++ tests/cypress/integration/html/generateHTML.spec.ts
@@ -3,7 +3,8 @@
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
-import { generateHTML } from '@tiptap/html'
+import Youtube from '@tiptap/extension-youtube'
+import { generateHTML, generateJSON } from '@tiptap/html'
 
 describe('generateHTML', () => {
   it('generate HTML from JSON without an editor instance', () => {
@@ -24,6 +25,41 @@ describe('generateHTML', () => {
       Text,
     ])
 
-    expect(html).to.eq('<p>Example Text</p>')
+    expect(html).to.eq('<p xmlns="http://www.w3.org/1999/xhtml">Example Text</p>')
+  })
+
+  it('can convert from & to html', async () => {
+    const extensions = [Document, Paragraph, Text, Youtube]
+    const html = `<p>Tiptap now supports YouTube embeds! Awesome!</p>
+      <div data-youtube-video>
+        <iframe src="https://www.youtube.com/watch?v=cqHqLQgVCgY"></iframe>
+      </div>`
+    const json = generateJSON(html, extensions)
+
+    expect(json).to.deep.equal({
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'text',
+              text: 'Tiptap now supports YouTube embeds! Awesome!',
+            },
+          ],
+        },
+        {
+          type: 'youtube',
+          attrs: {
+            src: 'https://www.youtube.com/watch?v=cqHqLQgVCgY',
+            start: 0,
+            width: 640,
+            height: 480,
+          },
+        },
+      ],
+    })
+
+    expect(generateHTML(json, extensions)).to.equal('<p xmlns="http://www.w3.org/1999/xhtml">Tiptap now supports YouTube embeds! Awesome!</p><div xmlns="http://www.w3.org/1999/xhtml" data-youtube-video=""><iframe width="640" height="480" allowfullscreen="true" autoplay="false" disablekbcontrols="false" enableiframeapi="false" endtime="0" ivloadpolicy="0" loop="false" modestbranding="false" origin="" playlist="" src="https://www.youtube.com/embed/cqHqLQgVCgY" start="0"></iframe></div>')
   })
 })
