ffe5b9a9a9725cf3087fb4cd874c4e919e2524c5
Nick the Sick <nicholas.perez@tiptap.dev>
2025-01-06 12:15:46 +0100

    docs: add demo for static renderer

--- packages/static-renderer/src/json/html-string/string.ts
+++ packages/static-renderer/src/json/html-string/string.ts
@@ -4,18 +4,18 @@ import type { MarkType, NodeType } from '@tiptap/core'
 import { TiptapStaticRenderer, TiptapStaticRendererOptions } from '../renderer.js'
 
 export function renderJSONContentToString<
-/**
- * A mark type is either a JSON representation of a mark or a Prosemirror mark instance
- */
-TMarkType extends { type: any } = MarkType,
-/**
- * A node type is either a JSON representation of a node or a Prosemirror node instance
- */
-TNodeType extends {
-  content?: { forEach:(cb: (node: TNodeType) => void) => void };
-  marks?: readonly TMarkType[];
-  type: string | { name: string };
-} = NodeType,
+  /**
+   * A mark type is either a JSON representation of a mark or a Prosemirror mark instance
+   */
+  TMarkType extends { type: any } = MarkType,
+  /**
+   * A node type is either a JSON representation of a node or a Prosemirror node instance
+   */
+  TNodeType extends {
+    content?: { forEach: (cb: (node: TNodeType) => void) => void }
+    marks?: readonly TMarkType[]
+    type: string | { name: string }
+  } = NodeType,
 >(options: TiptapStaticRendererOptions<string, TMarkType, TNodeType>) {
   return TiptapStaticRenderer(ctx => {
     return ctx.component(ctx.props as any)
