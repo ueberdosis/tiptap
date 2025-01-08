a2fe7b4d1c9f437b53cb01256e2ba91e1a56d68e
Nick the Sick <nicholas.perez@tiptap.dev>
2025-01-08 09:04:08 +0100

    test: update tests

--- demos/src/GuideContent/GenerateHTML/React/index.spec.js
+++ demos/src/GuideContent/GenerateHTML/React/index.spec.js
@@ -6,6 +6,6 @@ context('/src/GuideContent/GenerateHTML/React/', () => {
   it('should render the content as an HTML string', () => {
     cy.get('pre code').should('exist')
 
-    cy.get('pre code').should('contain', '<p>Example <strong>Text</strong></p>')
+    cy.get('pre code').should('contain', '<p xmlns="http://www.w3.org/1999/xhtml">Example <strong>Text</strong></p>')
   })
 })
