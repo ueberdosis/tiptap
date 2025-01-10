9bf776e23f23522a0a4166d15dfc26b99ddc7a58
Nick the Sick <nicholas.perez@tiptap.dev>
2025-01-10 15:55:58 +0100

    test: try alt key too

--- demos/src/Examples/Shortcuts/React/index.spec.js
+++ demos/src/Examples/Shortcuts/React/index.spec.js
@@ -11,14 +11,14 @@ context('/src/Examples/Shortcuts/React/', () => {
 
   it('should update the hint html when the keyboard shortcut is pressed', () => {
     cy.get('.tiptap')
-      .trigger('keydown', { modKey: true, key: 'Enter' })
+      .trigger('keydown', { modKey: true, altKey: true, key: 'Enter' })
     cy.get('.hint')
       .should('contain', 'Meta-Enter was the last shortcut')
   })
 
   it('should update the hint html when the keyboard shortcut is pressed', () => {
     cy.get('.tiptap')
-      .trigger('keydown', { shiftKey: true, key: 'Enter' })
+      .trigger('keydown', { shiftKey: true, altKey: true, key: 'Enter' })
     cy.get('.hint')
       .should('contain', 'Shift-Enter was the last shortcut')
   })
