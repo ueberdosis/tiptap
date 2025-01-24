94f3c0f675f63ce24669e3a1971499d1f3d18e8d
Nick the Sick <nicholas.perez@tiptap.dev>
2025-01-24 11:08:17 +0100

    chore: catch another access of window with a better error

--- packages/core/src/utilities/elementFromString.ts
+++ packages/core/src/utilities/elementFromString.ts
@@ -15,6 +15,9 @@ const removeWhitespaces = (node: HTMLElement) => {
 }
 
 export function elementFromString(value: string): HTMLElement {
+  if (typeof window === 'undefined') {
+    throw new Error('[tiptap error]: there is no window object available, so this function cannot be used')
+  }
   // add a wrapper to preserve leading and trailing whitespace
   const wrappedValue = `<body>${value}</body>`
 
