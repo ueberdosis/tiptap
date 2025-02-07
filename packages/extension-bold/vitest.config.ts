db4c725bd77f3920fc7e63b18b29b73927f3f588
Alex Casillas <alexvcasilas@gmail.com>
2025-02-07 13:32:59 +0100

    chore: remove resolve aliases in the package vitest config as its not needed

--- packages/extension-bold/vitest.config.ts
+++ packages/extension-bold/vitest.config.ts
@@ -1,4 +1,3 @@
-import path from 'path'
 import type { UserConfig } from 'vite'
 import { mergeConfig } from 'vitest/config'
 
@@ -8,14 +7,5 @@ export default mergeConfig(defaultConfig, {
   test: {
     environment: 'jsdom',
     include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
-  },
-  resolve: {
-    alias: {
-      '@tiptap/extension-bold': path.join(__dirname, '..', '..', 'packages/extension-bold/src'),
-      '@tiptap/extension-document': path.join(__dirname, '..', '..', 'packages/extension-document/src'),
-      '@tiptap/extension-paragraph': path.join(__dirname, '..', '..', 'packages/extension-paragraph/src'),
-      '@tiptap/extension-text': path.join(__dirname, '..', '..', 'packages/extension-text/src'),
-    },
-    preserveSymlinks: true,
   },
 }) satisfies UserConfig
