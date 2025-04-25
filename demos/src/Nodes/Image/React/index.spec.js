d384af65827cc03f6a82ed106fb58677acc9b608
Dominik Biedebach <dominik.biedebach@tiptap.dev>
2025-04-26 01:30:15 +0200

    add tests for resizable handles

--- demos/src/Nodes/Image/React/index.spec.js
+++ demos/src/Nodes/Image/React/index.spec.js
@@ -21,4 +21,26 @@ context('/src/Nodes/Image/React/', () => {
       cy.get('.tiptap').find('img').should('have.attr', 'src', 'foobar.png')
     })
   })
+
+  it('should have the resize handles on the image', () => {
+    const editor = cy.get('.tiptap')
+
+    // we are going to look for the first image container
+    const firstImg = editor.find('[data-node="image"]').first()
+
+    const handles = {
+      left: firstImg.find('.resize-handle-left'),
+      right: firstImg.find('.resize-handle-right'),
+      top: firstImg.find('.resize-handle-top'),
+      bottom: firstImg.find('.resize-handle-bottom'),
+      topLeft: firstImg.find('.resize-handle-top-left'),
+      topRight: firstImg.find('.resize-handle-top-right'),
+      bottomLeft: firstImg.find('.resize-handle-bottom-left'),
+      bottomRight: firstImg.find('.resize-handle-bottom-right'),
+    }
+
+    Object.values(handles).forEach(handle => {
+      handle.should('exist')
+    })
+  })
 })
