1ae247f505e87fc9754f3bb78932429a33f56650
bdbch <dominik@bdbch.com>
2025-05-05 18:12:54 +0200

    fix tests

--- demos/src/Extensions/TextAlign/Vue/index.spec.js
+++ demos/src/Extensions/TextAlign/Vue/index.spec.js
@@ -89,14 +89,14 @@ context('/src/Extensions/TextAlign/Vue/', () => {
     cy.get('.tiptap').find('p').should('not.have.css', 'text-align', 'left')
   })
 
-  it('toggle the text to right on the 6rd button', () => {
+  it('toggle the text to right on the 6th button', () => {
     cy.get('button:nth-child(6)').click()
 
     cy.get('.tiptap').find('p').should('have.css', 'text-align', 'right')
 
     cy.get('button:nth-child(6)').click()
 
-    cy.get('.tiptap').find('p').should('have.css', 'text-align', 'left')
+    cy.get('.tiptap').find('p').should('have.css', 'text-align', 'start')
   })
 
   it('aligns the text left when pressing the keyboard shortcut', () => {
