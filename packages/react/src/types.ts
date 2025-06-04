58090472552df445ca465816df27bef0c5d5196b
bdbch <dominik@bdbch.com>
2025-06-04 12:19:30 +0200

    remove unused 'node' property from ReactNodeViewProps type definition

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -3,5 +3,4 @@ import type React from 'react'
 
 export type ReactNodeViewProps<T = HTMLElement> = CoreNodeViewProps & {
   ref: React.RefObject<T>
-  node: any // TODO: Replace with a more specific type if possible
 }
