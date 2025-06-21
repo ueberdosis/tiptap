4e7edbde643be27b4b54f47b5945bcc5cee9f6da
adeboyedn <adeboyed93@gmail.com>
2025-06-21 15:02:42 +0100

    fix: lint

--- tests/cypress/integration/static-renderer/react-string.spec.ts
+++ tests/cypress/integration/static-renderer/react-string.spec.ts
@@ -6,7 +6,7 @@ import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import { Mark, Node } from '@tiptap/pm/model'
-import { mapAttrsToHTMLAttributes,renderToReactElement } from '@tiptap/static-renderer/pm/react'
+import { mapAttrsToHTMLAttributes, renderToReactElement } from '@tiptap/static-renderer/pm/react'
 import React from 'react'
 
 describe('static renderer: react', () => {
