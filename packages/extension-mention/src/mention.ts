338b06516fc3eb8f174502266e866aa8516f155d
Arnau Gómez Farell <arnau.gomez@tiptap.dev>
2025-06-27 13:15:16 +0200

    fix: do not instantiate editor

--- packages/extension-mention/src/mention.ts
+++ packages/extension-mention/src/mention.ts
@@ -1,4 +1,5 @@
-import { Editor, mergeAttributes, Node } from '@tiptap/core'
+import type { Editor } from '@tiptap/core'
+import { mergeAttributes, Node } from '@tiptap/core'
 import type { DOMOutputSpec } from '@tiptap/pm/model'
 import { Node as ProseMirrorNode } from '@tiptap/pm/model'
 import type { SuggestionOptions } from '@tiptap/suggestion'
@@ -113,7 +114,7 @@ function getSuggestions(options: GetSuggestionsOptions) {
   return (options.options.suggestions.length ? options.options.suggestions : [options.options.suggestion]).map(
     suggestion =>
       getSuggestionOptions({
-        editor: options.editor ?? new Editor(),
+        editor: options.editor ?? (null as any),
         overrideSuggestionOptions: suggestion,
         extensionName: options.name,
         char: suggestion.char,
