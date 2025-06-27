9064e80f9dcc691557a7ca60583c65167df837ee
Dominik Biedebach <dominik.biedebach@ueber.io>
2025-06-27 10:57:50 +0200

    use different class name for tests

--- demos/src/Extensions/Mathematics/Vue/index.spec.js
+++ demos/src/Extensions/Mathematics/Vue/index.spec.js
@@ -4,12 +4,12 @@ context('/src/Extensions/Mathematics/Vue/', () => {
   })
 
   it('should include katex-rendered inline and block nodes', () => {
-    cy.get('.ProseMirror').then(() => {
-      cy.get('.ProseMirror span[data-type="inline-math"]').should('have.length', 18)
-      cy.get('.ProseMirror div[data-type="block-math"]').should('have.length', 1)
+    cy.get('.tiptap').then(() => {
+      cy.get('.tiptap span[data-type="inline-math"]').should('have.length', 18)
+      cy.get('.tiptap div[data-type="block-math"]').should('have.length', 1)
 
-      cy.get('.ProseMirror span[data-type="inline-math"] .katex').should('have.length', 18)
-      cy.get('.ProseMirror div[data-type="block-math"] .katex').should('have.length', 1)
+      cy.get('.tiptap span[data-type="inline-math"] .katex').should('have.length', 18)
+      cy.get('.tiptap div[data-type="block-math"] .katex').should('have.length', 1)
     })
   })
 })
