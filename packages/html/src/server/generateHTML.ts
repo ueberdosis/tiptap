6c46020a250ad0209c7b51253b17b937b66c986e
bdbch <6538827+bdbch@users.noreply.github.com>
2025-07-03 01:09:48 +0200

    add additional throws

--- packages/html/src/server/generateHTML.ts
+++ packages/html/src/server/generateHTML.ts
@@ -4,6 +4,12 @@ import { Node } from '@tiptap/pm/model'
 import { getHTMLFromFragment } from './getHTMLFromFragment.js'
 
 export async function generateHTML(doc: JSONContent, extensions: Extensions): Promise<string> {
+  if (typeof window !== 'undefined') {
+    throw new Error(
+      'generateHTML can only be used in a Node environment\nIf you want to use this in a browser environment, use the `@tiptap/html` import instead.',
+    )
+  }
+
   const schema = getSchema(extensions)
   const contentNode = Node.fromJSON(schema, doc)
 
