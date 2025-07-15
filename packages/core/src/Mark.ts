bd90090f1ad80e102df5f6965c56e25662343346
bdbch <6538827+bdbch@users.noreply.github.com>
2025-07-15 20:44:59 +0200

    Fix typescript loosing context to "this" when non-function options are set in extended extensions (#6645) (#6647)
    * feat: add extend method overloads for Extension, Mark, and Node classes
    
    * added changelog

--- packages/core/src/Mark.ts
+++ packages/core/src/Mark.ts
@@ -188,11 +188,19 @@ export class Mark<Options = any, Storage = any> extends Extendable<Options, Stor
     return super.configure(options) as Mark<Options, Storage>
   }
 
+  extend<ExtendedOptions = Options, ExtendedStorage = Storage>(
+    extendedConfig: () => Partial<MarkConfig<ExtendedOptions, ExtendedStorage>>,
+  ): Mark<ExtendedOptions, ExtendedStorage>
+  extend<ExtendedOptions = Options, ExtendedStorage = Storage>(
+    extendedConfig: Partial<MarkConfig<ExtendedOptions, ExtendedStorage>>,
+  ): Mark<ExtendedOptions, ExtendedStorage>
   extend<
     ExtendedOptions = Options,
     ExtendedStorage = Storage,
     ExtendedConfig = MarkConfig<ExtendedOptions, ExtendedStorage>,
-  >(extendedConfig?: Partial<ExtendedConfig> | (() => Partial<ExtendedConfig>)) {
+  >(
+    extendedConfig?: Partial<ExtendedConfig> | (() => Partial<ExtendedConfig>),
+  ): Mark<ExtendedOptions, ExtendedStorage> {
     // If the extended config is a function, execute it to get the configuration object
     const resolvedConfig = typeof extendedConfig === 'function' ? extendedConfig() : extendedConfig
     return super.extend(resolvedConfig) as Mark<ExtendedOptions, ExtendedStorage>
