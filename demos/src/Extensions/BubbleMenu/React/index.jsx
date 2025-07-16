912511745834acfa04948da0ab06b859d6dccb6c
bdbch <dominik@bdbch.com>
2025-07-16 12:42:34 +0200

    remove inline from demos

--- demos/src/Extensions/BubbleMenu/React/index.jsx
+++ demos/src/Extensions/BubbleMenu/React/index.jsx
@@ -43,7 +43,7 @@ export default () => {
       </div>
 
       {editor && showMenu && (
-        <BubbleMenu editor={editor} options={{ placement: 'bottom', offset: 8, inline: true }}>
+        <BubbleMenu editor={editor} options={{ placement: 'bottom', offset: 8 }}>
           <div className="bubble-menu">
             <button
               onClick={() => editor.chain().focus().toggleBold().run()}
