9dbc2c85366567ea44568690d731205cfc93e238
donovan-fournier <donovan@there.do>
2025-08-26 18:43:10 +0200

    Add sample to demonstrate the expected behavior

--- demos/src/Extensions/BubbleMenu/React/index.jsx
+++ demos/src/Extensions/BubbleMenu/React/index.jsx
@@ -12,6 +12,13 @@ export default () => {
       <p>
         Hey, try to select some text here. There will popup a menu for selecting some inline styles. Remember: you have full control about content and styling of this menu.
       </p>
+      <ul>
+        <li>Select any item to display a global menu</li>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
     `,
   })
 
@@ -43,31 +50,56 @@ export default () => {
       </div>
 
       {editor && showMenu && (
-        <BubbleMenu editor={editor} options={{ placement: 'bottom', offset: 8 }}>
-          <div className="bubble-menu">
-            <button
-              onClick={() => editor.chain().focus().toggleBold().run()}
-              className={editor.isActive('bold') ? 'is-active' : ''}
-              type="button"
-            >
-              Bold
-            </button>
-            <button
-              onClick={() => editor.chain().focus().toggleItalic().run()}
-              className={editor.isActive('italic') ? 'is-active' : ''}
-              type="button"
-            >
-              Italic
-            </button>
-            <button
-              onClick={() => editor.chain().focus().toggleStrike().run()}
-              className={editor.isActive('strike') ? 'is-active' : ''}
-              type="button"
-            >
-              Strike
-            </button>
-          </div>
-        </BubbleMenu>
+        <>
+          <BubbleMenu editor={editor} options={{ placement: 'bottom', offset: 8 }}>
+            <div className="bubble-menu">
+              <button
+                onClick={() => editor.chain().focus().toggleBold().run()}
+                className={editor.isActive('bold') ? 'is-active' : ''}
+                type="button"
+              >
+                Bold
+              </button>
+              <button
+                onClick={() => editor.chain().focus().toggleItalic().run()}
+                className={editor.isActive('italic') ? 'is-active' : ''}
+                type="button"
+              >
+                Italic
+              </button>
+              <button
+                onClick={() => editor.chain().focus().toggleStrike().run()}
+                className={editor.isActive('strike') ? 'is-active' : ''}
+                type="button"
+              >
+                Strike
+              </button>
+            </div>
+          </BubbleMenu>
+
+          <BubbleMenu
+            editor={editor}
+            shouldShow={() => editor.isActive('bulletList') || editor.isActive('orderedList')}
+            options={{ placement: 'top', offset: 8 }}
+          >
+            <div className="bubble-menu">
+              <button
+                onClick={() => {
+                  const chain = editor.chain().focus()
+                  if (editor.isActive('bulletList')) {
+                    chain.toggleOrderedList()
+                  } else {
+                    chain.toggleBulletList()
+                  }
+                  chain.run()
+                }}
+                type="button"
+              >
+                Toggle list type
+              </button>
+            </div>
+          </BubbleMenu>
+        </>
       )}
       <EditorContent editor={editor} />
     </>
