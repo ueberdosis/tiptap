ddfe8577a3d6b988e153dabec69cc516091d49af
Aslam <iupin5212@gmail.com>
2025-10-10 05:52:32 +0700

    chore: update vue drag handle extension and demo

--- packages/extension-drag-handle-vue-3/src/DragHandle.ts
+++ packages/extension-drag-handle-vue-3/src/DragHandle.ts
@@ -4,15 +4,19 @@ import {
   DragHandlePlugin,
   dragHandlePluginDefaultKey,
 } from '@tiptap/extension-drag-handle'
+import type { Node as PMNode } from '@tiptap/pm/model'
 import type { Editor } from '@tiptap/vue-3'
+import type { Plugin, PluginKey } from 'packages/pm/state'
 import type { PropType } from 'vue'
-import { defineComponent, h, onBeforeUnmount, onMounted, ref } from 'vue'
+import { defineComponent, h, nextTick, onBeforeUnmount, onMounted, ref, shallowRef } from 'vue'
 
 type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
 
 export type DragHandleProps = Omit<Optional<DragHandlePluginProps, 'pluginKey'>, 'element'> & {
   class?: string
-  onNodeChange?: (data: { node: Node | null; editor: Editor; pos: number }) => void
+  onNodeChange?: (data: { node: PMNode | null; editor: Editor; pos: number }) => void
+  onElementDragStart?: DragHandlePluginProps['onElementDragStart']
+  onElementDragEnd?: DragHandlePluginProps['onElementDragEnd']
 }
 
 export const DragHandle = defineComponent({
@@ -20,7 +24,7 @@ export const DragHandle = defineComponent({
 
   props: {
     pluginKey: {
-      type: [String, Object] as PropType<DragHandleProps['pluginKey']>,
+      type: [String, Object] as PropType<PluginKey | string>,
       default: dragHandlePluginDefaultKey,
     },
 
@@ -39,6 +43,16 @@ export const DragHandle = defineComponent({
       default: null,
     },
 
+    onElementDragStart: {
+      type: Function as PropType<DragHandleProps['onElementDragStart']>,
+      default: null,
+    },
+
+    onElementDragEnd: {
+      type: Function as PropType<DragHandleProps['onElementDragEnd']>,
+      default: null,
+    },
+
     class: {
       type: String as PropType<DragHandleProps['class']>,
       default: 'drag-handle',
@@ -47,25 +61,45 @@ export const DragHandle = defineComponent({
 
   setup(props, { slots }) {
     const root = ref<HTMLElement | null>(null)
+    const pluginHandle = shallowRef<{ plugin: Plugin; unbind: () => void } | null>(null)
 
-    onMounted(() => {
-      const { editor, pluginKey, onNodeChange, computePositionConfig } = props
+    onMounted(async () => {
+      await nextTick()
 
-      editor.registerPlugin(
-        DragHandlePlugin({
-          editor,
-          element: root.value as HTMLElement,
-          pluginKey,
-          computePositionConfig: { ...defaultComputePositionConfig, ...computePositionConfig },
-          onNodeChange,
-        }).plugin,
-      )
+      const { editor, pluginKey, onNodeChange, onElementDragEnd, onElementDragStart, computePositionConfig } = props
+
+      if (!root.value) {
+        return
+      }
+      if (!props.editor || props.editor.isDestroyed) {
+        return
+      }
+
+      const init = DragHandlePlugin({
+        editor,
+        element: root.value,
+        pluginKey,
+        computePositionConfig: { ...defaultComputePositionConfig, ...computePositionConfig },
+        onNodeChange,
+        onElementDragStart,
+        onElementDragEnd,
+      })
+
+      pluginHandle.value = init
+      props.editor.registerPlugin(init.plugin)
     })
 
     onBeforeUnmount(() => {
-      const { pluginKey, editor } = props
+      if (!pluginHandle.value) {
+        return
+      }
 
-      editor.unregisterPlugin(pluginKey as string)
+      if (props.editor && !props.editor.isDestroyed) {
+        props.editor.unregisterPlugin(props.pluginKey)
+      }
+
+      pluginHandle.value.unbind?.()
+      pluginHandle.value = null
     })
 
     return () =>
