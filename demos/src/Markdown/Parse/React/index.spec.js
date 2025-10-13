a20b7749a09d978c3f10eb32eec2d6527813a47d
bdbch <dominik@bdbch.com>
2025-10-13 11:47:03 +0200

    feat: add markdown demo with custom React components and extended features

---
+++ demos/src/Markdown/Parse/React/index.spec.js
@@ -0,0 +1,35 @@
+context('/src/Examples/Minimal/React/', () => {
+  beforeEach(() => {
+    cy.visit('/src/Examples/Minimal/React/')
+  })
+
+  beforeEach(() => {
+    cy.get('.tiptap').then(([{ editor }]) => {
+      editor.commands.clearContent()
+    })
+  })
+
+  it('text should be wrapped in a paragraph by default', () => {
+    cy.get('.tiptap').type('Example Text').find('p').should('contain', 'Example Text')
+  })
+
+  it('should parse paragraphs correctly', () => {
+    cy.get('.tiptap').then(([{ editor }]) => {
+      editor.commands.setContent('<p>Example Text</p>')
+      expect(editor.getHTML()).to.eq('<p>Example Text</p>')
+
+      editor.commands.setContent('<p style="color:DodgerBlue;">Example Text</p>')
+      expect(editor.getHTML()).to.eq('<p>Example Text</p>')
+    })
+  })
+
+  it('enter should make a new paragraph', () => {
+    cy.get('.tiptap').type('First Paragraph{enter}Second Paragraph').find('p').should('have.length', 2)
+  })
+
+  it('backspace should remove the last paragraph', () => {
+    cy.get('.tiptap').type('{enter}').find('p').should('have.length', 2)
+
+    cy.get('.tiptap').type('{backspace}').find('p').should('have.length', 1)
+  })
+})
