90eafa9ea6257bac1dd56e6d3dd63a46dfbffe79
bdbch <dominik@bdbch.com>
2025-10-29 23:29:53 +0100

    Remove tests for resize handlers on image nodes in React and Vue demos

--- demos/src/Nodes/Image/Vue/index.spec.js
+++ demos/src/Nodes/Image/Vue/index.spec.js
@@ -21,41 +21,4 @@ context('/src/Nodes/Image/Vue/', () => {
       cy.get('.tiptap').find('img').should('have.attr', 'src', 'foobar.png')
     })
   })
-
-  it('should verify resize handlers on the image node', () => {
-    // Insert a pre-defined image content to ensure consistent testing
-    cy.get('.tiptap').then(([{ editor }]) => {
-      editor.commands.setContent('<img src="test-image.jpg" alt="Test image" />')
-
-      // Wait for the image to be properly rendered with its nodeview
-      cy.wait(500)
-
-      // Find the image container with data-node="image" attribute
-      cy.get('.tiptap [data-node="image"]')
-        .should('exist')
-        .and('have.length', 1)
-        .then($imageNode => {
-          // Check for all resize handles
-          const resizeHandles = [
-            'left',
-            'right',
-            'top',
-            'bottom',
-            'top-left',
-            'top-right',
-            'bottom-left',
-            'bottom-right',
-          ]
-
-          // Verify each resize handle exists with the correct position attribute
-          resizeHandles.forEach(position => {
-            cy.wrap($imageNode)
-              .find(`.resize-handle-${position}`)
-              .should('exist')
-              .should('have.attr', 'data-position', position)
-              .should('be.visible')
-          })
-        })
-    })
-  })
 })
