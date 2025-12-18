ueberdosis/tiptap

commit c0190bdc3ec19e84a0e2024d8d55cb4fc5491cab

fix: more accurate typing for `element` option (#6972)
* fix: more accurate typing for `element` option

* Enhance typing and docs for EditorOptions.element

Improve typing and documentation for EditorOptions.element, detailing supported mounting modes and behavior alignment across adapters.

---------

Co-authored-by: bdbch <6538827+bdbch@users.noreply.github.com>

packages/react/src/EditorContent.tsx
@@ -121,7 +121,7 @@ export class PureEditorContent extends React.Component<
 
       const element = this.editorContentRef.current
 
-      element.append(...editor.options.element.childNodes)
+      element.append(editor.view.dom)
 
       editor.setOptions({
         element,
@@ -176,14 +176,14 @@ export class PureEditorContent extends React.Component<
 
     editor.contentComponent = null
 
-    if (!editor.options.element?.firstChild) {
+    if (!editor.view.dom?.firstChild) {
       return
     }
 
     // TODO using the new editor.mount method might allow us to remove this
     const newElement = document.createElement('div')
 
-    newElement.append(...editor.options.element.childNodes)
+    newElement.append(editor.view.dom)
 
     editor.setOptions({
       element: newElement,

packages/vue-2/src/EditorContent.ts
@@ -25,11 +25,11 @@ export const EditorContent: Component = {
           this.$nextTick(() => {
             const element = this.$el
 
-            if (!element || !editor.options.element?.firstChild) {
+            if (!element || !editor.view.dom?.firstChild) {
               return
             }
 
-            element.append(...editor.options.element.childNodes)
+            element.append(editor.view.dom)
             editor.contentComponent = this
 
             editor.setOptions({
@@ -62,14 +62,14 @@ export const EditorContent: Component = {
 
     editor.contentComponent = null
 
-    if (!editor.options.element?.firstChild) {
+    if (!editor.view.dom?.firstChild) {
       return
     }
 
     // TODO using the new editor.mount method might allow us to remove this
     const newElement = document.createElement('div')
 
-    newElement.append(...editor.options.element.childNodes)
+    newElement.append(editor.view.dom)
 
     editor.setOptions({
       element: newElement,

packages/vue-3/src/EditorContent.ts
@@ -22,14 +22,14 @@ export const EditorContent = defineComponent({
 
       if (editor && editor.options.element && rootEl.value) {
         nextTick(() => {
-          if (!rootEl.value || !editor.options.element?.firstChild) {
+          if (!rootEl.value || !editor.view.dom?.firstChild) {
             return
           }
 
           // TODO using the new editor.mount method might allow us to remove this
           const element = unref(rootEl.value)
 
-          rootEl.value.append(...editor.options.element.childNodes)
+          rootEl.value.append(editor.view.dom)
 
           // @ts-ignore
           editor.contentComponent = instance.ctx._

packages/vue-3/src/useEditor.ts
@@ -12,7 +12,7 @@ export const useEditor = (options: Partial<EditorOptions> = {}) => {
 
   onBeforeUnmount(() => {
     // Cloning root node (and its children) to avoid content being lost by destroy
-    const nodes = editor.value?.options.element
+    const nodes = editor.value?.view.dom
     const newEl = nodes?.cloneNode(true) as HTMLElement
 
     nodes?.parentNode?.replaceChild(newEl, nodes)

packages/core/src/Editor.ts
@@ -304,7 +304,7 @@ export class Editor extends EventEmitter<EditorEvents> {
           this.editorState = state
         },
         dispatch: (tr: Transaction): ReturnType<EditorView['dispatch']> => {
-          this.editorState = this.state.apply(tr)
+          this.dispatchTransaction(tr)
         },
 
         // Stub some commonly accessed properties to prevent errors
@@ -500,7 +500,7 @@ export class Editor extends EventEmitter<EditorEvents> {
   /**
    * Creates a ProseMirror view.
    */
-  private createView(element: NonNullable<EditorOptions['element']> & {}): void {
+  private createView(element: NonNullable<EditorOptions['element']>): void {
     this.editorView = new EditorView(element, {
       ...this.options.editorProps,
       attributes: {

packages/core/src/types.ts
@@ -261,11 +261,13 @@ export type EnableRules = (AnyExtension | string)[] | boolean
 
 export interface EditorOptions {
   /**
-   * The element or selector to bind the editor to
-   * If `null` is passed, the editor will not be mounted automatically
-   * If a function is passed, it will be called with the editor's root element
+   * The element to bind the editor to:
+   * - If an `Element` is passed, the editor will be mounted appended to that element
+   * - If `null` is passed, the editor will not be mounted automatically
+   * - If an object with a `mount` property is passed, the editor will be mounted to that element
+   * - If a function is passed, it will be called with the editor's element, which should place the editor within the document
    */
-  element: Element | null
+  element: Element | { mount: HTMLElement } | ((editor: HTMLElement) => void) | null
   /**
    * The content of the editor (HTML, JSON, or a JSON array)
    */

commit 57432eb43b0948eadbf9ba69b9f7064d3993ffd5

fix: append all children of editors parent node to element
Fixes a regression introduced by #6972, that resulted in elements that
got appended to the editors parent node staying detached. E.g. the drag
handle plugin is affected by this regression.

Fixes: #7282

Signed-off-by: Jonas <jonas@freesources.org>

packages/react/src/EditorContent.tsx
@@ -114,14 +114,14 @@ export class PureEditorContent extends React.Component<
   init() {
     const editor = this.props.editor as EditorWithContentComponent | null
 
-    if (editor && !editor.isDestroyed && editor.options.element) {
+    if (editor && !editor.isDestroyed && editor.view.dom?.parentNode) {
       if (editor.contentComponent) {
         return
       }
 
       const element = this.editorContentRef.current
 
-      element.append(editor.view.dom)
+      element.append(...editor.view.dom.parentNode.childNodes)
 
       editor.setOptions({
         element,
@@ -179,14 +179,14 @@ export class PureEditorContent extends React.Component<
     // try to reset the editor element
     // may fail if this editor's view.dom was never initialized/mounted yet
     try {
-      if (!editor.view.dom?.firstChild) {
+      if (!editor.view.dom?.parentNode) {
         return
       }
 
       // TODO using the new editor.mount method might allow us to remove this
       const newElement = document.createElement('div')
 
-      newElement.append(editor.view.dom)
+      newElement.append(...editor.view.dom.parentNode.childNodes)
 
       editor.setOptions({
         element: newElement,

packages/vue-2/src/EditorContent.ts
@@ -25,11 +25,11 @@ export const EditorContent: Component = {
           this.$nextTick(() => {
             const element = this.$el
 
-            if (!element || !editor.view.dom?.firstChild) {
+            if (!element || !editor.view.dom?.parentNode) {
               return
             }
 
-            element.append(editor.view.dom)
+            element.append(...editor.view.dom.parentNode.childNodes)
             editor.contentComponent = this
 
             editor.setOptions({
@@ -62,14 +62,14 @@ export const EditorContent: Component = {
 
     editor.contentComponent = null
 
-    if (!editor.view.dom?.firstChild) {
+    if (!editor.view.dom?.parentNode) {
       return
     }
 
     // TODO using the new editor.mount method might allow us to remove this
     const newElement = document.createElement('div')
 
-    newElement.append(editor.view.dom)
+    newElement.append(...editor.view.dom.parentNode.childNodes)
 
     editor.setOptions({
       element: newElement,

packages/vue-3/src/EditorContent.ts
@@ -22,14 +22,14 @@ export const EditorContent = defineComponent({
 
       if (editor && editor.options.element && rootEl.value) {
         nextTick(() => {
-          if (!rootEl.value || !editor.view.dom?.firstChild) {
+          if (!rootEl.value || !editor.view.dom?.parentNode) {
             return
           }
 
           // TODO using the new editor.mount method might allow us to remove this
           const element = unref(rootEl.value)
 
-          rootEl.value.append(editor.view.dom)
+          rootEl.value.append(...editor.view.dom.parentNode.childNodes)
 
           // @ts-ignore
           editor.contentComponent = instance.ctx._

packages/vue-3/src/useEditor.ts
@@ -12,7 +12,7 @@ export const useEditor = (options: Partial<EditorOptions> = {}) => {
 
   onBeforeUnmount(() => {
     // Cloning root node (and its children) to avoid content being lost by destroy
-    const nodes = editor.value?.view.dom
+    const nodes = editor.value?.view.dom?.parentNode
     const newEl = nodes?.cloneNode(true) as HTMLElement
 
     nodes?.parentNode?.replaceChild(newEl, nodes)