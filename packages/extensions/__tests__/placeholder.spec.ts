f846d96007117b12f9e1e35c6d7816e0d57de4d3
bdbch <dominik@bdbch.com>
2026-01-28 12:47:43 +0100

    refactor code

--- packages/extensions/__tests__/placeholder.spec.ts
+++ packages/extensions/__tests__/placeholder.spec.ts
@@ -3,65 +3,79 @@ import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import { type PlaceholderOptions, Placeholder, preparePlaceholderAttribute } from '@tiptap/extensions'
-import { describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it } from 'vitest'
 
 describe('extension-placeholder', () => {
-  const createEditor = (placeholderOptions: Partial<PlaceholderOptions>) =>
-    new Editor({
+  let editor: Editor | null = null
+
+  const createEditor = (placeholderOptions: Partial<PlaceholderOptions>) => {
+    if (editor) {
+      editor.destroy()
+      editor = null
+    }
+
+    editor = new Editor({
       extensions: [Document, Paragraph, Text, Placeholder.configure(placeholderOptions)],
       content: '<p></p>',
     })
+  }
+
+  afterEach(() => {
+    if (editor) {
+      editor.destroy()
+    }
+  })
 
   it('uses the default data-placeholder attribute when not passing any dataAttribute option', () => {
-    const editor = createEditor({
+    createEditor({
       placeholder: 'Type something...',
     })
 
-    const paragraph = editor.view.dom.querySelector('p') as HTMLElement
+    const paragraph = editor!.view.dom.querySelector('p') as HTMLElement
     expect(paragraph.getAttribute('data-placeholder')).toBe('Type something...')
 
-    editor.destroy()
+    editor!.destroy()
   })
 
   it('falls back to the default when passing in an empty string', () => {
-    const editor = createEditor({
+    createEditor({
       placeholder: 'Type something...',
       dataAttribute: '',
     })
 
-    const paragraph = editor.view.dom.querySelector('p') as HTMLElement
+    const paragraph = editor!.view.dom.querySelector('p') as HTMLElement
     expect(paragraph.getAttribute('data-placeholder')).toBe('Type something...')
 
-    editor.destroy()
+    editor!.destroy()
   })
 
   it('uses a custom data-placeholder attribute when passing a dataAttribute option', () => {
-    const editor = createEditor({
+    createEditor({
       placeholder: 'Type something...',
       dataAttribute: 'my-placeholder',
     })
 
-    const paragraph = editor.view.dom.querySelector('p') as HTMLElement
+    const paragraph = editor!.view.dom.querySelector('p') as HTMLElement
     expect(paragraph.getAttribute('data-my-placeholder')).toBe('Type something...')
 
-    editor.destroy()
+    editor!.destroy()
   })
 
   it('auto-replaces spaces with dashes to keep a valid html element', () => {
-    const editor = createEditor({
+    createEditor({
       placeholder: 'Type something...',
       dataAttribute: 'my placeholder',
     })
 
-    const paragraph = editor.view.dom.querySelector('p') as HTMLElement
+    const paragraph = editor!.view.dom.querySelector('p') as HTMLElement
     expect(paragraph.getAttribute('data-my-placeholder')).toBe('Type something...')
 
-    editor.destroy()
+    editor!.destroy()
   })
 
   it('auto-repairs an invalid attribute string', () => {
     const brokenAttr = '5 My br0ken $tring'
-    const editor = createEditor({
+    createEditor({
       placeholder: 'Type something...',
       dataAttribute: brokenAttr,
     })
@@ -69,9 +83,9 @@ describe('extension-placeholder', () => {
     const attributeName = preparePlaceholderAttribute(brokenAttr)
     expect(attributeName).toBe('my-br0ken-tring')
 
-    const paragraph = editor.view.dom.querySelector('p') as HTMLElement
+    const paragraph = editor!.view.dom.querySelector('p') as HTMLElement
     expect(paragraph.getAttribute('data-my-br0ken-tring')).toBe('Type something...')
 
-    editor.destroy()
+    editor!.destroy()
   })
 })
