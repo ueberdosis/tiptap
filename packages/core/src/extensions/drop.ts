8d74d0fdda912de614f1841a3e584993601a5d09
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2026-03-30 16:02:13 -0500

    move drag-across-editor deletion from PasteRule to Drop extension

--- packages/core/src/extensions/drop.ts
+++ packages/core/src/extensions/drop.ts
@@ -1,16 +1,64 @@
 import { Plugin, PluginKey } from '@tiptap/pm/state'
 
+import type { Editor } from '../Editor.js'
 import { Extension } from '../Extension.js'
+
+// When dragging across editors, store the source editor globally so that
+// we can choose to delete the source content when dropped.
+let dragSourceEditor: Editor | null = null
 
 export const Drop = Extension.create({
   name: 'drop',
 
   addProseMirrorPlugins() {
+    const editor = this.editor
+
     return [
       new Plugin({
         key: new PluginKey('tiptapDrop'),
 
+        // we register a global drag handler to track the current drag source editor
+        view(view) {
+          const handleDragstart = (event: DragEvent) => {
+            if (view.dom.parentElement?.contains(event.target as Element)) {
+              dragSourceEditor = editor
+            }
+          }
+
+          const handleDragend = () => {
+            dragSourceEditor = null
+          }
+
+          window.addEventListener('dragstart', handleDragstart)
+          window.addEventListener('dragend', handleDragend)
+
+          return {
+            destroy() {
+              window.removeEventListener('dragstart', handleDragstart)
+              window.removeEventListener('dragend', handleDragend)
+            },
+          }
+        },
+
         props: {
+          handleDOMEvents: {
+            drop: () => {
+              if (dragSourceEditor !== null && dragSourceEditor !== editor && dragSourceEditor.isEditable) {
+                const dragSourceEditorCopy = dragSourceEditor
+
+                // setTimeout to avoid the wrong content after drop, timeout arg can't be empty or 0
+                setTimeout(() => {
+                  const selection = dragSourceEditorCopy.state.selection
+
+                  if (selection) {
+                    dragSourceEditorCopy.commands.deleteRange({ from: selection.from, to: selection.to })
+                  }
+                }, 10)
+              }
+              return false
+            },
+          },
+
           handleDrop: (_, e, slice, moved) => {
             this.editor.emit('drop', {
               editor: this.editor,
