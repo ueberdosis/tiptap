038acc7d29872130afad8cfc7987920dda185153
bdbch <dominik@bdbch.com>
2026-05-26 14:45:40 +0200

    chore(svelte): remove unnecessary svelte.d.ts

--- packages/svelte/src/svelte-env.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.svelte' {
-  import { type Component } from 'svelte'
-
-  const component: Component
-
-  // @ts-expect-error — for .svelte.ts runes modules, which export the component as default
-  export default component
-}
