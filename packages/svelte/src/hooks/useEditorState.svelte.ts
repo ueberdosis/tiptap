b456de1212806aca8061feb0cbd76d10cf63152e
bdbch <dominik@bdbch.com>
2026-05-26 14:49:13 +0200

    fix(svelte): unlisten transaction listener on useEditorState

--- packages/svelte/src/hooks/useEditorState.svelte.ts
+++ packages/svelte/src/hooks/useEditorState.svelte.ts
@@ -1,4 +1,4 @@
-import { type Writable, writable } from 'svelte/store'
+import { type Readable,writable } from 'svelte/store'
 
 import type { Editor } from '../Editor.js'
 
@@ -28,21 +28,41 @@ export const useEditorState = <T extends Record<string, any>>({
 }: {
   editor: Editor
   selector: (ctx: EditorStateSnapshot) => T
-}): Writable<T> => {
-  const store = writable(selector({ editor }))
+}): Readable<T> => {
+  const store = writable<T>(selector({ editor }))
 
-  const update = () => {
+  let subscriberCount = 0
+
+  const handleTransaction = () => {
     const next = selector({ editor })
 
     store.update(current => {
       if (shallowEqual(current, next)) {
         return current
       }
+
       return next
     })
   }
 
-  editor.on('transaction', update)
+  return {
+    subscribe(run, invalidate) {
+      subscriberCount += 1
 
-  return store
+      if (subscriberCount === 1) {
+        editor.on('transaction', handleTransaction)
+      }
+
+      const unsubscribe = store.subscribe(run, invalidate)
+
+      return () => {
+        unsubscribe()
+        subscriberCount -= 1
+
+        if (subscriberCount === 0) {
+          editor.off('transaction', handleTransaction)
+        }
+      }
+    },
+  }
 }
