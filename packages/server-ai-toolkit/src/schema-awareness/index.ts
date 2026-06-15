32877d931094c55210e4ea7624488ce2927927ba
Arnau Gómez Farell <arnau.gomez@tiptap.dev>
2026-06-15 19:13:09 +0200

    fix: remove legacy exports from Server AI Toolkit package (#7947)

--- packages/server-ai-toolkit/src/schema-awareness/index.ts
+++ packages/server-ai-toolkit/src/schema-awareness/index.ts
@@ -1,3 +1,2 @@
 export * from './get-editor-context.js'
-export * from './legacy-exports.js'
 export * from './types/index.js'
--- packages/server-ai-toolkit/src/schema-awareness/legacy-exports.ts
+++
@@ -1,3 +0,0 @@
-export type { GetEditorContextOptions as GetSchemaAwarenessDataOptions } from './get-editor-context.js'
-export { getEditorContext as getSchemaAwarenessData } from './get-editor-context.js'
-export type { EditorContext as SchemaAwarenessData } from './types/editor-context.js'
