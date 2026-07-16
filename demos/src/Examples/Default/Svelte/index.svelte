f5e2e3750c0db8fde7c641d15e71e2082675fab3
bdbch <6538827+bdbch@users.noreply.github.com>
2026-07-16 09:33:51 +0200

    Update demos/src/Examples/Default/Svelte/index.svelte
    Co-authored-by: Arnau Gómez Farell <arnau.gomez@tiptap.dev>

--- demos/src/Examples/Default/Svelte/index.svelte
+++ demos/src/Examples/Default/Svelte/index.svelte
@@ -13,7 +13,7 @@
     ],
     content: `
       <h2>Hi there,</h2>
-      <p>this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you'd probably expect from a text editor. But wait until you see the lists:</p>
+      <p>this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kinds of basic text styles you'd probably expect from a text editor. But wait until you see the lists:</p>
       <ul>
         <li>That's a bullet list with one …</li>
         <li>… or two list items.</li>
