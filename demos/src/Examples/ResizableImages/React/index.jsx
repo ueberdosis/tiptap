936d5217d733dbabf287bd8d84f12d7fbebbcc5f
bdbch <git@bdbch.com>
2026-08-26 16:20:17 +0200

    fix(demos): use non-blocked unsplash placeholders instead of placehold.it

--- demos/src/Examples/ResizableImages/React/index.jsx
+++ demos/src/Examples/ResizableImages/React/index.jsx
@@ -24,8 +24,8 @@ export default () => {
     ],
     content: `
         <p>This is a basic example of implementing images. Drag to re-order.</p>
-        <img src="https://placehold.co/600x400" />
-        <img src="https://placehold.co/800x400" />
+        <img src="https://unsplash.it/600/400" />
+        <img src="https://unsplash.it/800/400" />
       `,
   })
 
