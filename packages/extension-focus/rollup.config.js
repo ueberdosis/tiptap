a80c35d6af3a4bdbc18ab8e8c580808da93583a8
bdbch <git@bdbch.com>
2026-09-04 11:37:28 +0200

    chore: update JSON import attributes

--- packages/extension-focus/rollup.config.js
+++ packages/extension-focus/rollup.config.js
@@ -1,5 +1,5 @@
 import { baseConfig } from '@tiptap-shared/rollup-config'
 
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json' with { type: 'json' }
 
 export default baseConfig({ input: 'src/index.ts', pkg })
