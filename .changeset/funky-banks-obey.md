e102ea4f8f301493e711d6c160149c8c8df75639
Xiawei <xiaweiss@foxmail.com>
2026-09-18 20:25:21 +0800

    fix(core): ignore wrapper mutations in nested node views

---
+++ .changeset/funky-banks-obey.md
@@ -0,0 +1,5 @@
+---
+"@tiptap/core": patch
+---
+
+Fix node views break on enter on iOS and Android
--- packages/core/src/NodeView.ts
+++ packages/core/src/NodeView.ts
@@ -297,7 +297,8 @@ export class NodeView<
       this.dom.contains(mutation.target) &&
       mutation.type === 'childList' &&
       (isiOS() || isAndroid()) &&
-      this.editor.isFocused
+      this.editor.isFocused &&
+      this.contentDOM.hasAttribute('data-node-view-wrapper') === false
     ) {
       const changedNodes = [
         ...Array.from(mutation.addedNodes),
